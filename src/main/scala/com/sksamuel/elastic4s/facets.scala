a526e20b4bb94efadba5e263b967c23b2407dcb5
sksamuel <sam@sksamuel.com>
2013-06-29 14:37:12 +0100

    Added facets dsl

--- src/main/scala/com/sksamuel/elastic4s/facets.scala
+++ src/main/scala/com/sksamuel/elastic4s/facets.scala
@@ -1,58 +1,218 @@
 package com.sksamuel.elastic4s
 
-import org.elasticsearch.search.facet.FacetBuilders
+import org.elasticsearch.search.facet.{FacetBuilder, FacetBuilders}
+import org.elasticsearch.search.facet.terms.TermsFacet
+import org.elasticsearch.search.facet.histogram.HistogramFacet
+import org.elasticsearch.common.geo.GeoDistance
 
 /** @author Stephen Samuel */
-trait FacetBuilder {
-    def build: Facet
-}
-
-abstract class Facet(name: String)
-trait Query
-
-case class TermsFacet(name: String,
-                      fields: Seq[String],
-                      order: TermsFacetOrder = TermsFacetOrder.Count,
-                      size: Int = 10,
-                      allTerms: Boolean = false,
-                      excludeTerms: Seq[String] = Nil,
-                      script: Option[String] = None,
-                      regex: Option[String] = None,
-                      regexFlags: Seq[String] = Nil,
-                      script_field: Option[String] = None) extends Facet(name) {
-    def builder = FacetBuilders
-      .termsFacet(name)
-      .allTerms(allTerms)
-      .order(order.elasticType)
-      .regex(regex.orNull)
-      .script(script.orNull)
-      .scriptField(script_field.orNull)
-      .fields(fields: _ *)
-      .size(size)
-}
-
-case class RangeFacet(name: String, field: String, ranges: Seq[Range], keyField: Option[String] = None, valueField: Option[String] = None)
-  extends Facet(name) {
-    def builder = {
-        val builder = FacetBuilders.rangeFacet(name).field(field).keyField(keyField.orNull).valueField(valueField.orNull)
-        for ( range <- ranges )
-            builder.addRange(range.start, range.end)
-        builder
+trait FacetDsl {
+    def facet = new FacetExpectingName
+    class FacetExpectingName {
+        def name(name: String) = new FacetExpectingType(name)
+    }
+    class FacetExpectingType(name: String) {
+        def terms = new TermFacetDefinition(name)
+        def range = new RangeFacetDefinition(name)
+        def histogram = new HistogramFacetDefinition(name)
+        def filter = new FilterFacetDefinition(name)
+        def query = new QueryFacetDefinition(name)
+        def geodistance = new GeoDistanceDefinition(name)
     }
 }
 
-case class QueryFacet(name: String, query: Query) extends Facet(name) {
-    def builder = FacetBuilders.queryFacet(name, null)
+trait FacetDefinition {
+    val builder: FacetBuilder
 }
 
-case class FilterFacet(name: String, query: Query) extends Facet(name) {
-    def builder = FacetBuilders.filterFacet(name, null)
+class TermFacetDefinition(name: String) extends FacetDefinition {
+    val builder = FacetBuilders.termsFacet(name)
+    def allTerms(allTerms: Boolean): TermFacetDefinition = {
+        builder.allTerms(allTerms)
+        this
+    }
+    def executionHint(executionHint: String): TermFacetDefinition = {
+        builder.executionHint(executionHint)
+        this
+    }
+    def global(global: Boolean): TermFacetDefinition = {
+        builder.global(global)
+        this
+    }
+    def script(script: String): TermFacetDefinition = {
+        builder.script(script)
+        this
+    }
+    def lang(lang: String): TermFacetDefinition = {
+        builder.lang(lang)
+        this
+    }
+    def size(size: Int): TermFacetDefinition = {
+        builder.size(size)
+        this
+    }
+    def exclude(exclude: String*): TermFacetDefinition = {
+        builder.exclude(exclude)
+        this
+    }
+    def nested(nested: String): TermFacetDefinition = {
+        builder.nested(nested)
+        this
+    }
+    def regex(regex: String): TermFacetDefinition = {
+        builder.regex(regex)
+        this
+    }
+    def order(order: TermsFacet.ComparatorType): TermFacetDefinition = {
+        builder.order(order)
+        this
+    }
+    def fields(fields: String*): TermFacetDefinition = {
+        builder.fields(fields: _*)
+        this
+    }
 }
 
-abstract class TermsFacetOrder(val elasticType: org.elasticsearch.search.facet.terms.TermsFacet.ComparatorType)
-case object TermsFacetOrder {
-    case object Count extends TermsFacetOrder(org.elasticsearch.search.facet.terms.TermsFacet.ComparatorType.COUNT)
-    case object Term extends TermsFacetOrder(org.elasticsearch.search.facet.terms.TermsFacet.ComparatorType.TERM)
-    case object ReverseCount extends TermsFacetOrder(org.elasticsearch.search.facet.terms.TermsFacet.ComparatorType.REVERSE_COUNT)
-    case object ReverseTerm extends TermsFacetOrder(org.elasticsearch.search.facet.terms.TermsFacet.ComparatorType.REVERSE_TERM)
+class RangeFacetDefinition(name: String) extends FacetDefinition {
+    val builder = FacetBuilders.rangeFacet(name)
+    def addRange(from: Double, to: Double): RangeFacetDefinition = {
+        builder.addRange(from, to)
+        this
+    }
+    def addRange(from: String, to: String): RangeFacetDefinition = {
+        builder.addRange(from, to)
+        this
+    }
+    def global(global: Boolean): RangeFacetDefinition = {
+        builder.global(global)
+        this
+    }
+    def field(field: String): RangeFacetDefinition = {
+        builder.field(field)
+        this
+    }
+    def keyField(keyField: String): RangeFacetDefinition = {
+        builder.keyField(keyField)
+        this
+    }
+    def valueField(valueField: String): RangeFacetDefinition = {
+        builder.valueField(valueField)
+        this
+    }
+    def nested(nested: String): RangeFacetDefinition = {
+        builder.nested(nested)
+        this
+    }
+}
+
+class HistogramFacetDefinition(name: String) extends FacetDefinition {
+    val builder = FacetBuilders.histogramFacet(name)
+    def global(global: Boolean): HistogramFacetDefinition = {
+        builder.global(global)
+        this
+    }
+    def script(interval: Long): HistogramFacetDefinition = {
+        builder.interval(interval)
+        this
+    }
+    def valueField(valueField: String): HistogramFacetDefinition = {
+        builder.valueField(valueField)
+        this
+    }
+    def comparator(comparator: HistogramFacet.ComparatorType): HistogramFacetDefinition = {
+        builder.comparator(comparator)
+        this
+    }
+    def keyField(keyField: String): HistogramFacetDefinition = {
+        builder.keyField(keyField)
+        this
+    }
+    def nested(nested: String): HistogramFacetDefinition = {
+        builder.nested(nested)
+        this
+    }
+}
+
+class FilterFacetDefinition(name: String) extends FacetDefinition {
+    val builder = FacetBuilders.filterFacet(name)
+    def global(global: Boolean): FilterFacetDefinition = {
+        builder.global(global)
+        this
+    }
+    def nested(nested: String): FilterFacetDefinition = {
+        builder.nested(nested)
+        this
+    }
+    def facetFilter(block: => FilterDefinition): FilterFacetDefinition = {
+        builder.facetFilter(block.builder)
+        this
+    }
+    def lang(block: => FilterDefinition): FilterFacetDefinition = {
+        builder.filter(block.builder)
+        this
+    }
+}
+
+class QueryFacetDefinition(name: String) extends FacetDefinition {
+    val builder = FacetBuilders.queryFacet(name)
+    def global(global: Boolean): QueryFacetDefinition = {
+        builder.global(global)
+        this
+    }
+    def nested(nested: String): QueryFacetDefinition = {
+        builder.nested(nested)
+        this
+    }
+    def script(block: => QueryDefinition): QueryFacetDefinition = {
+        builder.query(block.builder)
+        this
+    }
+    def exclude(block: => FilterDefinition): QueryFacetDefinition = {
+        builder.facetFilter(block.builder)
+        this
+    }
+}
+
+class GeoDistanceDefinition(name: String) extends FacetDefinition {
+    val builder = FacetBuilders.geoDistanceFacet(name)
+    def global(global: Boolean): GeoDistanceDefinition = {
+        builder.global(global)
+        this
+    }
+    def addRange(from: Double, to: Double): GeoDistanceDefinition = {
+        builder.addRange(from, to)
+        this
+    }
+    def valueField(valueField: String): GeoDistanceDefinition = {
+        builder.valueField(valueField)
+        this
+    }
+    def valueScript(valueScript: String): GeoDistanceDefinition = {
+        builder.valueScript(valueScript)
+        this
+    }
+    def geoDistance(geoDistance: GeoDistance): GeoDistanceDefinition = {
+        builder.geoDistance(geoDistance)
+        this
+    }
+    def geohash(geohash: String): GeoDistanceDefinition = {
+        builder.geohash(geohash)
+        this
+    }
+    def lang(lang: String): GeoDistanceDefinition = {
+        builder.lang(lang)
+        this
+    }
+    def lang(lat: Double, long: Double): GeoDistanceDefinition = {
+        builder.point(lat, long)
+        this
+    }
+    def addUnboundedFrom(addUnboundedFrom: Double): GeoDistanceDefinition = {
+        builder.addUnboundedFrom(addUnboundedFrom)
+        this
+    }
+    def addUnboundedTo(addUnboundedTo: Double): GeoDistanceDefinition = {
+        builder.addUnboundedTo(addUnboundedTo)
+        this
+    }
 }
