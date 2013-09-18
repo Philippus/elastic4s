5dd5c94390f4ee7f1f423934238fefb0ae7c54ab
sksamuel <sam@sksamuel.com>
2013-09-18 07:48:07 +0100

    added decay scorers

--- src/main/scala/com/sksamuel/elastic4s/scorers.scala
+++ src/main/scala/com/sksamuel/elastic4s/scorers.scala
@@ -1,15 +1,23 @@
 package com.sksamuel.elastic4s
 
 import org.elasticsearch.index.query.functionscore.random.RandomScoreFunctionBuilder
-import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilder
+import org.elasticsearch.index.query.functionscore.{DecayFunctionBuilder, ScoreFunctionBuilder}
 import org.elasticsearch.index.query.functionscore.script.ScriptScoreFunctionBuilder
+import org.elasticsearch.index.query.functionscore.gauss.GaussDecayFunctionBuilder
+import org.elasticsearch.index.query.functionscore.exp.ExponentialDecayFunctionBuilder
+import org.elasticsearch.index.query.functionscore.lin.LinearDecayFunctionBuilder
 
 /** @author Stephen Samuel */
 trait ScoreDsl {
 
   def randomScore(seed: Long) = new RandomScoreDefinition(seed)
   def scriptScore(script: String) = new ScriptScoreDefinition(script)
-
+  def gaussianScore(field: String, origin: String, scale: String) =
+    new GaussianDecayScoreDefinition(field, origin, scale)
+  def linearScore(field: String, origin: String, scale: String) =
+    new LinearDecayScoreDefinition(field, origin, scale)
+  def exponentialScore(field: String, origin: String, scale: String) =
+    new ExponentialDecayScoreDefinition(field, origin, scale)
 }
 
 trait ScoreDefinition {
@@ -36,4 +44,30 @@ class ScriptScoreDefinition(script: String) extends ScoreDefinition {
   }
 }
 
+abstract class DecayScoreDefinition[T] extends ScoreDefinition {
+  val builder: DecayFunctionBuilder
+  def offset(offset: String): T = {
+    builder.setOffset(offset: String)
+    this.asInstanceOf[T]
+  }
+  def decay(decay: Double): T = {
+    builder.setDecay(decay)
+    this.asInstanceOf[T]
+  }
+}
+
+class GaussianDecayScoreDefinition(field: String, origin: String, scale: String)
+  extends DecayScoreDefinition[GaussianDecayScoreDefinition] {
+  val builder = new GaussDecayFunctionBuilder(field, origin, scale)
+}
+
+class LinearDecayScoreDefinition(field: String, origin: String, scale: String)
+  extends DecayScoreDefinition[LinearDecayScoreDefinition] {
+  val builder = new LinearDecayFunctionBuilder(field, origin, scale)
+}
+
+class ExponentialDecayScoreDefinition(field: String, origin: String, scale: String)
+  extends DecayScoreDefinition[ExponentialDecayScoreDefinition] {
+  val builder = new ExponentialDecayFunctionBuilder(field, origin, scale)
+}
 
