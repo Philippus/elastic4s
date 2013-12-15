1e85bb630a7224cc19c126ca069a369e3e3718b3
sksamuel <sam@sksamuel.com>
2013-12-15 15:25:29 +0000

    #51 Added doc support to update DSL

--- src/main/scala/com/sksamuel/elastic4s/UpdateDsl.scala
+++ src/main/scala/com/sksamuel/elastic4s/UpdateDsl.scala
@@ -3,13 +3,14 @@ package com.sksamuel.elastic4s
 import org.elasticsearch.action.update.{UpdateAction, UpdateRequestBuilder}
 import org.elasticsearch.action.support.replication.ReplicationType
 import org.elasticsearch.action.WriteConsistencyLevel
-import org.elasticsearch.common.xcontent.XContentFactory
-import scala.concurrent.duration._
+import org.elasticsearch.common.xcontent.{XContentBuilder, XContentFactory}
+import scala.collection.mutable.ListBuffer
 
 /** @author Stephen Samuel */
 trait UpdateDsl {
 
   def update = new UpdateExpectsId
+  def update(id: Any) = new UpdateExpectsIndex(id.toString)
   class UpdateExpectsId {
     def id(id: Any) = new UpdateExpectsIndex(id.toString)
   }
@@ -18,16 +19,34 @@ trait UpdateDsl {
   }
 
   class UpdateDefinition(index: String, id: String) extends RequestDefinition(UpdateAction.INSTANCE) with BulkCompatibleDefinition {
+
     val _builder = new UpdateRequestBuilder(null)
       .setIndex(index.split("/").head)
       .setType(index.split("/").last)
       .setId(id)
+
     def build = _builder.request
 
     def script(script: String): UpdateDefinition = {
       _builder.setScript(script)
       this
     }
+
+    private def fieldsAsXContent(fields: Iterable[(String, Any)]): XContentBuilder = {
+      val source = XContentFactory.jsonBuilder().startObject()
+      for ( tuple <- fields ) {
+        source.field(tuple._1, tuple._2)
+      }
+      source.endObject()
+    }
+
+    def doc(map: Map[String, Any]): UpdateDefinition = doc(map.toList)
+    def doc(_fields: (String, Any)*): UpdateDefinition = doc(_fields.toIterable)
+    def doc(iterable: Iterable[(String, Any)]): UpdateDefinition = {
+      _builder.setDoc(fieldsAsXContent(iterable))
+      this
+    }
+
     def routing(routing: String): UpdateDefinition = {
       _builder.setRouting(routing)
       this
--- src/test/scala/com/sksamuel/elastic4s/UpdateTest.scala
+++ src/test/scala/com/sksamuel/elastic4s/UpdateTest.scala
@@ -11,21 +11,22 @@ class UpdateTest extends FlatSpec with MockitoSugar with ElasticSugar {
 
   implicit val duration: Duration = 10.seconds
 
-  client.execute {
-    index into "scifi/trek" fields "character" -> "kirk" id 5
-  }
+  client.bulk(
+    index into "scifi/startrek" fields "character" -> "kirk" id 5,
+    index into "scifi/starwars" fields "character" -> "lando" id 8
+  )
 
   client.admin.cluster.prepareHealth().setWaitForEvents(Priority.LANGUID).setWaitForGreenStatus().execute().actionGet
 
   refresh("scifi")
-  blockUntilCount(1, "scifi")
+  blockUntilCount(2, "scifi")
 
   client.admin.cluster.prepareHealth().setWaitForEvents(Priority.LANGUID).setWaitForGreenStatus().execute().actionGet
 
   "an update request" should "add a field when a script assigns a value" in {
 
     client.sync.execute {
-      update id 5 in "scifi/trek" script "ctx._source.birthplace = 'iowa'"
+      update id 5 in "scifi/startrek" script "ctx._source.birthplace = 'iowa'"
     }
     refresh("scifi")
 
@@ -33,7 +34,30 @@ class UpdateTest extends FlatSpec with MockitoSugar with ElasticSugar {
     var hits = 0l
     while (k < 10 && hits == 0) {
       val resp = client.sync.search {
-        search in "scifi" types "trek" term "birthplace" -> "iowa"
+        search in "scifi" types "startrek" term "birthplace" -> "iowa"
+      }
+      hits = resp.getHits.totalHits()
+      Thread.sleep(k * 200)
+      k = k + 1
+    }
+    assert(1 == hits)
+  }
+
+  it should "support partial updates" in {
+
+    client.sync.execute {
+      update(8).in("scifi/starwars").doc(
+        "character" -> "lando",
+        "location" -> "cloud city"
+      )
+    }
+    refresh("scifi")
+
+    var k = 0
+    var hits = 0l
+    while (k < 10 && hits == 0) {
+      val resp = client.sync.search {
+        search in "scifi" types "starwars" term "location" -> "cloud"
       }
       hits = resp.getHits.totalHits()
       Thread.sleep(k * 200)
