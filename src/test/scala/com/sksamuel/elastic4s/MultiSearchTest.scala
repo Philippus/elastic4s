4b1af50287d2c6a693d4cd34dee31d0ba812c0fd
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2014-03-16 18:39:33 +0200

    Use asynchronous API in MultiSearchTest

--- src/test/scala/com/sksamuel/elastic4s/MultiSearchTest.scala
+++ src/test/scala/com/sksamuel/elastic4s/MultiSearchTest.scala
@@ -1,35 +1,49 @@
 package com.sksamuel.elastic4s
 
-import org.scalatest.FlatSpec
+import org.scalatest.{ FlatSpec, Matchers, OneInstancePerTest }
 import org.scalatest.mock.MockitoSugar
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.SpanSugar._
 import com.sksamuel.elastic4s.ElasticDsl._
 import org.elasticsearch.common.Priority
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /** @author Stephen Samuel */
-class MultiSearchTest extends FlatSpec with MockitoSugar with ElasticSugar {
+class MultiSearchTest
+    extends FlatSpec
+    with ElasticSugar
+    with Matchers
+    with OneInstancePerTest
+    with ScalaFutures {
 
-  client.execute {
-    index into "jtull/albums" fields ("name" -> "aqualung") id 14
-  }
-  client.sync.execute {
-    index into "jtull/albums" fields ("name" -> "passion play") id 51
-  }
-
-  client.admin.cluster.prepareHealth().setWaitForEvents(Priority.LANGUID).setWaitForGreenStatus().execute().actionGet
-
-  refresh("jtull")
-  blockUntilCount(2, "jtull")
-
-  client.admin.cluster.prepareHealth().setWaitForEvents(Priority.LANGUID).setWaitForGreenStatus().execute().actionGet
+  override implicit def patienceConfig = PatienceConfig(timeout = 10 seconds, interval = 1 seconds)
 
   "a multi search request" should "find matching documents for all queries" in {
 
-    val resp = client.sync.search(
-      search in "jtull" query "aqualung",
-      search in "jtull" query "passion"
-    )
-    assert(2 === resp.getResponses.size)
-    assert("14" === resp.getResponses.toSeq(0).getResponse.getHits.getAt(0).id())
-    assert("51" === resp.getResponses.toSeq(1).getResponse.getHits.getAt(0).id())
+    val futureInsert1 = client.execute {
+      index into "jtull/albums" fields ("name" -> "aqualung") id 14
+    }
+
+    val futureInsert2 = client.execute {
+      index into "jtull/albums" fields ("name" -> "passion play") id 51
+    }
+
+    val futureInserts = for {
+      insert1 <- futureInsert1
+      insert2 <- futureInsert2
+    } yield blockUntilCount(2, "jtull")
+
+    val futureResponse = futureInserts flatMap { _ =>
+      client search (
+        search in "jtull/albums" query "aqualung",
+        search in "jtull/albums" query "passion"
+      )
+    }
+
+    whenReady(futureResponse) { response =>
+      response.getResponses.size shouldBe 2
+      response.getResponses()(0).getResponse.getHits.getAt(0).id() shouldBe "14"
+      response.getResponses()(1).getResponse.getHits.getAt(0).id() shouldBe "51"
+    }
   }
 }
