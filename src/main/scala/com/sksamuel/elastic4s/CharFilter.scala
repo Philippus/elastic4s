51722d9e6bf2b7fcddd1626790e273b91f8d9c0d
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2014-04-13 21:17:27 +0300

    Fixes #99 Add support for character filters

--- src/main/scala/com/sksamuel/elastic4s/CharFilter.scala
+++ src/main/scala/com/sksamuel/elastic4s/CharFilter.scala
@@ -1,9 +1,43 @@
 package com.sksamuel.elastic4s
 
-/** @author Stephen Samuel */
-trait CharacterFilter
-object CharacterFilter {
-  case object HtmlStripCharFilter extends CharacterFilter
-  case class MappingCharFilter(map: Map[String, String]) extends CharacterFilter
-  case class PatternReplaceCharFilter(pattern: String, replacement: String) extends CharacterFilter
+import org.elasticsearch.common.xcontent.XContentBuilder
+
+trait AnalyzerFilter {
+  def name: String
 }
+
+trait AnalyzerFilterDefinition {
+  def filterType: String
+  def build(source: XContentBuilder): Unit
+}
+
+sealed trait CharFilter extends AnalyzerFilter
+
+sealed trait CharFilterDefinition extends CharFilter with AnalyzerFilterDefinition
+
+case object HtmlStripCharFilter extends CharFilter {
+  val name = "html_strip"
+}
+
+case class MappingCharFilter(val name: String, mappings: (String, String)*)
+    extends CharFilterDefinition {
+
+  val filterType = "mapping"
+
+  def build(source: XContentBuilder): Unit = {
+    source.field("mappings", mappings.map({ case (k, v) => s"$k=>$v" }) toArray: _*)
+  }
+
+}
+
+case class PatternReplaceCharFilter(val name: String, pattern: String, replacement: String)
+    extends CharFilterDefinition {
+
+  val filterType = "pattern_replace"
+
+  def build(source: XContentBuilder): Unit = {
+    source.field("pattern", pattern)
+    source.field("replacement", replacement)
+  }
+}
+
