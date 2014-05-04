edb608a2aab25398dadc05ab65b88c46a73a8288
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2014-05-04 20:06:02 +0300

    Fix SearchDslTest

--- src/test/scala/com/sksamuel/elastic4s/SearchDslTest.scala
+++ src/test/scala/com/sksamuel/elastic4s/SearchDslTest.scala
@@ -325,7 +325,7 @@ class SearchDslTest extends FlatSpec with MockitoSugar with OneInstancePerTest {
     val req = search in "music" types "bands" filter {
       hasChildFilter("singer") filter {
         termFilter("name", "chris")
-      } cache true cacheKey "band-singers" name "my-filter4"
+      } name "my-filter4"
     } preference Preference.Primary
     assert(json === mapper.readTree(req._builder.toString))
   }
@@ -335,7 +335,7 @@ class SearchDslTest extends FlatSpec with MockitoSugar with OneInstancePerTest {
     val req = search in "music" types "bands" filter {
       hasParentFilter("singer") filter {
         termFilter("name", "chris")
-      } cache true cacheKey "band-singers" name "my-filter5"
+      } name "my-filter5"
     } preference Preference.Primary
     assert(json === mapper.readTree(req._builder.toString))
   }
