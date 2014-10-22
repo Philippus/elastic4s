f33e027efbd5452d5f61bf4ab21d82a608a2e48c
sksamuel <sam@sksamuel.com>
2014-10-22 01:06:43 +0100

    Added missing version op to update dsl

--- src/main/scala/com/sksamuel/elastic4s/UpdateDsl.scala
+++ src/main/scala/com/sksamuel/elastic4s/UpdateDsl.scala
@@ -5,6 +5,7 @@ import org.elasticsearch.action.WriteConsistencyLevel
 import org.elasticsearch.action.support.replication.ReplicationType
 import org.elasticsearch.action.update.UpdateRequestBuilder
 import org.elasticsearch.common.xcontent.{ XContentBuilder, XContentFactory }
+import org.elasticsearch.index.VersionType
 import org.elasticsearch.script.ScriptService.ScriptType
 
 /** @author Stephen Samuel */
@@ -43,15 +44,15 @@ trait UpdateDsl extends IndexesTypesDsl {
       this
     }
 
-    def docAsUpsert(fields: (String, Any)*): UpdateDefinition = docAsUpsert(fields.toMap)
-    def docAsUpsert(iterable: Iterable[(String, Any)]): UpdateDefinition = docAsUpsert(iterable.toMap)
-    def docAsUpsert(map: Map[String, Any]): UpdateDefinition = {
+    def docAsUpsert(fields: (String, Any)*): this.type = docAsUpsert(fields.toMap)
+    def docAsUpsert(iterable: Iterable[(String, Any)]): this.type = docAsUpsert(iterable.toMap)
+    def docAsUpsert(map: Map[String, Any]): this.type = {
       _builder.setDocAsUpsert(true)
       doc(map)
     }
-    def doc(fields: (String, Any)*): UpdateDefinition = doc(fields.toMap)
-    def doc(iterable: Iterable[(String, Any)]): UpdateDefinition = doc(iterable.toMap)
-    def doc(map: Map[String, Any]): UpdateDefinition = {
+    def doc(fields: (String, Any)*): this.type = doc(fields.toMap)
+    def doc(iterable: Iterable[(String, Any)]): this.type = doc(iterable.toMap)
+    def doc(map: Map[String, Any]): this.type = {
       _builder.setDoc(_fieldsAsXContent(FieldsMapper.mapFields(map)))
       this
     }
@@ -60,58 +61,61 @@ trait UpdateDsl extends IndexesTypesDsl {
       _builder.setDoc(source.json)
       this
     }
-    def routing(routing: String): UpdateDefinition = {
+    def routing(routing: String): this.type = {
       _builder.setRouting(routing)
       this
     }
-    def params(entries: (String, Any)*): UpdateDefinition = params(entries.toMap)
-    def params(map: Map[String, Any]): UpdateDefinition = {
+    def params(entries: (String, Any)*): this.type = params(entries.toMap)
+    def params(map: Map[String, Any]): this.type = {
       map.foreach(arg => _builder.addScriptParam(arg._1, arg._2))
       this
     }
-    def retryOnConflict(retryOnConflict: Int): UpdateDefinition = {
+    def retryOnConflict(retryOnConflict: Int): this.type = {
       _builder.setRetryOnConflict(retryOnConflict)
       this
     }
-    def parent(parent: String): UpdateDefinition = {
+    def parent(parent: String): this.type = {
       _builder.setParent(parent)
       this
     }
-    def refresh(refresh: Boolean): UpdateDefinition = {
+    def refresh(refresh: Boolean): this.type = {
       _builder.setRefresh(refresh)
       this
     }
-    def replicationType(repType: ReplicationType): UpdateDefinition = {
+    def replicationType(repType: ReplicationType): this.type = {
       _builder.setReplicationType(repType)
       this
     }
-    def consistencyLevel(consistencyLevel: WriteConsistencyLevel): UpdateDefinition = {
+    def consistencyLevel(consistencyLevel: WriteConsistencyLevel): this.type = {
       _builder.setConsistencyLevel(consistencyLevel)
       this
     }
     def docAsUpsert: UpdateDefinition = docAsUpsert(shouldUpsertDoc = true)
-    def docAsUpsert(shouldUpsertDoc: Boolean): UpdateDefinition = {
+    def docAsUpsert(shouldUpsertDoc: Boolean): this.type = {
       _builder.setDocAsUpsert(shouldUpsertDoc: Boolean)
       this
     }
-    def lang(scriptLang: String): UpdateDefinition = {
+    def lang(scriptLang: String): this.type = {
       _builder.setScriptLang(scriptLang)
       this
     }
-    def upsert(map: Map[String, Any]): UpdateDefinition = {
+    def upsert(map: Map[String, Any]): this.type = {
       _builder.setUpsert(_fieldsAsXContent(FieldsMapper.mapFields(map)))
       this
     }
-    def upsert(fields: (String, Any)*): UpdateDefinition = upsert(fields.toMap)
-    def upsert(iterable: Iterable[(String, Any)]): UpdateDefinition = upsert(iterable.toMap)
+    def upsert(fields: (String, Any)*): this.type = upsert(fields.toMap)
+    def upsert(iterable: Iterable[(String, Any)]): this.type = upsert(iterable.toMap)
 
     def scriptedUpsert(upsert: Boolean): this.type = {
       _builder.setScriptedUpsert(upsert)
       this
     }
-
-    def version(version: Long): UpdateDefinition = {
+    def version(version: Long): this.type = {
       _builder.setVersion(version)
+      this
+    }
+    def version(versionType: VersionType): this.type = {
+      _builder.setVersionType(versionType)
       this
     }
   }
