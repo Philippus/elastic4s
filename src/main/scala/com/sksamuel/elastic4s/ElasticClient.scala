b5ac62db7cf43c40ade787746fcfcc4cd09a0cd5
sksamuel <sam@sksamuel.com>
2015-01-02 18:14:28 +0000

    Added elasticsearch client uri to simplify connection urls

--- src/main/scala/com/sksamuel/elastic4s/ElasticClient.scala
+++ src/main/scala/com/sksamuel/elastic4s/ElasticClient.scala
@@ -426,14 +426,14 @@ object ElasticClient {
 object ElasticsearchClientUri {
   private val PREFIX = "elasticsearch://"
   def apply(str: String): ElasticsearchClientUri = {
-    require(str != null && str.trim.nonEmpty, "Invalid uri, must be in format host:port,host:port,...")
+    require(str != null && str.trim.nonEmpty, "Invalid uri, must be in format elasticsearch://host:port,host:port,...")
     val withoutPrefix = str.replace(PREFIX, "")
     val hosts = withoutPrefix.split(',').map { host =>
       val parts = host.split(':')
       if (parts.size == 2) {
         parts(0) -> parts(1).toInt
       } else {
-        throw new IllegalArgumentException("Invalid uri, must be in format host:port,host:port,...")
+        throw new IllegalArgumentException("Invalid uri, must be in format elasticsearch://host:port,host:port,...")
       }
     }
     ElasticsearchClientUri(str, hosts.toList)
