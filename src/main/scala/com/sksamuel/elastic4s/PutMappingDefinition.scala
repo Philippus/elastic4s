0162e8408a26e12ff1ab64d1203a1566ca22692a
sksamuel <sam@sksamuel.com>
2015-01-04 15:28:38 +0000

    Added convenience method to put mapping

--- src/main/scala/com/sksamuel/elastic4s/PutMappingDefinition.scala
+++ src/main/scala/com/sksamuel/elastic4s/PutMappingDefinition.scala
@@ -1,14 +1,14 @@
 package com.sksamuel.elastic4s
 
-import com.sksamuel.elastic4s.DefinitionAttributes.{ DefinitionAttributeIndexesOptions, DefinitionAttributeIgnoreConflicts }
+import com.sksamuel.elastic4s.DefinitionAttributes.{DefinitionAttributeIndexesOptions, DefinitionAttributeIgnoreConflicts}
 import com.sksamuel.elastic4s.mappings.TypedFieldDefinition
-import org.elasticsearch.action.admin.indices.mapping.put.{ PutMappingRequestBuilder, PutMappingRequest }
+import org.elasticsearch.action.admin.indices.mapping.put.{PutMappingRequestBuilder, PutMappingRequest}
 import org.elasticsearch.common.xcontent.XContentFactory
 
 /** @author Stephen Samuel */
 class PutMappingDefinition(indexes: IndexesTypes)
-    extends DefinitionAttributeIgnoreConflicts
-    with DefinitionAttributeIndexesOptions {
+  extends DefinitionAttributeIgnoreConflicts
+  with DefinitionAttributeIndexesOptions {
 
   def build: PutMappingRequest = _builder.request
 
@@ -16,10 +16,11 @@ class PutMappingDefinition(indexes: IndexesTypes)
     .setIndices(indexes.index)
     .setType(indexes.typ.getOrElse("Must specify type for put mapping"))
 
-  def add(fields: TypedFieldDefinition*): this.type = {
+  def add(fields: TypedFieldDefinition*): this.type = add(fields)
+  def add(fields: Iterable[TypedFieldDefinition]): this.type = {
     val xcontent = XContentFactory.jsonBuilder().startObject()
     xcontent.startObject("properties")
-    for (field <- fields) {
+    for ( field <- fields ) {
       field.build(xcontent)
     }
     xcontent.endObject()
