c9a20080f587c2a68d8d7f4e9018beefc1afafba
sksamuel <sam@sksamuel.com>
2015-01-23 01:56:52 +0000

    Added filter as synonym of renamed postFilter after PR 258

--- src/main/scala/com/sksamuel/elastic4s/SearchDsl.scala
+++ src/main/scala/com/sksamuel/elastic4s/SearchDsl.scala
@@ -12,13 +12,13 @@ import scala.concurrent.duration.FiniteDuration
 
 /** @author Stephen Samuel */
 trait SearchDsl
-    extends QueryDsl
-    with FilterDsl
-    with FacetDsl
-    with HighlightDsl
-    with ScriptFieldDsl
-    with SuggestionDsl
-    with IndexesTypesDsl {
+  extends QueryDsl
+  with FilterDsl
+  with FacetDsl
+  with HighlightDsl
+  with ScriptFieldDsl
+  with SuggestionDsl
+  with IndexesTypesDsl {
 
   def select(indexes: String*): SearchDefinition = search(indexes: _*)
   def search(indexes: String*): SearchDefinition = new SearchDefinition(IndexesTypes(indexes))
@@ -31,14 +31,14 @@ trait SearchDsl
   def multi(searches: SearchDefinition*): MultiSearchDefinition = new MultiSearchDefinition(searches)
 
   implicit object SearchDefinitionExecutable
-      extends Executable[SearchDefinition, SearchResponse] {
+    extends Executable[SearchDefinition, SearchResponse] {
     override def apply(c: Client, t: SearchDefinition): Future[SearchResponse] = {
       injectFuture(c.search(t.build, _))
     }
   }
 
   implicit object MultiSearchDefinitionExecutable
-      extends Executable[MultiSearchDefinition, MultiSearchResponse] {
+    extends Executable[MultiSearchDefinition, MultiSearchResponse] {
     override def apply(c: Client, t: MultiSearchDefinition): Future[MultiSearchResponse] = {
       injectFuture(c.multiSearch(t.build, _))
     }
@@ -104,6 +104,8 @@ class SearchDefinition(indexesTypes: IndexesTypes) {
     _builder.setQuery(block.builder)
     this
   }
+
+  def filter(filter: FilterDefinition): this.type = postFilter(filter)
 
   def postFilter(block: => FilterDefinition): SearchDefinition = {
     _builder.setPostFilter(block.builder)
