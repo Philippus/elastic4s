bdc583d64e537f6f48c4fc334e00bf3ae8d95726
sksamuel <sam@sksamuel.com>
2015-02-19 00:25:42 +0000

    Added tests for exists filter

---
+++ src/test/scala/com/sksamuel/elastic4s/filter/ExistsFilterDefinition.scala
@@ -0,0 +1,41 @@
+package com.sksamuel.elastic4s.filter
+
+import com.sksamuel.elastic4s.ElasticDsl._
+import com.sksamuel.elastic4s.ElasticSugar
+import org.scalatest.{WordSpec, Matchers}
+
+class ExistsFilterDefinition extends WordSpec with ElasticSugar with Matchers {
+
+  client.execute(
+    bulk(
+      index into "person" / "interest" fields(
+        "name" -> "reese",
+        "weapon" -> "revolver"
+        ),
+      index into "person" / "interest" fields(
+        "name" -> "finch",
+        "weapon" -> "computer"
+        )
+    )
+  ).await
+
+  refresh("person")
+  blockUntilCount(2, "person")
+
+  "exists filter" should {
+    "match non-null fields" in {
+      client.execute {
+        search in "person" / "interest" postFilter {
+          existsFilter("name")
+        }
+      }.await.getHits.getTotalHits shouldBe 2
+    }
+    "not match null fields" in {
+      client.execute {
+        search in "person" / "interest" postFilter {
+          existsFilter("place")
+        }
+      }.await.getHits.getTotalHits shouldBe 0
+    }
+  }
+}
