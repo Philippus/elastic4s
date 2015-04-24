113d8438d69696a7ec664c46cabeaf46e61f670b
sksamuel <sam@sksamuel.com>
2015-04-24 02:12:53 +0100

    Added tests for type exists dsl

---
+++ src/test/scala/com/sksamuel/elastic4s/admin/TypeExistsTest.scala
@@ -0,0 +1,30 @@
+package com.sksamuel.elastic4s.admin
+
+import com.sksamuel.elastic4s.{ElasticDsl, ElasticSugar}
+import org.scalatest.WordSpec
+
+class TypeExistsTest extends WordSpec with ElasticSugar with ElasticDsl {
+
+  client.execute {
+    index into "typeexiststest" / "quantumleap" fields "name" -> "sam"
+  }.await
+
+  "type exists" should {
+    "return true for existing type" in {
+
+      val resp = client.execute {
+        types exist "quantumleap" in "typeexiststest"
+      }.await
+
+      assert(resp.isExists === true)
+    }
+    "return false for non existing type in existing index" in {
+
+      val resp = client.execute {
+        types exist "qwewqe" in "typeexiststest"
+      }.await
+
+      assert(resp.isExists === false)
+    }
+  }
+}
