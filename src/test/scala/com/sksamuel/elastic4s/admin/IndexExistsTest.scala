fec85c175841069339b4fe90ddd6862a6f411d59
sksamuel <sam@sksamuel.com>
2015-04-24 02:11:06 +0100

    Added test case for index exists

---
+++ src/test/scala/com/sksamuel/elastic4s/admin/IndexExistsTest.scala
@@ -0,0 +1,36 @@
+package com.sksamuel.elastic4s.admin
+
+import com.sksamuel.elastic4s.mappings.FieldType.StringType
+import com.sksamuel.elastic4s.{WhitespaceAnalyzer, ElasticDsl, ElasticSugar}
+import org.scalatest.WordSpec
+
+class IndexExistsTest extends WordSpec with ElasticSugar with ElasticDsl {
+
+  client.execute {
+    create index "indexexiststest" mappings {
+      "r" as Seq(
+        field name "a" withType StringType stored true analyzer WhitespaceAnalyzer,
+        field name "b" withType StringType
+      )
+    }
+  }.await
+
+  "index exists" should {
+    "return true for existing index" in {
+
+      val resp = client.execute {
+        index exists "indexexiststest"
+      }.await
+
+      assert(resp.isExists === true)
+    }
+    "return false for non existing index" in {
+
+      val resp = client.execute {
+        index exists "qweqwewqe"
+      }.await
+
+      assert(resp.isExists === false)
+    }
+  }
+}
