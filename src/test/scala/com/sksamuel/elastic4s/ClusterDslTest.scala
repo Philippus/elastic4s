1860c40d616e2b3217165b4f329be92fa6540cfc
Michael Stringer <michael.stringer@zestia.com>
2015-04-29 14:10:53 +0100

    Add timeout flag to cluster health request

--- src/test/scala/com/sksamuel/elastic4s/ClusterDslTest.scala
+++ src/test/scala/com/sksamuel/elastic4s/ClusterDslTest.scala
@@ -1,5 +1,6 @@
 package com.sksamuel.elastic4s
 
+import org.elasticsearch.common.unit.TimeValue
 import org.scalatest.{ Matchers, FlatSpec }
 import ElasticDsl._
 
@@ -13,5 +14,10 @@ class ClusterDslTest extends FlatSpec with Matchers {
   it should "accept a list of indices" in {
     val req = clusterHealth("index1", "index2")
     req.build.indices() should equal(Array("index1", "index2"))
+  }
+
+  it should "allow a timeout" in {
+    val req = clusterHealth timeout "1s"
+    req.build.timeout() should equal(TimeValue.timeValueSeconds(1))
   }
 }
