ef44e6b3c8c824010cf409b88a55a00767065776
Jakub Liska <liska.jakub@gmail.com>
2015-06-16 02:17:32 +0200

    Add docValuesFormat to timestamp mapping

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/mappings/TimestampDefinition.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/mappings/TimestampDefinition.scala
@@ -6,12 +6,14 @@ case class TimestampDefinition(enabled: Boolean,
                                path: Option[String] = None,
                                format: Option[String] = None,
                                default: Option[String] = None,
-                               store: Option[Boolean] = None) {
+                               store: Option[Boolean] = None,
+                               docValuesFormat: Option[Boolean] = None) {
 
   def path(path: String): TimestampDefinition = copy(path = Option(path))
   def format(format: String): TimestampDefinition = copy(format = Option(format))
   def default(default: String): TimestampDefinition = copy(default = Option(default))
   def store(store: Boolean): TimestampDefinition = copy(store = Option(store))
+  def docValuesFormat(b: Boolean): TimestampDefinition = copy(docValuesFormat = Option(b))
 
   private[elastic4s] def build(builder: XContentBuilder): Unit = {
     builder.startObject("_timestamp")
@@ -19,6 +21,7 @@ case class TimestampDefinition(enabled: Boolean,
     path.foreach(builder.field("path", _))
     store.foreach(s => builder.field("store", if (s) "yes" else "no"))
     format.foreach(builder.field("format", _))
+    docValuesFormat.foreach(builder.field("doc_values", _))
     default.foreach(builder.field("default", _))
     builder.endObject()
   }
