8e740cd27907e101687ac96ca85d19e66e79a706
sksamuel <sam@sksamuel.com>
2015-06-27 22:58:34 +0100

    Added show typeclass for validate #321

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/ValidateDsl.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/ValidateDsl.scala
@@ -1,7 +1,8 @@
 package com.sksamuel.elastic4s
 
-import org.elasticsearch.action.admin.indices.validate.query.ValidateQueryResponse
+import org.elasticsearch.action.admin.indices.validate.query.{ValidateQueryRequestBuilder, ValidateQueryResponse}
 import org.elasticsearch.client.Client
+import org.elasticsearch.common.xcontent.XContentHelper
 
 import scala.concurrent.Future
 
@@ -12,5 +13,48 @@ trait ValidateDsl {
     override def apply(c: Client, t: ValidateDefinition): Future[ValidateQueryResponse] = {
       injectFuture(c.admin.indices.validateQuery(t.build, _))
     }
+  }
+
+  implicit object ValidateDefinitionShow extends Show[ValidateDefinition] {
+    override def show(f: ValidateDefinition): String = XContentHelper
+      .convertToJson(f._builder.request.source, true, true)
+  }
+
+  implicit class ValidateDefinitionShowOps(f: ValidateDefinition) {
+    def show: String = ValidateDefinitionShow.show(f)
+  }
+}
+
+class ValidateDefinition(index: String, `type`: String) {
+
+  val _builder = new ValidateQueryRequestBuilder(ProxyClients.indices).setIndices(index).setTypes(`type`)
+  def build = _builder.request
+
+  /**
+   * Adds a single string query to this search
+   *
+   * @param string the query string
+   *
+   * @return this
+   */
+  def query(string: String): this.type = {
+    val q = new QueryStringQueryDefinition(string)
+    _builder.setSource(q.builder.buildAsBytes)
+    this
+  }
+
+  def rewrite(rewrite: Boolean): this.type = {
+    _builder.setRewrite(rewrite)
+    this
+  }
+
+  def query(block: => QueryDefinition): this.type = {
+    _builder.setSource(block.builder.buildAsBytes)
+    this
+  }
+
+  def explain(ex: Boolean): this.type = {
+    _builder.setExplain(ex)
+    this
   }
 }
---
+++ elastic4s-core/src/test/scala/com/sksamuel/elastic4s/ValidateShowTest.scala
@@ -0,0 +1,41 @@
+package com.sksamuel.elastic4s
+
+import org.scalatest.{Matchers, WordSpec}
+
+class ValidateShowTest extends WordSpec with Matchers with ElasticSugar {
+
+  import ElasticDsl._
+
+  "Search" should {
+    "have a show typeclass implementation" in {
+      val request = {
+        validate in "gameofthrones" / "characters" query {
+          bool {
+            should {
+              termQuery("name", "snow")
+            }.must {
+              matchQuery("location", "the wall")
+            }
+          }
+        } explain true
+      }
+      request.show shouldBe """{
+                              |  "bool" : {
+                              |    "must" : {
+                              |      "match" : {
+                              |        "location" : {
+                              |          "query" : "the wall",
+                              |          "type" : "boolean"
+                              |        }
+                              |      }
+                              |    },
+                              |    "should" : {
+                              |      "term" : {
+                              |        "name" : "snow"
+                              |      }
+                              |    }
+                              |  }
+                              |}""".stripMargin
+    }
+  }
+}
