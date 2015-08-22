833f2b9426076abab105fe1e677f25846a680f51
sksamuel <sam@sksamuel.com>
2015-08-22 02:46:55 +0100

    Updated script test to use elastic matchers

--- elastic4s-core-tests/src/test/scala/com/sksamuel/elastic4s/ScriptTest.scala
+++ elastic4s-core-tests/src/test/scala/com/sksamuel/elastic4s/ScriptTest.scala
@@ -1,18 +1,18 @@
 package com.sksamuel.elastic4s
 
 import com.sksamuel.elastic4s.ElasticDsl._
+import com.sksamuel.elastic4s.testkit.{ElasticMatchers, ElasticSugar}
 import org.elasticsearch.search.sort.SortOrder
-import org.scalatest.{ FreeSpec, Matchers }
-import com.sksamuel.elastic4s.testkit.ElasticSugar
+import org.scalatest.FreeSpec
 
-class ScriptTest extends FreeSpec with Matchers with ElasticSugar {
+class ScriptTest extends FreeSpec with ElasticMatchers with ElasticSugar {
 
   client.execute {
     bulk(
-      index into "script/tubestops" fields ("name" -> "south kensington", "line" -> "district"),
-      index into "script/tubestops" fields ("name" -> "earls court", "line" -> "district", "zone" -> 2),
-      index into "script/tubestops" fields ("name" -> "cockfosters", "line" -> "picadilly"),
-      index into "script/tubestops" fields ("name" -> "bank", "line" -> "northern")
+      index into "script/tubestops" fields("name" -> "south kensington", "line" -> "district"),
+      index into "script/tubestops" fields("name" -> "earls court", "line" -> "district", "zone" -> 2),
+      index into "script/tubestops" fields("name" -> "cockfosters", "line" -> "picadilly"),
+      index into "script/tubestops" fields("name" -> "bank", "line" -> "northern")
     )
   }.await
 
@@ -20,31 +20,22 @@ class ScriptTest extends FreeSpec with Matchers with ElasticSugar {
 
   "script fields" - {
     "can access doc fields" in {
-
-      val resp = client.execute {
-        search in "script/tubestops" query "bank" scriptfields (
-          script field "a" script "doc['line'].value + ' line'"
-        )
-      }.await
-      resp.getHits.getAt(0).field("a").value[String] shouldBe "northern line"
-
+      search in "script/tubestops" query "bank" scriptfields (
+        script field "a" script "doc['line'].value + ' line'"
+        ) should haveFieldValue("northern line")
     }
     "can use params" in {
-
-      val resp = client.execute {
-        search in "script/tubestops" query "earls" scriptfields (
-          script field "a" script "'Fare is: ' + doc['zone'].value * fare" params Map("fare" -> 4.50)
-        )
-      }.await
-      resp.getHits.getAt(0).field("a").value[String] shouldBe "Fare is: 9.0"
-
+      search in "script/tubestops" query "earls" scriptfields (
+        script field "a" script "'Fare is: ' + doc['zone'].value * fare" params Map("fare" -> 4.50)
+        ) should haveFieldValue("Fare is: 9.0")
     }
   }
   "script sort" - {
     "sort by name length" in {
       val sorted = client.execute {
-        search in "script/tubestops" query matchall sort {
-          script sort """if (_source.containsKey('name')) _source['name'].size() else 0""" order SortOrder.DESC typed "number"
+        search in "script/tubestops" query matchAllQuery sort {
+          script sort """if (_source.containsKey('name')) _source['name'].size() else 0""" order SortOrder
+            .DESC typed "number"
         }
       }.await
       sorted.hits(0).sourceAsMap("name") shouldBe "south kensington"
