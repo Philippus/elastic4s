894343d694b0f64530a3ec4d4f36932fad41365c
sksamuel <sam@sksamuel.com>
2015-08-23 14:21:42 +0100

    added UaxUrlEmailTokenizerTest builder #404

---
+++ elastic4s-core-tests/src/test/scala/com/sksamuel/elastic4s/analyzers/UaxUrlEmailTokenizerTest.scala
@@ -0,0 +1,16 @@
+package com.sksamuel.elastic4s.analyzers
+
+import com.sksamuel.elastic4s.anaylzers.TokenizerDsl
+import org.scalatest.{Matchers, WordSpec}
+
+class UaxUrlEmailTokenizerTest extends WordSpec with TokenizerDsl with Matchers {
+
+  "UaxUrlEmailTokenizer builder" should {
+    "set max token length" in {
+      uaxUrlEmailTokenizer("testy")
+        .maxTokenLength(14)
+        .json
+        .string shouldBe """{"type":"uax_url_email","max_token_length":14}"""
+    }
+  }
+}
