9cde5fe9a31fcdda00af75aa69245a1232ac679d
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2016-05-05 18:51:39 +0200

    Remove HitAsFromField and add test

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/Reader.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/Reader.scala
@@ -1,10 +1,16 @@
 package com.sksamuel.elastic4s
 
+import scala.annotation.implicitNotFound
+
 @deprecated("use HitAs, this reader trait has a broken contravariance implementation", "1.6.1")
 trait Reader[-U] {
   def read[T <: U : Manifest](json: String): T
 }
 
+@implicitNotFound(
+  "No HitAs deserializer found for type ${T}." +
+  "Try to implement an implicit HitAs instances for this type or use the elastic4s-shapeless contrib."
+)
 trait HitAs[T] {
   def as(hit: RichSearchHit): T
 }
--- elastic4s-shapeless/src/main/scala/com/sksamuel/elastic4s/shapeless/HitAsInstances.scala
+++ elastic4s-shapeless/src/main/scala/com/sksamuel/elastic4s/shapeless/HitAsInstances.scala
@@ -9,72 +9,9 @@ import com.sksamuel.elastic4s.{ HitAs, RichSearchHit }
 import com.sksamuel.elastic4s.SearchDefinition
 
 /**
- * This implementation requires the user to add the additional fields
- *
- * {{{
- * client.execute {
- *   search in "places" / "cities" fields("id", "name")
- * }
- * }}}
- * 
- * 
- * TODO provide fields name derivation as well
- * 
- * {{{
- * def fromDocument[T, Repr <: HList, KeysRepr <: HList](doc: T)(
- *     implicit
- *     gen: LabelledGeneric.Aux[T, Repr],
- *     keys: Keys.Aux[Repr, KeysRepr],
- *     ktl: ToList[KeysRepr, String]
- * ): List[String] = {
- *   gen.to(doc).keys.toList
- *   // keys().toList
- * }
- * }}}
- * 
- */
-trait HitAsFromFieldsInstances {
-
-  /** base representation */
-  implicit def hitAsHNil: HitAs[HNil] = new HitAs[HNil] {
-    override def as(hit: RichSearchHit): HNil = HNil
-  }
-
-  // primitive types and String
-
-  implicit def stringHitAsHCons[K <: Symbol, Rest <: HList](
-    implicit key: Witness.Aux[K],
-    hitRest: HitAs[Rest]) = new HitAs[FieldType[K, String] :: Rest] {
-    override def as(hit: RichSearchHit) = {
-      val value = hit.field(key.value.name).getValue[String]
-      field[K](value) :: hitRest.as(hit)
-    }
-  }
-
-  implicit def anyValhitAsHCons[K <: Symbol, V <: AnyVal, Rest <: HList](
-    implicit key: Witness.Aux[K],
-    hitRest: HitAs[Rest]) = new HitAs[FieldType[K, V] :: Rest] {
-    override def as(hit: RichSearchHit) = {
-      val value = hit.field(key.value.name).value[V]
-      field[K](value) :: hitRest.as(hit)
-    }
-  }
-
-  // generic representation to case class
-
-  implicit def hitAsGeneric[T, Repr](
-    implicit gen: LabelledGeneric.Aux[T, Repr],
-    hitsRepr: HitAs[Repr]) = new HitAs[T] {
-    override def as(hit: RichSearchHit): T = gen.from(hitsRepr.as(hit))
-  }
-  
-}
-
-
-/**
  * This implementation gets the information directly from the source map.
  *
- * - No need for additional fields in search query 
+ * - No need for additional fields in search query
  * - No need for field names derivation
  */
 trait HitAsFromSourceInstances {
@@ -113,12 +50,12 @@ trait HitAsFromSourceInstances {
   }
 }
 
-
+object HitAsFromSourceInstances extends HitAsFromSourceInstances
 
 // ---------------------------
 // ------ Test Code ----------
 // ---------------------------
-object TestDerivation extends App {
+object TestDerivation extends App with HitAsFromSourceInstances {
 
   // https://github.com/sksamuel/elastic4s
   // https://github.com/sksamuel/elastic4s/blob/master/guide/search.md
@@ -135,7 +72,7 @@ object TestDerivation extends App {
   case class City(id: Int, name: String)
 
   // this will get shapelessed soon, too
-  implicit object CharacterIndexable extends Indexable[City] {
+  implicit object CityIndexable extends Indexable[City] {
     override def json(t: City): String = s""" { "id" : ${t.id}, "name" : "${t.name}" } """
   }
   // ---
@@ -156,17 +93,20 @@ object TestDerivation extends App {
       )
     )
   }.await
-  println("-- index created")
 
-  // validate index is there
-  val mappings = client.execute {
-    getMapping("places" / "cities")
-  }.await
+  println("-- indices created")
 
-  println("-- mappings for places/cities")
-  println("  " + mappings.mappings("places")("cities").getSourceAsMap)
+  // show mappings
+  println("-- show mappings")
+  List(getMapping("places" / "cities")).foreach { cmd =>
+    val mappings = client.execute(cmd).await
+    mappings.mappings("places").foreach {
+      case (mType, mapping) => println(s"  ${mType}: ${mapping.sourceAsMap}")
+    }
+  }
+  
 
-  // insert some documents
+  // insert some cities
   List((1, "Munich"), (2, "Manchester"), (3, "London")).foreach {
     case (cityId, name) =>
       val indexResult = client.execute {
@@ -179,52 +119,23 @@ object TestDerivation extends App {
   // wait until ES persisted stuff
   Thread.sleep(1000)
 
-  println("-- start search")
-
-  object FromSource extends HitAsFromSourceInstances {
-
-    def apply() = {
-      println("----- from source")
-      val resp = client.execute {
-        search in "places" / "cities"
-      }.await
-
-      println(s"  Found: ${resp.hits.length}")
-
-      val cities = resp.as[City]
-      cities.foreach(c => println("  " + c))
-    }
-  }
-
-  object FromFields extends HitAsFromFieldsInstances {
-
-    def apply() = {
-    	println("----- from fields")
-    	
-    	// TODO derive field names from document class as well
-      val resp = client.execute {
-        search in "places" / "cities" fields ("id", "name")
-      }.await
-
-      println(s"  Found: ${resp.hits.length}")
-
-      val cities = resp.as[City]
-      cities.foreach(c => println("  " + c))
-    }
-  }
-  
-  // demonstrate both variations
-  FromSource()
-  FromFields()
-
-  
+  println("-- start city search")
   // -------------------------------
   // test implicits
-  
+
   // implicitly[HitAs[HNil]]
-  // implicitly[HitAs[City]]
+  implicitly[HitAs[City]]
 
   // this should never work
   // implicitly[HitAs[Int :: HNil]]
+
+  val resp = client.execute {
+    search in "places" / "cities"
+  }.await
+
+  println(s"  Found: ${resp.hits.length}")
+
+  val cities = resp.as[City]
+  cities.foreach(c => println("  " + c))
 
 }
