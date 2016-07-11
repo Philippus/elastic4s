702cae11d6432499afa1c938a9830855e12412b5
Eric Johnston <eric@liftigniter.com>
2016-07-11 10:46:13 -0700

    updated so it compiles

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/mappings/domain.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/mappings/domain.scala
@@ -3,8 +3,8 @@ package com.sksamuel.elastic4s.mappings
 /** @author Stephen Samuel */
 case class YesNo(val value: String)
 object YesNo {
-  case object Yes extends YesNo("yes")
-  case object No extends YesNo("no")
+  val Yes = YesNo("yes")
+  val No = YesNo("no")
   def apply(param: Boolean): YesNo = param match {
     case true => Yes
     case false => No
@@ -13,47 +13,47 @@ object YesNo {
 
 case class TermVector(val value: String)
 object TermVector {
-  case object No extends TermVector("no")
-  case object Yes extends TermVector("yes")
-  case object WithOffsets extends TermVector("with_offsets")
-  case object WithPositions extends TermVector("with_positions")
-  case object WithPositionsOffsets extends TermVector("with_positions_offsets")
-  case object WithPositionsOffsetsPayloads extends TermVector("with_positions_offsets_payloads")
+  val No = TermVector("no")
+  val Yes = TermVector("yes")
+  val WithOffsets = TermVector("with_offsets")
+  val WithPositions = TermVector("with_positions")
+  val WithPositionsOffsets = TermVector("with_positions_offsets")
+  val WithPositionsOffsetsPayloads = TermVector("with_positions_offsets_payloads")
 }
 
 case class IndexOptions(val value: String)
 object IndexOptions {
-  case object Docs extends IndexOptions("docs")
-  case object Freqs extends IndexOptions("freqs")
-  case object Positions extends IndexOptions("positions")
-  case object Offsets extends IndexOptions("offsets")
+  val Docs = IndexOptions("docs")
+  val Freqs = IndexOptions("freqs")
+  val Positions = IndexOptions("positions")
+  val Offsets = IndexOptions("offsets")
 }
 
 case class PostingsFormat(val value: String)
 object PostingsFormat {
-  case object Direct extends PostingsFormat("direct")
-  case object Memory extends PostingsFormat("memory")
-  case object Pulsing extends PostingsFormat("pulsing")
-  case object BloomDefault extends PostingsFormat("bloom_default")
-  case object BloomPulsing extends PostingsFormat("bloom_pulsing")
-  case object Default extends PostingsFormat("default")
+  val Direct = PostingsFormat("direct")
+  val Memory = PostingsFormat("memory")
+  val Pulsing = PostingsFormat("pulsing")
+  val BloomDefault = PostingsFormat("bloom_default")
+  val BloomPulsing = PostingsFormat("bloom_pulsing")
+  val Default = PostingsFormat("default")
 }
 
 case class DocValuesFormat(val value: String)
 object DocValuesFormat {
-  case object Memory extends DocValuesFormat("memory")
-  case object Disk extends DocValuesFormat("disk")
-  case object Default extends DocValuesFormat("default")
+  val Memory = DocValuesFormat("memory")
+  val Disk = DocValuesFormat("disk")
+  val Default = DocValuesFormat("default")
 }
 
-case class Similarity(val value: String)
+case class Similarity(value: String)
 object Similarity {
-  case object Default extends Similarity("default")
-  case object BM25 extends Similarity("BM25")
+  val Default = Similarity("default")
+  val BM25 = Similarity("BM25")
 }
 
 case class PrefixTree(val value: String)
 object PrefixTree {
-  case object Geohash extends PrefixTree("geohash")
-  case object Quadtree extends PrefixTree("quadtree")
+  val Geohash = PrefixTree("geohash")
+  val Quadtree = PrefixTree("quadtree")
 }
