89e2111ccf1c3c2b45d5f3b9dda560236bd6c3b1
sksamuel <sam@sksamuel.com>
2016-11-04 11:42:27 +0000

    Get cluster health DSL with parameters #600

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/admin/ClusterHealthDefinition.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/admin/ClusterHealthDefinition.scala
@@ -2,17 +2,20 @@ package com.sksamuel.elastic4s.admin
 
 import com.sksamuel.exts.OptionImplicits._
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequestBuilder
+import org.elasticsearch.cluster.health.ClusterHealthStatus
 import org.elasticsearch.common.Priority
 
 case class ClusterHealthDefinition(indices: Seq[String],
                                    timeout: Option[String] = None,
                                    waitForActiveShards: Option[Int] = None,
                                    waitForEvents: Option[Priority] = None,
+                                   waitForStatus: Option[ClusterHealthStatus] = None,
                                    waitForNodes: Option[String] = None) {
 
   def build(builder: ClusterHealthRequestBuilder): Unit = {
     builder.setIndices(indices: _*)
     timeout.foreach(builder.setTimeout)
+    waitForStatus.foreach(builder.setWaitForStatus)
     waitForNodes.foreach(builder.setWaitForNodes)
     waitForActiveShards.foreach(builder.setWaitForActiveShards)
     waitForEvents.foreach(builder.setWaitForEvents)
@@ -24,6 +27,9 @@ case class ClusterHealthDefinition(indices: Seq[String],
     copy(waitForActiveShards = waitForActiveShards.some)
 
   def waitForEvents(waitForEvents: Priority): ClusterHealthDefinition = copy(waitForEvents = waitForEvents.some)
+
+  def waitForStatus(waitForStatus: ClusterHealthStatus): ClusterHealthDefinition =
+    copy(waitForStatus = waitForStatus.some)
 
   def waitForNodes(waitForNodes: String): ClusterHealthDefinition = copy(waitForNodes = waitForNodes.some)
 }
