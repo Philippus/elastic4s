a7bbf7628016c784775167391b0b533914bcdaca
sksamuel <sam@sksamuel.com>
2016-11-06 21:16:42 +0000

    Added extra bool query test

--- elastic4s-core-tests/src/test/scala/com/sksamuel/elastic4s/search/BoolQueryTest.scala
+++ elastic4s-core-tests/src/test/scala/com/sksamuel/elastic4s/search/BoolQueryTest.scala
@@ -15,7 +15,10 @@ class BoolQueryTest extends FlatSpec with ElasticSugar with Matchers {
       indexInto("fonts/family").fields("name" -> "helvetica 2", "style" -> "serif"),
       indexInto("fonts/family").fields("name" -> "arial", "style" -> "serif"),
       indexInto("fonts/family").fields("name" -> "verdana", "style" -> "serif"),
-      indexInto("fonts/family").fields("name" -> "times new roman", "style" -> "serif")
+      indexInto("fonts/family").fields("name" -> "times new roman", "style" -> "serif"),
+      indexInto("fonts/family").fields("name" -> "roman comic", "style" -> "comic"),
+      indexInto("fonts/family").fields("name" -> "comic sans", "style" -> "comic")
+
     )
   }
 
@@ -39,7 +42,7 @@ class BoolQueryTest extends FlatSpec with ElasticSugar with Matchers {
       }
     }.await
 
-    resp.totalHits shouldBe 4
+    resp.totalHits shouldBe 5
     resp.hits.head.sourceField("style") shouldBe "serif"
   }
 
@@ -50,7 +53,21 @@ class BoolQueryTest extends FlatSpec with ElasticSugar with Matchers {
       }
     }.await
 
-    resp.totalHits shouldBe 1
-    resp.hits.head.sourceField("name") shouldBe "times new roman"
+    resp.totalHits shouldBe 2
+    resp.hits.map(_.sourceField("name")).toSet shouldBe Set("times new roman", "roman comic")
+  }
+
+  it should "support or using should" in {
+    val resp = client.execute {
+      search("fonts/family").query {
+        boolQuery().should(
+          matchPhraseQuery("name", "times new roman"),
+          matchPhraseQuery("name", "comic sans")
+        )
+      }
+    }.await
+
+    resp.totalHits shouldBe 2
+    resp.hits.map(_.sourceField("name")).toSet shouldBe Set("times new roman", "comic sans")
   }
 }
