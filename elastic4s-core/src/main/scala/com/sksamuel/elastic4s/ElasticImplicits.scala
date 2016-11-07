cbdfde6b87d7cb659df60bebf75a90369c7fa667
sksamuel <sam@sksamuel.com>
2016-11-07 19:59:06 +0000

    ADded string implicit for indexables. Added rich update dsl

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/ElasticImplicits.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/ElasticImplicits.scala
@@ -1,9 +1,15 @@
 package com.sksamuel.elastic4s
 
 trait ElasticImplicits {
+
   implicit class RichString(index: String) {
     def /(`type`: String): IndexAndType = IndexAndType(index, `type`)
     def /(types: Iterable[String]): IndexAndTypes = IndexAndTypes(index, types.toSeq)
+  }
+
+  // allows any string to be indexed as source
+  implicit object StringIndexable extends Indexable[String] {
+    override def json(js: String): String = js
   }
 }
 
--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/update/UpdateDsl.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/update/UpdateDsl.scala
@@ -1,7 +1,8 @@
 package com.sksamuel.elastic4s.update
 
 import com.sksamuel.elastic4s.searches.QueryDefinition
-import com.sksamuel.elastic4s.{Executable, IndexAndTypes, Indexes}
+import com.sksamuel.elastic4s.{DocumentRef, Executable, IndexAndTypes, Indexes}
+import org.elasticsearch.action.DocWriteResponse.Result
 import org.elasticsearch.action.update.UpdateResponse
 import org.elasticsearch.client.Client
 import org.elasticsearch.index.reindex.{BulkIndexByScrollResponse, UpdateByQueryAction}
@@ -21,9 +22,9 @@ trait UpdateDsl {
   }
 
   implicit object UpdateDefinitionExecutable
-    extends Executable[UpdateDefinition, UpdateResponse, UpdateResponse] {
-    override def apply(c: Client, t: UpdateDefinition): Future[UpdateResponse] = {
-      injectFuture(c.update(t.build, _))
+    extends Executable[UpdateDefinition, UpdateResponse, RichUpdateResponse] {
+    override def apply(c: Client, t: UpdateDefinition): Future[RichUpdateResponse] = {
+      injectFutureAndMap(c.update(t.build, _))(RichUpdateResponse.apply)
     }
   }
 
@@ -35,4 +36,18 @@ trait UpdateDsl {
       injectFuture(builder.execute)
     }
   }
+}
+
+case class RichUpdateResponse(original: UpdateResponse) {
+  def result = original.getResult
+  def get = original.getGetResult
+  def status = original.status()
+  def index = original.getIndex
+  def `type` = original.getType
+  def id = original.getId
+  def ref: DocumentRef = DocumentRef(index, `type`, id)
+  def shardId = original.getShardId
+  def shardInfo = original.getShardInfo
+  def version = original.getVersion
+  def created: Boolean = original.getResult == Result.CREATED
 }
