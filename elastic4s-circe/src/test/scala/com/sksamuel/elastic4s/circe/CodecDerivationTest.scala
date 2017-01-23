1c28c52c997beb23bd78a43c67424f40fce4401f
sksamuel <sam@sksamuel.com>
2017-01-23 03:21:39 +0000

    Fixed circe tests

--- elastic4s-circe/src/test/scala/com/sksamuel/elastic4s/circe/CodecDerivationTest.scala
+++ elastic4s-circe/src/test/scala/com/sksamuel/elastic4s/circe/CodecDerivationTest.scala
@@ -10,20 +10,20 @@ class CodecDerivationTest extends WordSpec with Matchers with GivenWhenThen {
   case class Place(id: Int, name: String)
   case class Cafe(name: String, place: Place)
 
-  "A derived HitAs instance" should {
+  "A derived HitReader instance" should {
 
     "be implicitly found if circe.generic.auto is in imported" in {
       """
         import io.circe.generic.auto._
-        import com.sksamuel.elastic4s.HitAs
-        implicitly[HitAs[Cafe]]
+        import com.sksamuel.elastic4s.HitReader
+        implicitly[HitReader[Cafe]]
       """ should compile
     }
 
     "not compile if no decoder is in scope" in {
       """
-        import com.sksamuel.elastic4s.HitAs
-        implicitly[HitAs[Cafe]]
+        import com.sksamuel.elastic4s.HitReader
+        implicitly[HitReader[Cafe]]
       """ shouldNot compile
     }
 
