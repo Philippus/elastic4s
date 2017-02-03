9952aa0436d15830d36ca8ee4a098f9e25dd7959
sksamuel <sam@sksamuel.com>
2017-02-03 19:24:36 +0000

    Added exists query support to http client

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/searches/queries/ExistsQueryDefinition.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/searches/queries/ExistsQueryDefinition.scala
@@ -4,6 +4,9 @@ case class ExistsQueryDefinition(field: String,
                                  boost: Option[Float] = None,
                                  queryName: Option[String] = None) extends QueryDefinition {
 
-  def withBoost(boost: Float) = copy(boost = Option(boost))
-  def withQueryName(queryName: String) = copy(queryName = Option(queryName))
+  def boost(boost: Float): ExistsQueryDefinition = copy(boost = Option(boost))
+  def withBoost(boost: Float): ExistsQueryDefinition = copy(boost = Option(boost))
+
+  def queryName(queryName: String): ExistsQueryDefinition = copy(queryName = Option(queryName))
+  def withQueryName(queryName: String): ExistsQueryDefinition = copy(queryName = Option(queryName))
 }
---
+++ elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/search/queries/ExistsQueryBodyFn.scala
@@ -0,0 +1,18 @@
+package com.sksamuel.elastic4s.http.search.queries
+
+import com.sksamuel.elastic4s.searches.queries.ExistsQueryDefinition
+import org.elasticsearch.common.xcontent.{XContentBuilder, XContentFactory}
+
+object ExistsQueryBodyFn {
+  def apply(q: ExistsQueryDefinition): XContentBuilder = {
+    val builder = XContentFactory.jsonBuilder()
+    builder.startObject()
+    builder.startObject("exists")
+    builder.field("field", q.field)
+    q.boost.foreach(builder.field("boost", _))
+    q.queryName.foreach(builder.field("_name", _))
+    builder.endObject()
+    builder.endObject()
+    builder
+  }
+}
