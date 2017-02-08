4922241c349527085109a049a02583d48eae6053
sksamuel <sam@sksamuel.com>
2017-02-08 07:03:15 +0000

    Improved `Show[SearchDefinition]` instance #735

--- elastic4s-tcp/src/main/scala/com/sksamuel/elastic4s/search/SearchImplicits.scala
+++ elastic4s-tcp/src/main/scala/com/sksamuel/elastic4s/search/SearchImplicits.scala
@@ -2,9 +2,16 @@ package com.sksamuel.elastic4s.search
 
 import cats.Show
 import com.sksamuel.elastic4s.Executable
+import com.sksamuel.elastic4s.script.{ScriptFieldDefinition, SortBuilderFn}
 import com.sksamuel.elastic4s.searches._
+import com.sksamuel.elastic4s.searches.highlighting.HighlightBuilderFn
 import org.elasticsearch.action.search.{MultiSearchResponse, SearchResponse}
 import org.elasticsearch.client.Client
+import org.elasticsearch.common.unit.TimeValue
+import org.elasticsearch.script.{Script, ScriptType}
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.SortBuilder
+import org.elasticsearch.search.suggest.SuggestBuilder
 
 import scala.concurrent.Future
 
@@ -36,7 +43,65 @@ trait SearchImplicits {
   }
 
   implicit object SearchDefinitionShow extends Show[SearchDefinition] {
-    override def show(f: SearchDefinition): String = f.toString
+    override def show(search: SearchDefinition): String = {
+      import scala.collection.JavaConverters._
+      val builder = new SearchSourceBuilder()
+      search.query.map(QueryBuilderFn.apply).foreach(builder.query)
+      search.minScore.map(_.toFloat).foreach(builder.minScore)
+      search.version.foreach(v => builder.version(v))
+      search.postFilter.map(QueryBuilderFn.apply).foreach(builder.postFilter)
+      search.explain.foreach(b => builder.explain(b))
+      search.from.foreach(builder.from)
+      search.size.foreach(builder.size)
+      search.trackScores.foreach(builder.trackScores)
+      search.terminateAfter.foreach(builder.terminateAfter)
+      search.timeout.map(dur => TimeValue.timeValueNanos(dur.toNanos)).foreach(builder.timeout)
+      search.indexBoosts.foreach { case (index, boost) => builder.indexBoost(index, boost.toFloat) }
+
+      if (search.storedFields.nonEmpty)
+        builder.storedFields(search.storedFields.asJava)
+
+      if (search.aggs.nonEmpty)
+        search.aggs.map(_.builder).foreach(builder.aggregation)
+
+      if (search.inners.nonEmpty) {
+        for (inner <- search.inners)
+          sys.error("todo")
+        // builder.(inner.name, inner.inner)
+      }
+
+      if (search.sorts.nonEmpty)
+        search.sorts.foreach { sort =>
+          builder.getClass.getMethod("addSort", classOf[SortBuilder[_]]).invoke(builder, SortBuilderFn.apply(sort))
+        }
+
+      if (search.scriptFields.nonEmpty)
+        search.scriptFields.foreach {
+          case ScriptFieldDefinition(name, script, None, None, _, ScriptType.INLINE) =>
+            builder.scriptField(name, new Script(script))
+          case ScriptFieldDefinition(name, script, lang, params, options, scriptType) =>
+            builder.scriptField(name, new Script(scriptType, lang.getOrElse(Script.DEFAULT_SCRIPT_LANG), script,
+              options.map(_.asJava).getOrElse(new java.util.HashMap()),
+              params.map(_.asJava).getOrElse(new java.util.HashMap())))
+        }
+      if (search.suggs.nonEmpty) {
+        val suggest = new SuggestBuilder()
+        search.suggs.foreach { sugg => suggest.addSuggestion(sugg.name, sugg.builder) }
+        builder.suggest(suggest)
+      }
+
+      search.highlight.foreach { highlight =>
+        val highlightBuilder = HighlightBuilderFn(highlight.options, highlight.fields.toSeq)
+        builder.highlighter(highlightBuilder)
+      }
+
+      search.rescorers.map(RescoreBuilderFn.apply).foreach(builder.addRescorer)
+
+      if (search.stats.nonEmpty)
+        builder.stats(search.stats.asJava)
+
+      builder.toString
+    }
   }
 
   implicit class SearchDefinitionShowOps(f: SearchDefinition) {
