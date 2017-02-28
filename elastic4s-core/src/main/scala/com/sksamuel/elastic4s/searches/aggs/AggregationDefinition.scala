883adb9a06276bcb4348f8970da506623ffcdb6a
sksamuel <sam@sksamuel.com>
2017-02-28 23:30:44 +0000

    Add subaggs as alias for subAggregations #768

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/searches/aggs/AggregationDefinition.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/searches/aggs/AggregationDefinition.scala
@@ -12,7 +12,11 @@ trait AggregationDefinition {
   def pipelines(first: PipelineAggregationDefinition, rest: PipelineAggregationDefinition*): T = pipelines(first +: rest)
   def pipelines(pipelines: Iterable[PipelineAggregationDefinition]): T
 
-  def subAggregation(agg: AggregationDefinition): T = subAggregation(agg)
+  def subagg(agg: AggregationDefinition): T = subaggs(agg)
+  def subaggs(first: AggregationDefinition, rest: AggregationDefinition*): T = subaggs(first +: rest)
+  def subaggs(aggs: Iterable[AggregationDefinition]): T = subAggregations(aggs)
+
+  def subAggregation(agg: AggregationDefinition): T = subAggregations(agg)
   def subAggregations(first: AggregationDefinition, rest: AggregationDefinition*): T = subAggregations(first +: rest)
   def subAggregations(aggs: Iterable[AggregationDefinition]): T
 
