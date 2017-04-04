5049451a393aa115e0bccbaf350d610fecf5bc1b
Sir Alejandro Sellero <alex1712@gmail.com>
2017-04-04 09:31:47 +0200

    Fixing the tests

--- elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/mappings/PutMappingApiTest.scala
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/mappings/PutMappingApiTest.scala
@@ -26,20 +26,24 @@ class PutMappingApiTest extends FlatSpec with Matchers with ElasticDsl with Dual
   }
 
   it should "accept same fields as mapping api" in {
-    putMapping("index" / "type").as(
-      dateField("content") nullValue "no content"
-    ) dynamic DynamicMapping.False numericDetection true boostNullValue 12.2 boostName "boosty"
+    execute {
+      putMapping("index" / "type").as(
+        dateField("content") nullValue "no content"
+      ) dynamic DynamicMapping.False numericDetection true boostNullValue 12.2
+    }.await
   }
 
   it should "accept same several new fields with different types as mapping api" in {
-    putMapping("index" / "type").as(
-      dateField("content") nullValue "no content",
-      textField("description") boost 1.5 maxInputLength 500,
-      doubleField("price") enabled false,
-      nestedField("children") fields(
-        textField("name") nullValue "no name",
-        dateField("date") nullValue "no date"
-      )
-    ) dynamic DynamicMapping.False numericDetection true boostNullValue 12.2 boostName "boosty"
+    execute {
+      putMapping("index" / "type").as(
+        dateField("content") nullValue "no content",
+        textField("description") boost 1.5 ,
+        doubleField("price"),
+        nestedField("children") fields(
+          textField("name"),
+          dateField("date") nullValue "no date"
+        )
+      ) dynamic DynamicMapping.False numericDetection true boostNullValue 12.2
+    }.await
   }
 }
