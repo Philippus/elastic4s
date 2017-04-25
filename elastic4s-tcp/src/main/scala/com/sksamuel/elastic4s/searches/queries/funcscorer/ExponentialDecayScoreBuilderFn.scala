c59d9746ca3e6ce1738bb39a3c797b0bab84f317
sksamuel <sam@sksamuel.com>
2017-04-25 01:07:44 +0100

    In exponential decay score the decay is disregarded when offset was not provided #883

--- elastic4s-tcp/src/main/scala/com/sksamuel/elastic4s/searches/queries/funcscorer/ExponentialDecayScoreBuilderFn.scala
+++ elastic4s-tcp/src/main/scala/com/sksamuel/elastic4s/searches/queries/funcscorer/ExponentialDecayScoreBuilderFn.scala
@@ -7,6 +7,7 @@ object ExponentialDecayScoreBuilderFn {
     val builder = (exp.offset, exp.decay) match {
       case (Some(o), Some(d)) => ScoreFunctionBuilders.exponentialDecayFunction(exp.field, exp.origin, exp.scale, o, d)
       case (Some(o), None) => ScoreFunctionBuilders.exponentialDecayFunction(exp.field, exp.origin, exp.scale, o)
+      case (None, Some(d)) => ScoreFunctionBuilders.exponentialDecayFunction(exp.field, exp.origin, exp.scale, null, d)
       case _ => ScoreFunctionBuilders.exponentialDecayFunction(exp.field, exp.origin, exp.scale)
     }
     exp.weight.map(_.toFloat).foreach(builder.setWeight)
