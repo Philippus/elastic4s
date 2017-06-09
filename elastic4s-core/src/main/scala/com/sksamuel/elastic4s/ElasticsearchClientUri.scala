7dddf9d40bed21bbd8aba67a0bbab6f24dec629e
sksamuel <sam@sksamuel.com>
2017-06-09 03:37:14 +0100

    Connecting to Elasticsearch using HTTPS #932

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/ElasticsearchClientUri.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/ElasticsearchClientUri.scala
@@ -11,8 +11,8 @@ object ElasticsearchClientUri {
   implicit def stringtoUri(str: String): ElasticsearchClientUri = ElasticsearchClientUri(str)
 
   /**
-   * Creates an ElasticsearchClientUri from a single host and port with no options.
-   */
+    * Creates an ElasticsearchClientUri from a single host and port with no options.
+    */
   def apply(host: String, port: Int): ElasticsearchClientUri = apply(s"elasticsearch://$host:$port")
 
   def apply(str: String): ElasticsearchClientUri = {
@@ -36,11 +36,13 @@ object ElasticsearchClientUri {
 }
 
 /**
-* Uri used to connect to an Elasticsearch cluster. The general format is
-*
-* elasticsearch://host:port,host:port?querystring
-*
-* Multiple host:port combinations can be specified, seperated by commas.
-* Options can be specified using standard uri query string syntax, eg cluster.name=superman
-*/
+  * Uri used to connect to an Elasticsearch cluster. The general format is
+  *
+  * elasticsearch://host:port,host:port?querystring
+  *
+  * Multiple host:port combinations can be specified, seperated by commas.
+  * Options can be specified using standard uri query string syntax, eg cluster.name=superman
+  *
+  * To use HTTPS when using the HTTP client, add ssl=true to the query parameters.
+  */
 case class ElasticsearchClientUri(uri: String, hosts: List[(String, Int)], options: Map[String, String] = Map.empty)
