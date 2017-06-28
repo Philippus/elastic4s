11371082f66c1af52d8bcacaa7a7b23990d8e9c1
Remco Beckers <remco.beckers@gmail.com>
2017-06-28 17:24:56 +0200

    Fix createTemplate json serialization

--- elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/indexes/CreateIndexTemplateDefinitionTest.scala
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/indexes/CreateIndexTemplateDefinitionTest.scala
@@ -1,19 +1,53 @@
 package com.sksamuel.elastic4s.indexes
 
-import org.scalatest.WordSpec
+import com.sksamuel.elastic4s.ElasticApi
+import com.sksamuel.elastic4s.ElasticDsl.{intField, keywordField}
+import com.sksamuel.elastic4s.http.ElasticDsl
+import com.sksamuel.elastic4s.testkit.DiscoveryLocalNodeProvider
+import org.scalatest.{BeforeAndAfter, Matchers, WordSpec}
 
-class CreateIndexTemplateDefinitionTest extends WordSpec {
+import scala.util.Try
 
-  import com.sksamuel.elastic4s.ElasticDsl._
+class CreateIndexTemplateDefinitionTest extends WordSpec
+  with Matchers
+  with BeforeAndAfter
+  with ElasticApi
+  with ElasticDsl
+  with DiscoveryLocalNodeProvider  {
 
-  private val req = createTemplate("my_template").pattern("matchme.*").mappings(
-    mapping("sometype1").fields(
-      keywordField("field1"),
-      geopointField("field2")
-    ),
-    mapping("sometype2").fields(
-      keywordField("field3"),
-      intField("field4")
-    )
-  )
+  before {
+    Try {
+      http.execute {
+        deleteIndex("matchme.template")
+      }.await
+    }
+  }
+
+  "Create Index Template HTTP request" should {
+    "create and use the template for an index" in {
+      http.execute {
+        createTemplate("matchme.*").pattern("matchme.*").mappings(
+          mapping("sometype1").fields(
+            keywordField("field1"),
+            geopointField("field2"),
+            keywordField("field3"),
+            intField("field4")
+          )
+        )
+      }.await
+
+      http.execute {
+        createIndex("matchme.template").shards(1).waitForActiveShards(1)
+      }.await
+
+      val resp = http.execute {
+        getMapping("matchme.template")
+      }.await
+
+      resp.map(_.index) shouldBe Seq("matchme.template")
+      resp.head.mappings.keySet shouldBe Set("sometype1")
+      resp.head.mappings("sometype1").keySet shouldBe Set("field1", "field2", "field3", "field4")
+    }
+  }
+
 }
