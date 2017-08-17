f242f1fe987c4d199c20dd1d8421a90ffaca195d
sksamuel <sam@sksamuel.com>
2017-08-17 20:15:48 +0100

    Flaky error with ElasticSugar #990

--- elastic4s-testkit/src/main/scala/com/sksamuel/elastic4s/testkit/providers.scala
+++ elastic4s-testkit/src/main/scala/com/sksamuel/elastic4s/testkit/providers.scala
@@ -8,7 +8,7 @@ import com.sksamuel.elastic4s.TcpClient
 import com.sksamuel.elastic4s.embedded.{LocalNode, RemoteLocalNode}
 import com.sksamuel.elastic4s.http.HttpClient
 
-import scala.util.Random
+import scala.util.{Failure, Random, Success, Try}
 import scala.util.control.NonFatal
 
 // LocalNodeProvider provides helper methods to create a local (embedded) node
@@ -24,21 +24,37 @@ trait LocalNodeProvider {
 // implementation of LocalNodeProvider that uses a single
 // node instance for all classes in the same thread classloader.
 trait ClassloaderLocalNodeProvider extends LocalNodeProvider {
-  override def getNode: LocalNode = ClassloaderLocalNode.node
+
+  private lazy val dir: Path  = Paths get System.getProperty("java.io.tmpdir")
+  private lazy val home: Path = dir resolve UUID.randomUUID().toString
+
+  override lazy val getNode: LocalNode = localNode(0)
+
+  private def localNode(timesTried: Int): LocalNode =
+    Try(LocalNode("node_" + ClassLocalNodeProvider.counter.getAndIncrement(), home.toAbsolutePath.toString)) match {
+      case Failure(ex: IllegalStateException) if timesTried < 5 =>
+        ex.printStackTrace()
+        localNode(timesTried + 1)
+      case Success(okNode) => okNode
+      case Failure(other)  => throw other
+    }
 }
 
 // implementation of LocalNodeProvider that attempts to find local nodes already started
 // and then connects to that, or creates a new one if one cannot be found
 trait DiscoveryLocalNodeProvider extends LocalNodeProvider {
+
   override def getNode: LocalNode = {
 
     try {
+
       // assume the local node is running on 9200
       val client = HttpClient("elasticsearch://localhost:9200")
       import com.sksamuel.elastic4s.http.ElasticDsl._
       val nodeinfo = client.execute(nodeInfo()).await
       val (id, node) = nodeinfo.nodes.head
       println(s"Found local node $id")
+
       val paths = node.settingsAsMap("path").asInstanceOf[Map[String, AnyRef]]
       val pathData = Paths get paths("data").toString
       val pathHome = Paths get paths("home").toString
@@ -48,9 +64,19 @@ trait DiscoveryLocalNodeProvider extends LocalNodeProvider {
     } catch {
       case NonFatal(e) =>
         println(s"Creating new local node")
+
         val tempDirectoryPath: Path = Paths get System.getProperty("java.io.tmpdir")
         val pathHome: Path = tempDirectoryPath resolve UUID.randomUUID().toString
-        val node = LocalNode("localnode-cluster", pathHome.toAbsolutePath.toString)
+
+        def createLocalNode(timesTried: Int): LocalNode = {
+          Try(LocalNode("localnode-cluster", pathHome.toAbsolutePath.toString)) match {
+            case Failure(_: IllegalStateException) if timesTried < 5 => createLocalNode(timesTried + 1)
+            case Success(okNode) => okNode
+            case Failure(other) => throw other
+          }
+        }
+
+        val node = createLocalNode(0)
         // allow time for the node to start up
         Thread.sleep(3000)
         node
