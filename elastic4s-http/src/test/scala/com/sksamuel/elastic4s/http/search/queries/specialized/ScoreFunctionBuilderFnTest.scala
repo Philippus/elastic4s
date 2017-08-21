73836f58221c0bfdafa530d3e647cc0828dff61f
Brett Christensen <bchristensen@seek.com.au>
2017-08-21 21:50:31 +0200

    fix path to test data

--- elastic4s-http/src/test/scala/com/sksamuel/elastic4s/http/search/queries/specialized/ScoreFunctionBuilderFnTest.scala
+++ elastic4s-http/src/test/scala/com/sksamuel/elastic4s/http/search/queries/specialized/ScoreFunctionBuilderFnTest.scala
@@ -12,28 +12,28 @@ class ScoreFunctionBuilderFnTest extends FlatSpec with JsonSugar {
     val req = linearScore("myfield", "1 2", "2km").offset("200m").decay(0.1).weight(0.1).multiValueMode(MultiValueMode.Min)
     val builder = ScoreFunctionBuilderFn(req)
     val actual = builder.string()
-    actual should matchJsonResource("/json/score/score_linear.json")
+    actual should matchJsonResource("/score_linear.json")
   }
 
   it should "generate correct json for a gaussian decay function scorer" in {
     val req = gaussianScore("myfield", "1 2", "3km").offset("1km").decay(0.2).weight(0.2).multiValueMode(MultiValueMode.Min)
     val builder = ScoreFunctionBuilderFn(req)
     val actual = builder.string()
-    actual should matchJsonResource("/json/score/score_gaussian.json")
+    actual should matchJsonResource("/score_gaussian.json")
   }
 
   it should "generate correct json for an exponential decay function scorer" in {
     val req = exponentialScore("myfield", "1 2", "4km").offset("100m").decay(0.4).weight(0.3).multiValueMode(MultiValueMode.Min)
     val builder = ScoreFunctionBuilderFn(req)
     val actual = builder.string()
-    actual should matchJsonResource("/json/score/score_exponential.json")
+    actual should matchJsonResource("/score_exponential.json")
   }
 
   it should "generate correct json for a random function scorer" in {
     val req = randomScore(12345)
     val builder = ScoreFunctionBuilderFn(req)
     val actual = builder.string()
-    actual should matchJsonResource("/json/score/score_random.json")
+    actual should matchJsonResource("/score_random.json")
   }
 
   it should "generate correct json for a script scorer" in {
@@ -42,20 +42,20 @@ class ScoreFunctionBuilderFnTest extends FlatSpec with JsonSugar {
     }.weight(0.4)
     val builder = ScoreFunctionBuilderFn(req)
     val actual = builder.string()
-    actual should matchJsonResource("/json/score/score_script.json")
+    actual should matchJsonResource("/score_script.json")
   }
 
   it should "generate correct json for a weight function scorer" in {
     val req = weightScore(1.5)
     val builder = ScoreFunctionBuilderFn(req)
     val actual = builder.string()
-    actual should matchJsonResource("/json/score/score_weight.json")
+    actual should matchJsonResource("/score_weight.json")
   }
 
   it should "generate correct json for a field value factor function scorer" in {
     val req = fieldFactorScore("likes").factor(1.2).modifier(FieldValueFactorFunctionModifier.SQRT).missing(1.0)
     val builder = ScoreFunctionBuilderFn(req)
     val actual = builder.string()
-    actual should matchJsonResource("/json/score/score_fieldvaluefactor.json")
+    actual should matchJsonResource("/score_fieldvaluefactor.json")
   }
 }
