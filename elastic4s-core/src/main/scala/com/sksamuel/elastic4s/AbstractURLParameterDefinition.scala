0191543547b94b6ff42cb546f517a1a7a4cc08a4
kimxogus <kgyoo8232@gmail.com>
2017-09-09 20:43:56 +0900

    add BaseURLParameterDefinition

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/AbstractURLParameterDefinition.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/AbstractURLParameterDefinition.scala
@@ -4,9 +4,39 @@ import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
 
 import scala.concurrent.duration.FiniteDuration
 
-abstract class AbstractURLParameterDefinition(val timeout: Option[FiniteDuration] = None,
-                                              val refresh: Option[RefreshPolicy] = None,
-                                              val requestsPerSecond: Option[Float] = None,
-                                              val waitForActiveShards: Option[Int] = None,
-                                              val waitForCompletion: Option[Boolean] = None
-                                             )
+trait AbstractURLParameterDefinition {
+  def timeout: Option[FiniteDuration] = None
+
+  def refresh: Option[RefreshPolicy] = None
+
+  def requestsPerSecond: Option[Float] = None
+
+  def waitForActiveShards: Option[Int] = None
+
+  def waitForCompletion: Option[Boolean] = None
+}
+
+case class BaseURLParameterDefinition(override val timeout: Option[FiniteDuration] = None,
+                                      override val refresh: Option[RefreshPolicy] = None,
+                                      override val requestsPerSecond: Option[Float] = None,
+                                      override val waitForActiveShards: Option[Int] = None,
+                                      override val waitForCompletion: Option[Boolean] = None)
+  extends AbstractURLParameterDefinition {
+
+  import com.sksamuel.exts.OptionImplicits._
+
+  def timeout(timeout: FiniteDuration): BaseURLParameterDefinition =
+    copy(timeout = timeout.some)
+
+  def refresh(refresh: RefreshPolicy): BaseURLParameterDefinition =
+    copy(refresh = refresh.some)
+
+  def requestsPerSecond(requestsPerSecond: Float): BaseURLParameterDefinition =
+    copy(requestsPerSecond = requestsPerSecond.some)
+
+  def waitForActiveShards(waitForActiveShards: Int): BaseURLParameterDefinition =
+    copy(waitForActiveShards = waitForActiveShards.some)
+
+  def waitForCompletion(waitForCompletion: Boolean): BaseURLParameterDefinition =
+    copy(waitForCompletion = waitForCompletion.some)
+}
