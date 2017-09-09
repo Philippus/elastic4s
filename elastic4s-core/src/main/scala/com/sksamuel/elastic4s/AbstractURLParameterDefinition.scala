2d1a51e9e7bf6e5d0b9385443bfd5d0011bba157
kimxogus <kgyoo8232@gmail.com>
2017-09-09 20:48:51 +0900

    use abstract class

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/AbstractURLParameterDefinition.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/AbstractURLParameterDefinition.scala
@@ -4,17 +4,13 @@ import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
 
 import scala.concurrent.duration.FiniteDuration
 
-trait AbstractURLParameterDefinition {
-  def timeout: Option[FiniteDuration] = None
 
-  def refresh: Option[RefreshPolicy] = None
-
-  def requestsPerSecond: Option[Float] = None
-
-  def waitForActiveShards: Option[Int] = None
-
-  def waitForCompletion: Option[Boolean] = None
-}
+abstract class AbstractURLParameterDefinition(val timeout: Option[FiniteDuration] = None,
+                                              val refresh: Option[RefreshPolicy] = None,
+                                              val requestsPerSecond: Option[Float] = None,
+                                              val waitForActiveShards: Option[Int] = None,
+                                              val waitForCompletion: Option[Boolean] = None
+                                             )
 
 case class BaseURLParameterDefinition(override val timeout: Option[FiniteDuration] = None,
                                       override val refresh: Option[RefreshPolicy] = None,
@@ -22,8 +18,6 @@ case class BaseURLParameterDefinition(override val timeout: Option[FiniteDuratio
                                       override val waitForActiveShards: Option[Int] = None,
                                       override val waitForCompletion: Option[Boolean] = None)
   extends AbstractURLParameterDefinition {
-
-  import com.sksamuel.exts.OptionImplicits._
 
   def timeout(timeout: FiniteDuration): BaseURLParameterDefinition =
     copy(timeout = timeout.some)
