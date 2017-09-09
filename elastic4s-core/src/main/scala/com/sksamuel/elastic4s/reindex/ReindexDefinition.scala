c5e8faecca57785f475c08f4d29ae2041d742464
kimxogus <kgyoo8232@gmail.com>
2017-09-09 21:25:53 +0900

    methods for urlParams values

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/reindex/ReindexDefinition.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/reindex/ReindexDefinition.scala
@@ -4,6 +4,7 @@ import com.sksamuel.elastic4s.script.ScriptDefinition
 import com.sksamuel.elastic4s.searches.queries.QueryDefinition
 import com.sksamuel.elastic4s.{Indexes, URLParameters}
 import com.sksamuel.exts.OptionImplicits._
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -18,6 +19,23 @@ case class ReindexDefinition(sourceIndexes: Indexes,
                              script: Option[ScriptDefinition] = None,
                              urlParams: Option[URLParameters] = None
                             ) {
+
+  private def getURLParams = urlParams.getOrElse(URLParameters())
+
+  def timeout(timeout: FiniteDuration): ReindexDefinition =
+    copy(urlParams = getURLParams.timeout(timeout).some)
+
+  def refresh(refresh: RefreshPolicy): ReindexDefinition =
+    copy(urlParams = getURLParams.refresh(refresh).some)
+
+  def requestsPerSecond(requestsPerSecond: Float): ReindexDefinition =
+    copy(urlParams = getURLParams.requestsPerSecond(requestsPerSecond).some)
+
+  def waitForActiveShards(waitForActiveShards: Int): ReindexDefinition =
+    copy(urlParams = getURLParams.waitForActiveShards(waitForActiveShards).some)
+
+  def waitForCompletion(waitForCompletion: Boolean): ReindexDefinition =
+    copy(urlParams = getURLParams.waitForCompletion(waitForCompletion).some)
 
   def urlParams(urlParams: URLParameters): ReindexDefinition = copy(urlParams = urlParams.some)
 
