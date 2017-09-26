96861ccacc4e4457010ef2d1fbfe983a8d18e86c
Galen Meurer <galen.meurer@mdinsider.com>
2017-09-26 10:42:45 -0600

    Add InnerHit.sort property to retrieve calculated sort score for sorted inner hits

--- elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/search/responses.scala
+++ elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/search/responses.scala
@@ -47,7 +47,8 @@ case class SearchHit(@JsonProperty("_id") id: String,
             nested = hits.get("_nested").map(_.asInstanceOf[Map[String, AnyRef]]).getOrElse(Map.empty),
             score = hits("_score").asInstanceOf[Double],
             source = hits("_source").asInstanceOf[Map[String, AnyRef]],
-            highlight = hits.get("highlight").map(_.asInstanceOf[Map[String, Seq[String]]]).getOrElse(Map.empty)
+            highlight = hits.get("highlight").map(_.asInstanceOf[Map[String, Seq[String]]]).getOrElse(Map.empty),
+            sort = hits.get("sort").map(_.asInstanceOf[Seq[Double]]).getOrElse(Seq.empty)
           )
         }
       )
@@ -69,7 +70,8 @@ case class InnerHits(total: Int,
 case class InnerHit(nested: Map[String, AnyRef],
                     score: Double,
                     source: Map[String, AnyRef],
-                    highlight: Map[String, Seq[String]])
+                    highlight: Map[String, Seq[String]],
+                    sort: Seq[Double])
 
 case class SearchResponse(took: Int,
                           @JsonProperty("timed_out") isTimedOut: Boolean,
