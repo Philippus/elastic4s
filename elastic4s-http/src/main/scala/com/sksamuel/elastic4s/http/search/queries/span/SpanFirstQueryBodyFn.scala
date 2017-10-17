2a7e87260e083589a03876bfcbc1c0a1b12d13c9
Catalin Ursachi <catalin.ursachi@softwire.com>
2017-10-17 17:15:20 +0100

    Fix SpanFirstQueryBodyFn

--- elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/search/queries/span/SpanFirstQueryBodyFn.scala
+++ elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/search/queries/span/SpanFirstQueryBodyFn.scala
@@ -7,7 +7,9 @@ import com.sksamuel.elastic4s.searches.queries.span.SpanFirstQueryDefinition
 object SpanFirstQueryBodyFn {
   def apply(q: SpanFirstQueryDefinition): XContentBuilder = {
     val builder = XContentFactory.jsonBuilder()
-    builder.rawField("span_first", QueryBuilderFn(q.query))
+    builder.startObject("span_first")
+    builder.rawField("match", QueryBuilderFn(q.query))
+    builder.field("end", q.end)
     q.boost.foreach(builder.field("boost", _))
     q.queryName.foreach(builder.field("_name", _))
     builder.endObject()
