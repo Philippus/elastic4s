1af4690038c2cd5f7c2cc610425043fd27e058d5
sksamuel <sam@sksamuel.com>
2017-10-31 20:40:28 +0000

    Bumped elasticsearch to 6 rc2

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object Build extends AutoPlugin {
     val CatsVersion = "1.0.0-MF"
     val CirceVersion = "0.9.0-M1"
     val CommonsIoVersion = "2.4"
-    val ElasticsearchVersion = "6.0.0-rc1"
+    val ElasticsearchVersion = "6.0.0-rc2"
     val ExtsVersion = "1.57.1"
     val JacksonVersion = "2.9.1"
     val Json4sVersion = "3.5.3"
