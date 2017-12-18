1d311c3490ef5cd7b4c499bd93bae2c1d8bfc3a0
Rune Barikmo <rune.barikmo@gmail.com>
2017-12-18 19:29:50 +0100

    Refactored geo shape query model to support multiple geo json shapes

---
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/searches/queries/geo/Shapes.scala
@@ -0,0 +1,11 @@
+package com.sksamuel.elastic4s.searches.queries.geo
+
+import com.sksamuel.elastic4s.DistanceUnit
+import com.sksamuel.elastic4s.searches.GeoPoint
+
+object Shapes {
+
+  case class Polygon(points: Seq[GeoPoint], holes: Option[Seq[GeoPoint]])
+
+  case class Circle(point: GeoPoint, distance: (Double, DistanceUnit))
+}
