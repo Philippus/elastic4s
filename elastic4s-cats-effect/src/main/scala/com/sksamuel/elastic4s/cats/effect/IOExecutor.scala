7704c7df407ac86bba2243a07785b9688fb004c9
sksamuel <sam@sksamuel.com>
2018-02-04 11:46:45 +0000

    Added Scalaz Task impl of executor

--- elastic4s-cats-effect/src/main/scala/com/sksamuel/elastic4s/cats/effect/IOExecutor.scala
+++ elastic4s-cats-effect/src/main/scala/com/sksamuel/elastic4s/cats/effect/IOExecutor.scala
@@ -3,7 +3,7 @@ package com.sksamuel.elastic4s.cats.effect
 import cats.effect.IO
 import com.sksamuel.elastic4s.http.{ElasticRequest, Executor, HttpClient, HttpResponse}
 
-class CatsIOExecutor extends Executor[IO] {
+class IOExecutor extends Executor[IO] {
   override def exec(client: HttpClient, request: ElasticRequest): IO[HttpResponse] =
     IO.async[HttpResponse] { k =>
       client.send(request, k)
