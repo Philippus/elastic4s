66ad2d622a75138d141df55e183f991081d1f31e
sksamuel <sam@sksamuel.com>
2018-02-04 11:48:49 +0000

    Added monix Task impl of executor

---
+++ elastic4s-monix/src/main/scala/com/sksamuel/elastic4s/monix/TaskExecutor.scala
@@ -0,0 +1,17 @@
+package com.sksamuel.elastic4s.monix
+
+import com.sksamuel.elastic4s.http.{ElasticRequest, Executor, HttpClient, HttpResponse}
+import monix.eval.Task
+import monix.execution.Cancelable
+
+class TaskExecutor extends Executor[Task] {
+  override def exec(client: HttpClient, request: ElasticRequest): Task[HttpResponse] = {
+    Task.async { case (_, callback) =>
+      client.send(request, {
+        case Left(t) => callback.onError(t)
+        case Right(v) => callback.onSuccess(v)
+      })
+      Cancelable.empty
+    }
+  }
+}
