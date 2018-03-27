bf97e6cffac879ffe81d149ca185037c525420ae
lucaagostini <agostini.luca@gmail.com>
2018-03-27 17:32:09 +0200

    Add support for custom auth chain

--- elastic4s-aws/src/main/scala/com/sksamuel/elastic4s/aws/Aws4ElasticClient.scala
+++ elastic4s-aws/src/main/scala/com/sksamuel/elastic4s/aws/Aws4ElasticClient.scala
@@ -1,13 +1,13 @@
 package com.sksamuel.elastic4s.aws
 
+import com.amazonaws.auth.{AWSCredentialsProvider, AWSStaticCredentialsProvider, BasicAWSCredentials, DefaultAWSCredentialsProviderChain}
+import com.amazonaws.regions.DefaultAwsRegionProviderChain
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials, DefaultAWSCredentialsProviderChain}
-
-import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
 import org.apache.http.protocol.HttpContext
 import org.apache.http.{HttpRequest, HttpRequestInterceptor}
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback
 
 case class Aws4ElasticConfig(endpoint: String, key: String, secret: String, region: String, service: String = "es") {
   require(key.length > 16 && key.length < 128 && key.matches("[\\w]+"), "Key id must be between 16 and 128 characters.")
@@ -20,7 +20,7 @@ object Aws4ElasticClient {
     */
   def apply(config: Aws4ElasticConfig): HttpClient = {
     val elasticUri = ElasticsearchClientUri(config.endpoint)
-    HttpClient(elasticUri, httpClientConfigCallback = new SignedClientConfig(config))
+    HttpClient(elasticUri, httpClientConfigCallback = new SignedClient(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.key, config.secret)), config.region))
   }
 
   /**
@@ -29,38 +29,26 @@ object Aws4ElasticClient {
     */
   def apply(endpoint: String): HttpClient = {
     val elasticUri = ElasticsearchClientUri(endpoint)
-    HttpClient(elasticUri, httpClientConfigCallback = new DefaultSignedClientConfig)
+    HttpClient(elasticUri, httpClientConfigCallback = new SignedClient(new DefaultAWSCredentialsProviderChain, (new DefaultAwsRegionProviderChain).getRegion))
+  }
+
+  /**
+    * Creates ES HttpClient with custom AWS credential and region providers.
+    */
+  def apply(endpoint: String, credentialProvider: AWSCredentialsProvider, region: String): HttpClient = {
+    val elasticUri = ElasticsearchClientUri(endpoint)
+    HttpClient(elasticUri, httpClientConfigCallback = new SignedClient(credentialProvider, region))
   }
 
 }
 
-private class SignedClientConfig(config: Aws4ElasticConfig) extends HttpClientConfigCallback {
+private class SignedClient(chainProvider: AWSCredentialsProvider, region: String) extends HttpClientConfigCallback {
   override def customizeHttpClient(httpClientBuilder: HttpAsyncClientBuilder): HttpAsyncClientBuilder =
-    httpClientBuilder.addInterceptorLast(new Aws4HttpRequestInterceptor(config))
+    httpClientBuilder.addInterceptorLast(new Aws4HttpRequestInterceptor(chainProvider, region))
 }
 
-private class DefaultSignedClientConfig extends HttpClientConfigCallback {
-  override def customizeHttpClient(httpClientBuilder: HttpAsyncClientBuilder): HttpAsyncClientBuilder =
-    httpClientBuilder.addInterceptorLast(new DefaultAws4HttpRequestInterceptor)
-}
-
-private class Aws4HttpRequestInterceptor(config: Aws4ElasticConfig) extends HttpRequestInterceptor {
-  private val chainProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.key, config.secret))
-  private val signer        = new Aws4RequestSigner(chainProvider, config.region, config.service)
-
-  override def process(request: HttpRequest, context: HttpContext): Unit = signer.withAws4Headers(request)
-
-}
-
-/**
-  * Default Request Interceptor for convenience. Uses the default environment variable names.
-  * See <a href="http://docs.aws.amazon.com/cli/latest/userguide/cli-environment.html">amazon environment variable documentation</a>
-  *
-  */
-private class DefaultAws4HttpRequestInterceptor extends HttpRequestInterceptor {
-  private val defaultChainProvider = new DefaultAWSCredentialsProviderChain
-  private val region               = sys.env("AWS_DEFAULT_REGION")
-  private val signer               = new Aws4RequestSigner(defaultChainProvider, region)
+private class Aws4HttpRequestInterceptor(credentialProvider: AWSCredentialsProvider, region: String) extends HttpRequestInterceptor {
+  private val signer = new Aws4RequestSigner(credentialProvider, region)
 
   override def process(request: HttpRequest, context: HttpContext): Unit = signer.withAws4Headers(request)
 
