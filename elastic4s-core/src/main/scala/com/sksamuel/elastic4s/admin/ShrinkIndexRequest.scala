21acd57205ec213b7439b39df8ccb16f501a5539
Tom DeVoe <tdevoe@dataminr.com>
2018-06-08 12:37:24 -0400

    Change shrink index settings copy builder to use param

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/admin/ShrinkIndexRequest.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/admin/ShrinkIndexRequest.scala
@@ -5,5 +5,5 @@ case class ShrinkIndexRequest(source: String,
                               waitForActiveShards: Option[Int] = None,
                               settings: Map[String, String] = Map.empty) {
 
-  def settings(map: Map[String, String]): ShrinkIndexRequest = copy(settings = settings)
+  def settings(map: Map[String, String]): ShrinkIndexRequest = copy(settings = map)
 }
