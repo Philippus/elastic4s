15e5dc4bee6668dc10e75decf6895b384f06a1f5
TzeKei Lee <chikei@gmail.com>
2018-06-27 15:38:34 +0800

    Add handler for ClusterSettings (#1389)

---
+++ elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/cluster/ClusterBodyBuilderFn.scala
@@ -0,0 +1,22 @@
+package com.sksamuel.elastic4s.http.cluster
+
+import com.sksamuel.elastic4s.cluster.ClusterSettingsRequest
+import com.sksamuel.elastic4s.json.{XContentBuilder, XContentFactory}
+
+object ClusterBodyBuilderFn {
+  def apply(request: ClusterSettingsRequest): XContentBuilder = {
+    val builder = XContentFactory.jsonBuilder()
+    if(request.persistentSettings.nonEmpty) {
+      builder.startObject("persistent")
+      request.persistentSettings.foreach(t ⇒ builder.field(t._1, t._2))
+      builder.endObject()
+    }
+
+    if(request.transientSettings.nonEmpty) {
+      builder.startObject("transient")
+      request.transientSettings.foreach(t ⇒ builder.field(t._1, t._2))
+      builder.endObject()
+    }
+    builder
+  }
+}
