09e7aedfc8051a618a2ca6cba4293f32569fb1dc
Ches Martin <ches@whiskeyandgrits.net>
2018-07-12 22:10:51 +0700

    Stopgap Doc Updates (#1456)
    * Remove samples for TcpClient
    
    There's probably no sense showing and maintaining examples for
    deprecated code.
    
    * Update samples for 6.3.3
    
    * Update some README documentation for current release/master

--- samples/elastic4s-http-client-sbt/build.sbt
+++ samples/elastic4s-http-client-sbt/build.sbt
@@ -1,6 +1,6 @@
 lazy val root = Project("elastic4s-http-client-sbt", file("."))
   .settings(name := "elastic4s-http-client-sbt")
-  .settings(scalaVersion := "2.12.2")
+  .settings(scalaVersion := "2.12.6")
   .settings(libraryDependencies ++= Seq(
-    "com.sksamuel.elastic4s" %% "elastic4s-http" % "5.4.2"
+    "com.sksamuel.elastic4s" %% "elastic4s-http" % "6.3.3"
   ))
--- samples/elastic4s-http-client-sbt/src/main/scala/com/sksamuel/elastic4s/samples/HttpClientExampleApp.scala
+++ samples/elastic4s-http-client-sbt/src/main/scala/com/sksamuel/elastic4s/samples/HttpClientExampleApp.scala
@@ -1,30 +1,30 @@
 package com.sksamuel.elastic4s.samples
 
-import com.sksamuel.elastic4s.ElasticsearchClientUri
-import com.sksamuel.elastic4s.http.HttpClient
+import com.sksamuel.elastic4s.RefreshPolicy
+import com.sksamuel.elastic4s.http.{ElasticClient, ElasticProperties}
+import com.sksamuel.elastic4s.http.Response
 import com.sksamuel.elastic4s.http.search.SearchResponse
-import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
 
 object HttpClientExampleApp extends App {
 
   // you must import the DSL to use the syntax helpers
   import com.sksamuel.elastic4s.http.ElasticDsl._
 
-  val client = HttpClient(ElasticsearchClientUri("localhost", 9200))
+  val client = ElasticClient(ElasticProperties("http://localhost:9200"))
 
   client.execute {
     bulk(
       indexInto("myindex" / "mytype").fields("country" -> "Mongolia", "capital" -> "Ulaanbaatar"),
       indexInto("myindex" / "mytype").fields("country" -> "Namibia", "capital" -> "Windhoek")
-    ).refresh(RefreshPolicy.WAIT_UNTIL)
+    ).refresh(RefreshPolicy.WaitFor)
   }.await
 
-  val result: SearchResponse = client.execute {
+  val response: Response[SearchResponse] = client.execute {
     search("myindex").matchQuery("capital", "ulaanbaatar")
   }.await
 
   // prints out the original json
-  println(result.hits.hits.head.sourceAsString)
+  println(response.result.hits.hits.head.sourceAsString)
 
   client.close()
 
