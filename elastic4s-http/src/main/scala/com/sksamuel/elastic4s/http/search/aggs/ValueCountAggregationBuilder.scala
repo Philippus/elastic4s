0b525201a8915c7b7df96c5ca6c4fe5defe9f81c
Paul Lavery <paul.lavery@contentsquare.com>
2018-08-21 10:13:48 +0200

    Moved ValueCountAggregationBuilder to a dedicated file

--- elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/search/aggs/ValueCountAggregationBuilder.scala
+++ elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/search/aggs/ValueCountAggregationBuilder.scala
@@ -1,16 +1,15 @@
 package com.sksamuel.elastic4s.http.search.aggs
 
 import com.sksamuel.elastic4s.http.ScriptBuilderFn
-import com.sksamuel.elastic4s.searches.aggs.SumAggregationDefinition
+import com.sksamuel.elastic4s.searches.aggs.ValueCountAggregationDefinition
 import org.elasticsearch.common.xcontent.{XContentBuilder, XContentFactory, XContentType}
 
-object SumAggregationBuilder {
-  def apply(agg: SumAggregationDefinition): XContentBuilder = {
+object ValueCountAggregationBuilder {
+  def apply(agg: ValueCountAggregationDefinition): XContentBuilder = {
     val builder = XContentFactory.jsonBuilder()
     builder.startObject()
-    builder.startObject("sum")
+    builder.startObject("value_count")
     agg.field.foreach(builder.field("field", _))
-    agg.missing.foreach(builder.field("missing", _))
     agg.script.foreach { script =>
       builder.rawField("script", ScriptBuilderFn(script).bytes, XContentType.JSON)
     }
