d517b9701891cd0966c6bebded2c8826ec6d4cd9
Dmitrii_Naumenko <dmitrii_naumenko@epam.com>
2018-10-08 15:55:00 +0300

    Fixed bug with extra zero bytes, simplified logic of building array fields (for near queries), removed low level code, added critical tests for XContentBuilderExtensions

--- elastic4s-http/src/test/scala/com/sksamuel/elastic4s/http/search/queries/span/XContentBuilderExtensionsTest.scala
+++ elastic4s-http/src/test/scala/com/sksamuel/elastic4s/http/search/queries/span/XContentBuilderExtensionsTest.scala
@@ -3,54 +3,72 @@ package com.sksamuel.elastic4s.http.search.queries.span
 import com.sksamuel.elastic4s.http.search.queries.span.XContentBuilderExtensions._
 import org.elasticsearch.common.xcontent.{XContentBuilder, XContentFactory}
 import org.scalatest.FunSuite
+import org.scalatest.Matchers._
 
 import scala.util.parsing.json.JSON
 
 class XContentBuilderExtensionsTest extends FunSuite {
 
-  test("RichXContentBuilder.rawArrayValue should write not write bytes for empty array") {
+  test("RichXContentBuilder.addArray should not write bytes for empty array") {
     val builder = XContentFactory.jsonBuilder()
-    builder.startArray()
-    builder.rawArrayValue(Seq())
-    builder.endArray()
+    builder.startObject()
+    builder.addArray("arrayField", Seq())
+    builder.endObject()
 
     val actual = JSON.parseRaw(builder.string())
-    val expected = JSON.parseRaw("""[]""")
+    val expected = JSON.parseRaw("""{"arrayField": []}""")
     assert(actual === expected)
   }
 
-  test("RichXContentBuilder.rawArrayValue should write bytes array with one element") {
+  test("RichXContentBuilder.addArray should write bytes array with one element") {
     val builder = XContentFactory.jsonBuilder()
-    builder.startArray()
-    builder.rawArrayValue(Seq(getSimpleObject("f1", "v1")))
-    builder.endArray()
+    builder.startObject()
+    builder.addArray("arrayField", Seq(buildSimpleObject("f1", "v1")))
+    builder.endObject()
 
     val actual = JSON.parseRaw(builder.string())
-    val expected = JSON.parseRaw("""[{"f1": "v1"}]""")
+    val expected = JSON.parseRaw("""{"arrayField": [{"f1": "v1"}]}""")
     assert(actual === expected)
   }
 
-  test("RichXContentBuilder.rawArrayValue should write bytes array many elements") {
+  test("RichXContentBuilder.addArray should write bytes array with many elements") {
     val builder = XContentFactory.jsonBuilder()
-    builder.startArray()
-    builder.rawArrayValue(Seq(
-      getSimpleObject("f1", "v1"),
-      getSimpleObject("f2", "v2"),
-      getSimpleObject("f3", "v3")
+    builder.startObject()
+    builder.addArray("arrayField", Seq(
+      buildSimpleObject("f1", "v1"),
+      buildSimpleObject("f2", "v2"),
+      buildSimpleObject("f3", "v3")
     ))
-    builder.endArray()
+    builder.endObject()
 
     val actual = JSON.parseRaw(builder.string())
     val expected = JSON.parseRaw(
-      """[
-        |   {"f1": "v1"},
-        |   {"f2": "v2"},
-        |   {"f3": "v3"}
-        |]""".stripMargin)
+      """{
+        |  "arrayField": [
+        |    {"f1": "v1"},
+        |    {"f2": "v2"},
+        |    {"f3": "v3"}
+        |  ]
+        |}""".stripMargin)
     assert(actual === expected)
   }
 
-  private def getSimpleObject(field: String, value: String): XContentBuilder = {
+  test("RichXContentBuilder.addArray should not write extra zero bytes between/after comma character, because ES fails to handle them") {
+    val builder = XContentFactory.jsonBuilder()
+    builder.startObject()
+    builder.addArray("arrayField", Seq(
+      buildSimpleObject("f1", "v1"),
+      buildSimpleObject("f2", "v2"),
+      buildSimpleObject("f3", "v3")
+    ))
+    builder.endObject()
+
+    val result = builder.string()
+    result should not contain '\u0000'
+    result shouldBe """{"arrayField":[{"f1":"v1"},{"f2":"v2"},{"f3":"v3"}]}"""
+  }
+
+  private def buildSimpleObject(field: String, value: String): XContentBuilder = {
     XContentFactory.jsonBuilder().startObject().field(field, value).endObject()
   }
 }
