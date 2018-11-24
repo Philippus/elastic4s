aaf510ea7a9c6a4f587a1c7c65209ae669d01e55
Andrew Gustafson <gus4000@gmail.com>
2018-11-24 18:08:25 +0000

    Minor refactoring

--- elastic4s-fs2-streams/src/main/scala/com/sksamuel/elastic4s/streams/fs2/Fs2Subscriber.scala
+++ elastic4s-fs2-streams/src/main/scala/com/sksamuel/elastic4s/streams/fs2/Fs2Subscriber.scala
@@ -2,19 +2,24 @@ package com.sksamuel.elastic4s.streams.fs2
 
 import cats.Applicative
 import cats.syntax.functor._
-import com.sksamuel.elastic4s.http.ElasticDsl.{clearScroll, searchScroll}
 import com.sksamuel.elastic4s.http.search.{SearchHit, SearchResponse}
 import com.sksamuel.elastic4s.http._
-import com.sksamuel.elastic4s.searches.{SearchRequest, SearchScrollRequest}
+import com.sksamuel.elastic4s.searches.SearchRequest
 import com.sksamuel.exts.Logging
-import com.sksamuel.exts.OptionImplicits._
 import fs2._
 import scala.concurrent.TimeoutException
 import scala.concurrent.duration._
 import scala.language.higherKinds
 
-class Fs2Subscriber[F[_]: Applicative: Executor: Functor: RaiseThrowable](client: ElasticClient)
-    extends ElasticDsl {
+/**
+  * Fs2 subscriber which returns a [[fs2.Stream]] of [[SearchHit]]
+  *
+  * @param client The elastic client which is used under the hood.
+  * @tparam F The stream effect type
+  */
+class Fs2Subscriber[F[_]: Applicative: Functor: Executor: RaiseThrowable](client: ElasticClient)
+    extends ElasticDsl with Logging {
+
   type ScrollId = String
 
   val defaultKeepAlive: FiniteDuration = 1.minute
@@ -22,7 +27,7 @@ class Fs2Subscriber[F[_]: Applicative: Executor: Functor: RaiseThrowable](client
   def stream(query: SearchRequest): Stream[F, SearchHit] = {
     val keepAlive = query.keepAlive.getOrElse(defaultKeepAlive.toSeconds + "s")
 
-    def getNextResponse(response: Response[SearchResponse]) : Stream[F, SearchHit] = response match {
+    def getNextResponse(response: Response[SearchResponse]): Stream[F, SearchHit] = response match {
       case response: RequestFailure =>
         val errorMessage = response.error.toString
         logger.warn("Request errored, will terminate the subscription", errorMessage)
@@ -36,21 +41,22 @@ class Fs2Subscriber[F[_]: Applicative: Executor: Functor: RaiseThrowable](client
       // if we had no results from ES then we have nothing left to publish and our work here is done
       case response: RequestSuccess[SearchResponse] if response.result.isEmpty =>
         logger.debug("Response from ES came back empty; this means no more items upstream so will complete subscription")
-        val searchHits = response.result.hits.hits
-        response.result.scrollId match {
-          case Some(id) =>
-            Stream.emits(searchHits).onFinalize[F] {
+        response.result.scrollId.foldLeft(
+          Stream.emits[F, SearchHit](response.result.hits.hits)
+        ) {
+          case (stream, id) =>
+            stream.onFinalize[F] {
               client.execute(clearScroll(id)).map(_ => ())
             }
-          case None =>
-            Stream.emits(searchHits)
         }
 
       case response: RequestSuccess[SearchResponse] =>
         response.result.scrollId match {
           case Some(id) =>
             Stream.emits(response.result.hits.hits) ++
-              Stream.eval(client.execute(searchScroll(id, keepAlive))).flatMap(getNextResponse)
+              Stream
+                .eval(client.execute(searchScroll(id, keepAlive)))
+                .flatMap(getNextResponse)
           case None =>
             Stream.raiseError(new IllegalStateException("Response did not include a scroll id"))
         }
