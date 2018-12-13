9c3f760cf41220a8a78179506cb3a6c95667daf8
ab04784 <ButlerAdamJ@johnDeere.com>
2018-12-13 10:10:56 -0500

    Addresses #1582
    Adds error handling around potential JsonMapperExceptions if the response body in an ElasticSearch error is incomplete or otherwise malformed.

--- elastic4s-http/src/test/scala/com/sksamuel/elastic4s/http/ElasticErrorTest.scala
+++ elastic4s-http/src/test/scala/com/sksamuel/elastic4s/http/ElasticErrorTest.scala
@@ -8,7 +8,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ElasticErrorTest extends FlatSpec with Matchers with ElasticDsl with MockitoSugar {
 
-  "ElasticError" should "properly handle an error response with an empty body" in {
+  "ElasticError" should "properly handle an error response with an incorrect body" in {
     val error = ElasticError.parse(HttpResponse(123, Some(StringEntity("{", None)), Map()))
     assert(error.reason == "123")
   }
