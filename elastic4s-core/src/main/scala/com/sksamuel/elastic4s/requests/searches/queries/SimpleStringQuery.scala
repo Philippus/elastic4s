32f74977e334a4136db3103246c7eacff6c27a58
Fabrizio Fortino <fabrizio.fortino@gmail.com>
2019-04-01 09:52:04 +0200

    use string instead of int for minimum_should_match in query_string and simple_query_string

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/SimpleStringQuery.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/SimpleStringQuery.scala
@@ -29,7 +29,7 @@ case class SimpleStringQuery(query: String,
                              lenient: Option[Boolean] = None,
                              fields: Seq[(String, Option[Double])] = Nil,
                              flags: Seq[SimpleQueryStringFlag] = Nil,
-                             minimumShouldMatch: Option[Int] = None)
+                             minimumShouldMatch: Option[String] = None)
     extends Query {
 
   def quoteFieldSuffix(suffix: String): SimpleStringQuery     = copy(quote_field_suffix = suffix.some)
@@ -41,7 +41,7 @@ case class SimpleStringQuery(query: String,
 
   def lenient(lenient: Boolean): SimpleStringQuery = copy(lenient = lenient.some)
 
-  def minimumShouldMatch(minimumShouldMatch: Int): SimpleStringQuery =
+  def minimumShouldMatch(minimumShouldMatch: String): SimpleStringQuery =
     copy(minimumShouldMatch = minimumShouldMatch.some)
 
   def analyzeWildcard(analyzeWildcard: Boolean): SimpleStringQuery =
