e13b2926033eb2ef17ef54563dcace135ada975d
Sean Sullivan <github@seansullivan.com>
2019-05-28 16:24:21 -0400

    AWS SDK 2.5.52

--- project/Build.scala
+++ project/Build.scala
@@ -21,7 +21,7 @@ object Build extends AutoPlugin {
     val ExtsVersion            = "1.61.0"
     val JacksonVersion         = "2.9.8"
     val Json4sVersion          = "3.6.6"
-    val AWSJavaSdkVersion      = "2.5.8"
+    val AWSJavaSdkVersion      = "2.5.52"
     val Log4jVersion           = "2.11.1"
     val MockitoVersion         = "1.10.19"
     val MonixVersion           = "2.3.3"
