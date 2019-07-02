8cb4db3536789b51bf8fc6de2aafa92eb148717b
sksamuel <sam@sksamuel.com>
2019-07-01 23:34:46 -0500

    Release 6.7.2

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object Build extends AutoPlugin {
     val CatsVersion            = "1.4.0"
     val CirceVersion           = "0.9.3"
     val CommonsIoVersion       = "2.4"
-    val ElasticsearchVersion   = "6.6.2"
+    val ElasticsearchVersion   = "6.7.2"
     val ExtsVersion            = "1.60.0"
     val JacksonVersion         = "2.9.9"
     val Json4sVersion          = "3.6.1"
