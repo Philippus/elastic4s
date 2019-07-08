41e2bdb3d2f8a7447ca462168e3606ee57a944bc
Scala Steward <me@scala-steward.org>
2019-07-08 19:32:52 +0200

    Update sttp:async-http-client-backend-future, ... to 1.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -31,7 +31,7 @@ object Build extends AutoPlugin {
     val ScalamockVersion       = "4.3.0"
     val ScalazVersion          = "7.2.28"
     val SprayJsonVersion       = "1.3.5"
-    val SttpVersion            = "1.6.0"
+    val SttpVersion            = "1.6.1"
     val Slf4jVersion           = "1.7.26"
   }
 
