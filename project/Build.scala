0f65936d5f0943bb11bb3d974ef2fd0fcc232cfe
Scala Steward <me@scala-steward.org>
2019-07-27 07:09:31 +0200

    Update jackson-databind to 2.9.9.2

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object Build extends AutoPlugin {
     val CommonsIoVersion       = "2.6"
     val ElasticsearchVersion   = "7.1.1"
     val ExtsVersion            = "1.61.1"
-    val JacksonVersion         = "2.9.9"
+    val JacksonVersion         = "2.9.9.2"
     val Json4sVersion          = "3.6.7"
     val AWSJavaSdkVersion      = "2.7.1"
     val Log4jVersion           = "2.12.0"
