57f0d59a5fd12db4ea3f4394c5ce241d561da44e
Jens Zettelmeyer <zettelmerj@gmail.com>
2019-11-05 14:05:36 +0000

    Bump jackson to version 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object Build extends AutoPlugin {
     val CommonsIoVersion       = "2.6"
     val ElasticsearchVersion   = "7.2.0"
     val ExtsVersion            = "1.61.1"
-    val JacksonVersion         = "2.9.9"
+    val JacksonVersion         = "2.10.0"
     val Json4sVersion          = "3.6.7"
     val AWSJavaSdkVersion      = "2.5.52"
     val Log4jVersion           = "2.11.1"
