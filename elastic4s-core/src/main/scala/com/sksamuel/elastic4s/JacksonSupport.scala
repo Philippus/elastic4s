05300bc084f1848cb9d0821e8e2e37af1f212c0b
Erik LaBianca <erik.labianca@gmail.com>
2019-12-21 17:25:46 -0500

    Add custom backwards compatible deserializer for Total
    - Allows reading a number as produced by es6 vs the object produced by es7

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/JacksonSupport.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/JacksonSupport.scala
@@ -2,9 +2,11 @@ package com.sksamuel.elastic4s
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import com.sksamuel.elastic4s.requests.searches.Total
 
 object JacksonSupport {
 
@@ -17,4 +19,9 @@ object JacksonSupport {
   mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
   mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
   mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
+
+  val module = new SimpleModule {
+    addDeserializer(classOf[Total], new Total.Deserializer(classOf[Total]))
+  }
+  mapper.registerModule(module)
 }
