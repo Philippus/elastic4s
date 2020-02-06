c03ae6bcfed7b5e9fe9fa2e20b95087c851f17de
Kiril Yurovnik <kiril.yurovnik@riskified.com>
2020-02-06 15:10:59 +0200

    Add support for include_in_root|parent settings for nested fields

---
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/mappings/NestedFieldTest.scala
@@ -0,0 +1,30 @@
+package com.sksamuel.elastic4s.mappings
+
+import com.sksamuel.elastic4s.ElasticApi
+import org.scalatest.{FlatSpec, Matchers}
+
+class NestedFieldTest extends FlatSpec with Matchers with ElasticApi {
+
+  val field: NestedField = nestedField("myfield")
+
+  "A NestedField" should "support boolean dynamic property" in {
+    FieldBuilderFn(field.dynamic(true)).string() shouldBe
+      """{"type":"nested","dynamic":"true"}"""
+  }
+
+  it should "support string dynamic property" in {
+    FieldBuilderFn(field.dynamic("strict")).string() shouldBe
+      """{"type":"nested","dynamic":"strict"}"""
+  }
+
+  it should "support include_in_root property" in {
+    FieldBuilderFn(field.includeInRoot(true)).string() shouldBe
+      """{"type":"nested","include_in_root":true}"""
+  }
+
+  it should "support include_in_parent property" in {
+    FieldBuilderFn(field.includeInParent(true)).string() shouldBe
+      """{"type":"nested","include_in_parent":true}"""
+  }
+}
+
