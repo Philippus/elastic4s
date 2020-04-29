d84780446d85a86ae66dab8e97598c7200bcf439
Domantas Petrauskas <dom.petrauskas@gmail.com>
2020-04-29 12:09:36 +0300

    Update Jackson to 2.10.3

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object Build extends AutoPlugin {
     val CommonsIoVersion       = "2.4"
     val ElasticsearchVersion   = "6.3.0"
     val ExtsVersion            = "1.60.0"
-    val JacksonVersion         = "2.9.6"
+    val JacksonVersion         = "2.10.3"
     val Json4sVersion          = "3.5.4"
     val SprayJsonVersion       = "1.3.4"
     val AWSJavaSdkVersion      = "1.11.342"
