3ea9d60e202a014954086a3c1718d9c861c5e770
sksamuel <sam@sksamuel.com>
2020-05-05 21:15:14 -0500

    Revert "Added routing to terms set query"
    This reverts commit 067597b8

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/term/TermsLookupQueryBodyFn.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/term/TermsLookupQueryBodyFn.scala
@@ -23,4 +23,13 @@ object TermsLookupQueryBodyFn {
   }
 }
 
-
+object TermsSetQueryBodyFn {
+  def apply(t: TermsSetQuery): XContentBuilder = {
+    val builder = XContentFactory.jsonBuilder().startObject("terms_set")
+    builder.startObject(t.field)
+    builder.array("terms", t.terms.map(_.toString).toArray[String])
+    t.minimumShouldMatchField.foreach(builder.field("minimum_should_match_field", _))
+    t.minimumShouldMatchScript.foreach(script => builder.rawField("minimum_should_match_script", ScriptBuilderFn(script)))
+    builder.endObject().endObject()
+  }
+}
