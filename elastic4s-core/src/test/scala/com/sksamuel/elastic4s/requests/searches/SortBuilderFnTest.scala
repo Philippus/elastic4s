e85a0040f2548ea36178067d9177c35a01ea0642
Philippus <philippus@gmail.com>
2020-07-25 21:35:49 +0200

    Merge SortBuilderFnTest classes

--- elastic4s-core/src/test/scala/com/sksamuel/elastic4s/requests/searches/SortBuilderFnTest.scala
+++ elastic4s-core/src/test/scala/com/sksamuel/elastic4s/requests/searches/SortBuilderFnTest.scala
@@ -1,10 +1,13 @@
 package com.sksamuel.elastic4s.requests.searches
 
+import com.sksamuel.elastic4s.ElasticDsl.{matchQuery, nestedSort, script, scriptSort}
 import com.sksamuel.elastic4s.JsonSugar
+import com.sksamuel.elastic4s.requests.common.DistanceUnit
+import com.sksamuel.elastic4s.requests.script.ScriptType
 import com.sksamuel.elastic4s.requests.searches.queries.matches.MatchQuery
-import com.sksamuel.elastic4s.requests.searches.queries.{FieldSortBuilderFn, GeoDistanceSortBuilderFn, RangeQuery}
-import com.sksamuel.elastic4s.requests.searches.sort.{FieldSort, GeoDistanceSort, NestedSort}
-import com.sksamuel.elastic4s.requests.searches.sort.SortMode.{Avg, Min}
+import com.sksamuel.elastic4s.requests.searches.queries.{FieldSortBuilderFn, GeoDistanceSortBuilderFn, RangeQuery, SortBuilderFn}
+import com.sksamuel.elastic4s.requests.searches.sort.{FieldSort, GeoDistanceSort, NestedSort, ScriptSortType, SortOrder}
+import com.sksamuel.elastic4s.requests.searches.sort.SortMode.Min
 import com.sksamuel.elastic4s.requests.searches.sort.SortOrder.Asc
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
@@ -71,5 +74,43 @@ class SortBuilderFnTest extends AnyFunSuite with Matchers with JsonSugar {
 
     GeoDistanceSortBuilderFn(geoDistanceSort).string() shouldBe
       """{"_geo_distance":{"pin.location":[[-70.0,40.0]],"ignore_unmapped":true}}"""
+  }
+
+  test("geo distance sort does not generate unit field by default") {
+    val sort = GeoDistanceSort(
+      field = "location",
+      points = Seq(GeoPoint(43.65435, -79.38871))
+    )
+
+    SortBuilderFn(sort).string() shouldBe
+      """{"_geo_distance":{"location":[[-79.38871,43.65435]]}}"""
+  }
+
+  test("geo distance sort generates unit field when informed") {
+    val sort = GeoDistanceSort(
+      field = "location",
+      points = Seq(GeoPoint(43.65435, -79.38871)),
+      unit = Some(DistanceUnit.Kilometers)
+    )
+
+    SortBuilderFn(sort).string() shouldBe
+      """{"_geo_distance":{"location":[[-79.38871,43.65435]],"unit":"km"}}"""
+  }
+
+  test("sort script parameters are encoded with the correct type") {
+    val scr = script("dummy script")
+      .lang("painless")
+      .scriptType(ScriptType.Source)
+      .params(Map(
+        "nump" -> 10.2,
+        "stringp" -> "ciao",
+        "boolp" -> true
+      ))
+    val request = scriptSort(scr)
+      .typed(ScriptSortType.Number)
+      .order(SortOrder.Desc)
+      .nested(nestedSort().path("foo.bar").filter(matchQuery("foo.bar", "foo")))
+    SortBuilderFn(request).string() shouldBe
+      """{"_script":{"script":{"source":"dummy script","lang":"painless","params":{"nump":10.2,"stringp":"ciao","boolp":true}},"type":"number","order":"desc","nested":{"path":"foo.bar","filter":{"match":{"foo.bar":{"query":"foo"}}}}}}"""
   }
 }
