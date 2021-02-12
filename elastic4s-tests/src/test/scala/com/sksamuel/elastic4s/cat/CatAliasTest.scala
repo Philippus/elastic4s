2b179e7b9059cb7124a8d38a92dd0c98a3186090
Marek Kadek <kadek.marek@gmail.com>
2021-02-12 16:35:06 +0100

    Add pattern to catAlias (#2311)

--- elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/cat/CatAliasTest.scala
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/cat/CatAliasTest.scala
@@ -14,7 +14,8 @@ class CatAliasTest extends AnyFlatSpec with Matchers with DockerTests {
   client.execute {
     aliases(
       addAlias("ally1", "catalias"),
-      addAlias("ally2", "catalias")
+      addAlias("ally2", "catalias"),
+      addAlias("ally22", "catalias")
     )
   }.await
 
@@ -24,5 +25,14 @@ class CatAliasTest extends AnyFlatSpec with Matchers with DockerTests {
     }.await
     result.result.map(_.alias).toSet.contains("ally1") shouldBe true
     result.result.map(_.alias).toSet.contains("ally2") shouldBe true
+  }
+
+  it should "return aliases matching a pattern" in {
+    val result = client.execute {
+      catAliases("ally2*")
+    }.await
+    result.result.map(_.alias).toSet.contains("ally1") shouldBe false
+    result.result.map(_.alias).toSet.contains("ally2") shouldBe true
+    result.result.map(_.alias).toSet.contains("ally22") shouldBe true
   }
 }
