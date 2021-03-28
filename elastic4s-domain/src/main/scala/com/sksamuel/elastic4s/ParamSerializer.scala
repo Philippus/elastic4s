da147cb6bda413d082dc08e1170469393dfdecb5
sksamuel <sam@sksamuel.com>
2021-03-28 13:26:48 -0500

    maor

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/ParamSerializer.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/ParamSerializer.scala
@@ -1,7 +1,8 @@
 package com.sksamuel.elastic4s
 
 /**
-  * A type class that is used to serialize the parameters sent to a [[com.sksamuel.elastic4s.requests.script.Script]].
+  * A typeclass that is used to serialize the parameters sent
+  * to a [[com.sksamuel.elastic4s.requests.script.Script]].
   *
   * [[ParamSerializer]]s can be automatically derived if you add a module like `elastic4s-json-spray`.
   */
