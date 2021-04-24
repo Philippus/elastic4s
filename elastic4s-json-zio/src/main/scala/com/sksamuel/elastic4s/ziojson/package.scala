83211341a1d0df5852aedb3b5e9c6e61f22ac001
Remi Guittaut <remi.guittaut@gmail.com>
2021-04-24 17:16:39 +0200

    Issue 2400 - fixing typo

--- elastic4s-json-zio/src/main/scala/com/sksamuel/elastic4s/ziojson/package.scala
+++ elastic4s-json-zio/src/main/scala/com/sksamuel/elastic4s/ziojson/package.scala
@@ -8,11 +8,11 @@ package object ziojson {
   @implicitNotFound("No JsonEncoder for type ${T} found. Bring an implicit JsonEncoder[T] instance in scope")
   implicit def zioJsonIndexable[T](implicit encoder: JsonEncoder[T]): Indexable[T] = _.toJson
 
-  @implicitNotFound("No JsonDecoder for type ${T} found. Bring an implicit ReadsJsonDecoder[T] instance in scope")
+  @implicitNotFound("No JsonDecoder for type ${T} found. Bring an implicit JsonDecoder[T] instance in scope")
   implicit def zioJsonHitReader[T](implicit decoder: JsonDecoder[T]): HitReader[T] =
     _.sourceAsString.fromJson[T].left.map(new Throwable(_)).toTry
 
-  @implicitNotFound("No JsonDecoder for type ${T} found. Bring an implicit ReadsJsonDecoder[T] instance in scope")
+  @implicitNotFound("No JsonDecoder for type ${T} found. Bring an implicit JsonDecoder[T] instance in scope")
   implicit def zioJsonAggReader[T](implicit decoder: JsonDecoder[T]): AggReader[T] =
     _.fromJson[T].left.map(new Throwable(_)).toTry
 
