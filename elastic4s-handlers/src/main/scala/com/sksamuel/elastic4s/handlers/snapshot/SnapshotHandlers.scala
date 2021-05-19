b2ecdb6af5907ea0d045316bd0966db48231e770
Hugh Simpson <hugh.simpson@babylonhealth.com>
2021-05-19 10:09:44 +0100

    More general handling of create/restore snapshot responses

--- elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/snapshot/SnapshotHandlers.scala
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/snapshot/SnapshotHandlers.scala
@@ -1,8 +1,8 @@
 package com.sksamuel.elastic4s.handlers.snapshot
 
 import com.sksamuel.elastic4s.json.XContentFactory
-import com.sksamuel.elastic4s.requests.snapshots.{CreateRepositoryRequest, CreateRepositoryResponse, CreateSnapshotRequest, CreateSnapshotResponse, DeleteSnapshotRequest, DeleteSnapshotResponse, GetSnapshotResponse, GetSnapshotsRequest, RestoreSnapshotRequest, RestoreSnapshotResponse}
-import com.sksamuel.elastic4s.{ElasticRequest, Handler, HttpEntity}
+import com.sksamuel.elastic4s.requests.snapshots.{CreateRepositoryRequest, CreateRepositoryResponse, CreateSnapshotRequest, CreateSnapshotResponse, DeleteSnapshotRequest, DeleteSnapshotResponse, GetSnapshotResponse, GetSnapshotsRequest, RestoreSnapshotRequest, RestoreSnapshotResponse, RestoreSnapshotResponseAsync, RestoreSnapshotResponseAwait}
+import com.sksamuel.elastic4s.{ElasticRequest, Handler, HttpEntity, ResponseHandler}
 
 trait SnapshotHandlers {
 
--- elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/requests/snapshots/SnapshotTest.scala
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/requests/snapshots/SnapshotTest.scala
@@ -9,6 +9,7 @@ import org.scalatest.matchers.should.Matchers
 class SnapshotTest extends AnyFlatSpec with Matchers with DockerTests {
 
   private val repoName = "repotest_" + UUID.randomUUID().toString
+  private val snapshotName = "snap1"
 
   "createRepository" should "create a new repo" in {
     val resp = client.execute {
@@ -24,23 +25,30 @@ class SnapshotTest extends AnyFlatSpec with Matchers with DockerTests {
   }
 
   "createSnapshot" should "create a new snapshot" in {
-    val resp = client.execute {
-      createSnapshot("snap1", repoName)
-    }.await
-    resp.result.accepted shouldBe true
+    // create an index to restore, so that we can test some of the behaviour around that
+    createIdx("tmpidx").isSuccess shouldBe true
+    client.execute {
+      createSnapshot("snap0", repoName)
+    }.await.result.succeeded shouldBe true
+  }
+
+  it should "deserialize when waitForCompletion = true" in {
+    client.execute {
+      createSnapshot(snapshotName, repoName) waitForCompletion true
+    }.await.result.succeeded shouldBe true
   }
 
   it should "error when the repo does not exist" in {
     client.execute {
-      createSnapshot("snap1", "abbbbc")
+      createSnapshot(snapshotName, "abbbbc")
     }.await.error.`type` shouldBe "repository_missing_exception"
   }
 
   "getSnapshot" should "return the named snapshot" in {
     val resp = client.execute {
-      getSnapshot("snap1", repoName)
+      getSnapshot(snapshotName, repoName)
     }.await.result
-    resp.snapshots.head.snapshot shouldBe "snap1"
+    resp.snapshots.head.snapshot shouldBe snapshotName
     resp.snapshots.head.uuid should not be null
     resp.snapshots.head.version should not be null
   }
@@ -53,34 +61,55 @@ class SnapshotTest extends AnyFlatSpec with Matchers with DockerTests {
 
   it should "error when the repo does not exist" in {
     client.execute {
-      getSnapshot("snap1", "bbbbb")
+      getSnapshot(snapshotName, "bbbbb")
     }.await.error.`type` shouldBe "repository_missing_exception"
   }
 
-  "restore snapshot" should "be accepted" in {
+  "restore snapshot" should "error when an index clashes" in {
     client.execute {
-      restoreSnapshot("snap1", repoName)
-    }.await.result.accepted shouldEqual true
+      restoreSnapshot(snapshotName, repoName) waitForCompletion true
+    }.await.error.`type` shouldBe "snapshot_restore_exception"
+  }
+
+  it should "succeed when request is correct" in {
+    client.execute {deleteIndex("tmpidx")}.await.isSuccess shouldBe true
+    client.execute {
+      restoreSnapshot(snapshotName, repoName)// waitForCompletion true
+    }.await.result.succeeded shouldEqual true
+  }
+
+  it should "succeeded if waitForCompletion = false" in {
+    client.execute { deleteIndex("tmpidx") }.await.isSuccess shouldBe true
+    client.execute {
+      restoreSnapshot(snapshotName, repoName) waitForCompletion false
+    }.await.result.succeeded shouldEqual true
+  }
+
+  it should "succeeded if waitForCompletion = true" in {
+    client.execute { deleteIndex("tmpidx") }.await.isSuccess shouldBe true
+    client.execute {
+      restoreSnapshot(snapshotName, repoName) waitForCompletion true
+    }.await.result.succeeded shouldEqual true
   }
 
   it should "error when the snapshot does not exist" in {
     client.execute {
       restoreSnapshot("missing_snapshot", repoName)
-    }.await.error.`type` shouldBe "snapshot_missing_exception"
+    }.await.error.`type` shouldBe "snapshot_restore_exception"
   }
 
   it should "error when the repo does not exist" in {
     client.execute {
-      restoreSnapshot("snap1", "missing_repo")
+      restoreSnapshot(snapshotName, "missing_repo")
     }.await.error.`type` shouldBe "repository_missing_exception"
   }
 
   "deleteSnapshot" should "remove the named snapshot" in {
     client.execute {
-      deleteSnapshot("snap1", repoName)
+      deleteSnapshot(snapshotName, repoName)
     }.await.result.acknowledged shouldBe true
     client.execute {
-      getSnapshot("snap1", repoName)
+      getSnapshot(snapshotName, repoName)
     }.await.error.`type` shouldBe "snapshot_missing_exception"
   }
 }
