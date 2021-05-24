68906dbbd25c6ff76e0b52b9b73f11a5c17a8144
Hugh Simpson <hugh.simpson@babylonhealth.com>
2021-05-24 13:51:01 +0100

    include the mapping field in dynamic template requests

--- elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/index/mapping/DynamicTemplateBodyFn.scala
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/index/mapping/DynamicTemplateBodyFn.scala
@@ -1,5 +1,6 @@
 package com.sksamuel.elastic4s.handlers.index.mapping
 
+import com.sksamuel.elastic4s.handlers.fields.ElasticFieldBuilderFn
 import com.sksamuel.elastic4s.json.{XContentBuilder, XContentFactory}
 import com.sksamuel.elastic4s.requests.mappings.dynamictemplate.DynamicTemplateRequest
 
@@ -17,7 +18,7 @@ object DynamicTemplateBodyFn {
     dyn.MatchPattern.foreach(builder.field("match_pattern", _))
     dyn.matchMappingType.foreach(builder.field("match_mapping_type", _))
 
-    //    builder.rawField("mapping", FieldBuilderFn(dyn.mapping))
+    builder.rawField("mapping", ElasticFieldBuilderFn(dyn.mapping))
 
     builder.endObject()
   }
