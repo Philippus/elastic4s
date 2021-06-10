9db6c748e0b9244e6907fcfa7abba28685c401de
Scala Steward <me@scala-steward.org>
2021-06-10 17:04:20 +0200

    Update akka-actor, akka-stream to 2.6.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Dependencies {
   val AkkaHttpVersion                = "10.2.3"
-  val AkkaVersion                    = "2.6.14"
+  val AkkaVersion                    = "2.6.15"
   val CatsEffect2Version             = "2.5.0"
   val CatsEffectVersion              = "3.1.1"
   val CatsVersion                    = "2.0.0"
