3852a5341caff16ecb26826d80cfdf62bcfa063b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-07-25 11:39:47 +0200

    Update cats-effect to 3.2.0 (#2479)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val AkkaHttpVersion                = "10.2.3"
   val AkkaVersion                    = "2.6.14"
   val CatsEffect2Version             = "2.5.0"
-  val CatsEffectVersion              = "3.1.1"
+  val CatsEffectVersion              = "3.2.0"
   val CatsVersion                    = "2.0.0"
   val CirceVersion                   = "0.14.1"
   val CommonsIoVersion               = "2.9.0"
