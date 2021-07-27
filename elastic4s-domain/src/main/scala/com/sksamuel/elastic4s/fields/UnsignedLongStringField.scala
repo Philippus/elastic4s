2f1574b7559551dd1c3635046d1c3198b7a51b13
cwgroppe <christophergroppe@gmail.com>
2021-07-27 13:30:46 -0400

    fixed compilation errors from typos like elastic4s somehow got changed to elastic5s

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/UnsignedLongStringField.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/UnsignedLongStringField.scala
@@ -1,4 +1,6 @@
- case class UnsignedLongStringField(name: String,
+package com.sksamuel.elastic4s.fields
+
+case class UnsignedLongStringField(name: String,
                                     boost: Option[Double] = None,
                                     coerce: Option[Boolean] = None,
                                     copyTo: Seq[String] = Nil,
@@ -6,7 +8,7 @@
                                     ignoreMalformed: Option[Boolean] = None,
                                     index: Option[Boolean] = None,
                                     store: Option[Boolean] = None,
-                                    nullValue: Option[Long] = None,
+                                    nullValue: Option[String] = None,
                                     meta: Map[String, Any] = Map.empty) extends NumberField[String] {
-    override def `type`: String = "unsigned_long"
-  }
+  override def `type`: String = "unsigned_long"
+}
