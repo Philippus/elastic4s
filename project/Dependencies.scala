7fd39f927a66c1526b9f1769edc2a36d9dc578dc
Scala Steward <me@scala-steward.org>
2021-07-27 10:57:16 +0200

    Update akka-http to 10.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.Keys.libraryDependencies
 import sbt._
 
 object Dependencies {
-  val AkkaHttpVersion                = "10.2.3"
+  val AkkaHttpVersion                = "10.2.5"
   val AkkaVersion                    = "2.6.14"
   val CatsEffect2Version             = "2.5.0"
   val CatsEffectVersion              = "3.2.0"
