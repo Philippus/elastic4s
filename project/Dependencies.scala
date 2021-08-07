a5c1c8810f1883709f7ad1989bf905b64ed94b4a
Scala Steward <me@scala-steward.org>
2021-08-07 05:11:23 +0200

    Update cats-effect to 2.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
   val AkkaHttpVersion                = "10.2.5"
   val AkkaVersion                    = "2.6.15"
-  val CatsEffect2Version             = "2.5.2"
+  val CatsEffect2Version             = "2.5.3"
   val CatsEffectVersion              = "3.2.2"
   val CatsVersion                    = "2.0.0"
   val CirceVersion                   = "0.14.1"
