8abb47c5fa954f2cd8a57bdc488dfaf4222e63cb
chris <c@24.org.uk>
2021-09-24 16:56:39 +0100

    Add support for case-insensitive prefix queries. Also add a test for prefix query body.

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/PrefixQuery.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/PrefixQuery.scala
@@ -6,10 +6,12 @@ case class PrefixQuery(field: String,
                        prefix: Any,
                        boost: Option[Double] = None,
                        queryName: Option[String] = None,
-                       rewrite: Option[String] = None)
+                       rewrite: Option[String] = None,
+                       caseInsensitive: Option[Boolean] = None)
   extends MultiTermQuery {
 
   def queryName(queryName: String): PrefixQuery = copy(queryName = queryName.some)
   def boost(boost: Double): PrefixQuery = copy(boost = boost.some)
   def rewrite(rewrite: String): PrefixQuery = copy(rewrite = rewrite.some)
+  def caseInsensitive(caseInsensitive: Boolean): PrefixQuery = copy(caseInsensitive = caseInsensitive.some)
 }
