d62cd782b126cc7f94db88e24ad8447d8d26ccc8
Mathieu Pichette <mpichette@apple.com>
2021-10-21 13:41:36 -0700

    Add support for Stored Script APIs

---
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/script/PutStoredScriptRequest.scala
@@ -0,0 +1,3 @@
+package com.sksamuel.elastic4s.requests.script
+
+case class PutStoredScriptRequest(id: String, script: StoredScriptSource)
---
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/script/StoredScriptHandlers.scala
@@ -0,0 +1,45 @@
+package com.sksamuel.elastic4s.handlers.script
+
+import com.sksamuel.elastic4s.requests.script.{
+  DeleteStoredScriptRequest,
+  DeleteStoredScriptResponse,
+  GetStoredScriptRequest,
+  GetStoredScriptResponse,
+  PutStoredScriptRequest,
+  PutStoredScriptResponse
+}
+import com.sksamuel.elastic4s.json.XContentFactory
+import com.sksamuel.elastic4s._
+
+trait StoredScriptHandlers {
+
+  implicit object DeleteStoredScriptHandler extends Handler[DeleteStoredScriptRequest, DeleteStoredScriptResponse] {
+    override def build(request: DeleteStoredScriptRequest): ElasticRequest = {
+      val endpoint = "/_scripts/" + request.id
+      ElasticRequest("DELETE", endpoint)
+    }
+  }
+
+  implicit object GetStoredScriptHandler extends Handler[GetStoredScriptRequest, GetStoredScriptResponse] {
+    override def build(request: GetStoredScriptRequest): ElasticRequest = {
+      val endpoint = "/_scripts/" + request.id
+      ElasticRequest("GET", endpoint)
+    }
+  }
+
+  implicit object PutStoredScriptHandler extends Handler[PutStoredScriptRequest, PutStoredScriptResponse] {
+    override def build(request: PutStoredScriptRequest): ElasticRequest = {
+      val endpoint = "/_scripts/" + request.id
+
+      val builder = XContentFactory.jsonBuilder()
+      builder.startObject("script")
+      builder.field("lang", request.script.lang)
+      builder.field("source", request.script.source)
+      builder.endObject()
+
+      ElasticRequest("PUT", endpoint, HttpEntity(builder.string()))
+    }
+  }
+}
+
+object StoredScriptHandlers extends StoredScriptHandlers
