14e690db5ab7413e8fa134576a33174ea2eb819e
Scala Steward <me@scala-steward.org>
2022-01-19 11:27:02 +0100

    Update scalaz-concurrent, scalaz-core to 7.2.34

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val ScalatestPlusMockitoArtifactId = "mockito-3-2"
   val ScalatestPlusVersion           = "3.1.2.0"
   val ScalatestVersion               = "3.2.10"
-  val ScalazVersion                  = "7.2.32"
+  val ScalazVersion                  = "7.2.34"
   val Slf4jVersion                   = "1.7.33"
   val SprayJsonVersion               = "1.3.6"
   val SttpVersion                    = "1.7.2"
