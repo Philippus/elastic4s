c95615f4ba3141454cc160b757e9beb008618b5b
Raffael Tancman <rtancman@gmail.com>
2022-03-07 21:11:09 -0300

    Add parameter in AnnotatedTextField

--- elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/fields/AnnotatedTextFieldBuilderFn.scala
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/fields/AnnotatedTextFieldBuilderFn.scala
@@ -4,12 +4,16 @@ import com.sksamuel.elastic4s.fields.AnnotatedTextField
 import com.sksamuel.elastic4s.json.{XContentBuilder, XContentFactory}
 
 object AnnotatedTextFieldBuilderFn {
-  def toField(name: String, values: Map[String, Any]): AnnotatedTextField = AnnotatedTextField(name)
+  def toField(name: String, values: Map[String, Any]): AnnotatedTextField = AnnotatedTextField(
+    name,
+    values.get("copy_to").map(_.asInstanceOf[Seq[String]]).getOrElse(Seq.empty)
+  )
 
   def build(field: AnnotatedTextField): XContentBuilder = {
 
     val builder = XContentFactory.jsonBuilder()
     builder.field("type", field.`type`)
+    if (field.copyTo.nonEmpty) builder.array("copy_to", field.copyTo.toArray)
     builder.endObject()
   }
 }
