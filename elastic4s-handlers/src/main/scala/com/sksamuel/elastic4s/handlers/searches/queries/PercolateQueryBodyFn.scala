2366ecfc6dd11dbe145d24d6fc8d4da6c886616f
Raffael Tancman <rtancman@gmail.com>
2022-04-04 08:34:37 -0300

    Fix the PercolateQuery
    - Field type has been removed.
    - Field document_type has been removed.

--- elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/searches/queries/PercolateQueryBodyFn.scala
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/searches/queries/PercolateQueryBodyFn.scala
@@ -9,10 +9,8 @@ object PercolateQueryBodyFn {
     val builder = XContentFactory.jsonBuilder()
     builder.startObject("percolate")
     builder.field("field", q.field)
-    builder.field("document_type", q.`type`)
     q.ref.foreach { ref =>
       builder.field("index", ref.index.name)
-      ref.`type`.foreach(`type` => builder.field("type", `type`))
       builder.field("id", ref.id)
     }
     q.source.foreach { source =>
