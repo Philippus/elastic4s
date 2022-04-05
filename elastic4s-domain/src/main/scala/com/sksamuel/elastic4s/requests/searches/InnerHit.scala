27efe30d0b1ff24e7f32fe7de69ebde0a403715b
Raffael Tancman <rtancman@gmail.com>
2022-04-05 15:22:00 -0300

    Remove _type in SearchHit
    - Searches on the _type field are no longer supported.
      https://www.elastic.co/guide/en/elasticsearch/reference/current/migrating-8.0.html#breaking-changes-8.0

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/InnerHit.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/InnerHit.scala
@@ -3,7 +3,6 @@ package com.sksamuel.elastic4s.requests.searches
 import com.sksamuel.elastic4s.requests.get.{HitField, MetaDataFields}
 
 case class InnerHit(index: String,
-                    `type`: String,
                     id: String,
                     nested: Map[String, AnyRef],
                     score: Option[Double],
