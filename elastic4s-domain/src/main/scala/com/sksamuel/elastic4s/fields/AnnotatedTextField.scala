770208cd96de683c171d96723473a29579c342e0
Jackson Fernandes <39283806+Jacksonfern@users.noreply.github.com>
2022-04-17 17:10:58 -0400

    Feat: add support for analyzers in annotated_text fields (#2654)
    * feat: add support for analyzers in annotated_text fields
    
    * fix tests

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/AnnotatedTextField.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/AnnotatedTextField.scala
@@ -3,6 +3,16 @@ package com.sksamuel.elastic4s.fields
 object AnnotatedTextField {
   val `type` = "annotated_text"
 }
-case class AnnotatedTextField(name: String, copyTo: Seq[String] = Nil) extends ElasticField {
+case class AnnotatedTextField(name: String,
+                              analyzer: Option[String] = None,
+                              searchAnalyzer: Option[String] = None,
+                              searchQuoteAnalyzer: Option[String] = None,
+                              copyTo: Seq[String] = Nil) extends ElasticField {
   override def `type`: String = AnnotatedTextField.`type`
+
+  def analyzer(name: String): AnnotatedTextField = copy(analyzer = Option(name))
+
+  def searchAnalyzer(name: String): AnnotatedTextField = copy(searchAnalyzer = Option(name))
+
+  def searchQuoteAnalyzer(name: String): AnnotatedTextField = copy(searchQuoteAnalyzer = Option(name))
 }
