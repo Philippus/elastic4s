23f8a6046fb79c6fd6c3307728e5bf7832f89987
Scala Steward <me@scala-steward.org>
2022-05-03 18:47:39 +0200

    Update elasticsearch-rest-client, ... to 8.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val CatsVersion                    = "2.0.0"
   val CirceVersion                   = "0.14.1"
   val CommonsIoVersion               = "2.11.0"
-  val ElasticsearchVersion           = "8.1.3"
+  val ElasticsearchVersion           = "8.2.0"
   val ExtsVersion                    = "1.61.1"
   val JacksonVersion                 = "2.13.2"
   val Json4sVersion                  = "4.0.5"
