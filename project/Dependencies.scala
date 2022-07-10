f9e1d39cdfac30a8986a2ea4a9aff247d63d0206
Scala Steward <scala_steward@virtuslab.com>
2022-07-10 04:20:20 +0000

    Update mockito-core to 4.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val JacksonVersion                 = "2.13.3"
   val Json4sVersion                  = "4.0.5"
   val Log4jVersion                   = "2.15.0"
-  val MockitoVersion                 = "4.5.1"
+  val MockitoVersion                 = "4.6.1"
   val MonixVersion                   = "3.4.1"
   val PlayJsonVersion                = "2.9.2"
   val ReactiveStreamsVersion         = "1.0.3"
