15366c7e16b82faa4931231afb0104125f079fea
Joël Vimenet <1035781+joelvim@users.noreply.github.com>
2022-09-06 03:05:43 +0200

    Make SpanNotQuery, SpanContainaingQuery and SpanWithinQuery inherit SpanQuery (#2717)

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/span/SpanContainingQuery.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/span/SpanContainingQuery.scala
@@ -1,13 +1,12 @@
 package com.sksamuel.elastic4s.requests.searches.span
 
-import com.sksamuel.elastic4s.requests.searches.queries.Query
 import com.sksamuel.elastic4s.ext.OptionImplicits._
 
 case class SpanContainingQuery(little: SpanQuery,
                                big: SpanQuery,
                                boost: Option[Double] = None,
                                queryName: Option[String] = None)
-  extends Query {
+  extends SpanQuery {
 
   def boost(boost: Double): SpanContainingQuery = copy(boost = boost.some)
   def queryName(queryName: String): SpanContainingQuery = copy(queryName = queryName.some)
