62b3798f05084486a6f7779e33795859389b8c68
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-11-04 14:45:08 +0100

    Update zio to 1.0.17 (#2739)
    Co-authored-by: Sam <sam@sksamuel.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   val SprayJsonVersion               = "1.3.6"
   val SttpVersion                    = "1.7.2"
   val ZIOJson1Version                = "0.1.5"
-  val ZIO1Version                    = "1.0.16"
+  val ZIO1Version                    = "1.0.17"
   val ZIOJsonVersion                 = "0.3.0"
   val ZIOVersion                     = "2.0.3"
 
