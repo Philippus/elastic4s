85492fb573863a183ce189e9720899f655d2ae7a
Scala Steward <scala_steward@virtuslab.com>
2022-11-05 01:39:48 +0000

    Update play-json to 2.10.0-RC7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val Log4jVersion                   = "2.15.0"
   val MockitoVersion                 = "4.8.1"
   val MonixVersion                   = "3.4.1"
-  val PlayJsonVersion                = "2.10.0-RC6"
+  val PlayJsonVersion                = "2.10.0-RC7"
   val ReactiveStreamsVersion         = "1.0.3"
   val ScalamockVersion               = "5.2.0"
   val ScalatestPlusMockitoArtifactId = "mockito-3-4"
