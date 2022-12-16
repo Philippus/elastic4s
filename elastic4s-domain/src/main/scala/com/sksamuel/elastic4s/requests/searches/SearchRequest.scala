cb1f8a10bd9f1c79e4bb37a99a7aebb500be651d
cdmbr <cdmbr85@gmail.com>
2022-12-16 16:38:44 -0300

    chore: do not import *

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/SearchRequest.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/SearchRequest.scala
@@ -9,7 +9,7 @@ import com.sksamuel.elastic4s.requests.searches.aggs.AbstractAggregation
 import com.sksamuel.elastic4s.requests.searches.collapse.CollapseRequest
 import com.sksamuel.elastic4s.requests.searches.queries.compound.BoolQuery
 import com.sksamuel.elastic4s.requests.searches.queries.matches.{MatchAllQuery, MatchQuery}
-import com.sksamuel.elastic4s.requests.searches.queries._
+import com.sksamuel.elastic4s.requests.searches.queries.{PrefixQuery, Query, QueryStringQuery, RawQuery, RegexQuery}
 import com.sksamuel.elastic4s.requests.searches.sort.{FieldSort, Sort}
 import com.sksamuel.elastic4s.requests.searches.suggestion.Suggestion
 import com.sksamuel.elastic4s.requests.searches.term.TermQuery
