668f421d5f361afe243471fd4a4a5cba8a39d8a8
TakiuGit <paulnoferi@gmail.com>
2023-07-03 13:56:12 +0200

    support boost on TermsSetQuery

--- elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/searches/queries/term/TermsSetQueryBodyFn.scala
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/searches/queries/term/TermsSetQueryBodyFn.scala
@@ -9,6 +9,7 @@ object TermsSetQueryBodyFn {
     val builder = XContentFactory.jsonBuilder().startObject("terms_set")
     builder.startObject(t.field)
     builder.array("terms", t.terms.toArray[String])
+    t.boost.foreach(builder.field("boost", _))
     t.minimumShouldMatchField.foreach(builder.field("minimum_should_match_field", _))
     t.minimumShouldMatchScript.foreach(script => builder.rawField("minimum_should_match_script", handlers.script.ScriptBuilderFn(script)))
     builder.endObject().endObject()
