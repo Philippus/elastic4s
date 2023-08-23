8c3d8982d9a1b661aff8d5cda753e0d64b52e4b8
Travis Andelin <travisa@qualtrics.com>
2023-08-23 14:09:36 -0700

    Removed unused import, simplified basic knn test

--- elastic4s-core/src/test/scala/com/sksamuel/elastic4s/requests/searches/queries/KnnBuilderFnTest.scala
+++ elastic4s-core/src/test/scala/com/sksamuel/elastic4s/requests/searches/queries/KnnBuilderFnTest.scala
@@ -1,6 +1,5 @@
 package com.sksamuel.elastic4s.requests.searches.queries
 
-import com.sksamuel.elastic4s.api.KnnApi
 import com.sksamuel.elastic4s.handlers.searches.knn.KnnBuilderFn
 import com.sksamuel.elastic4s.requests.searches.knn.Knn
 import com.sksamuel.elastic4s.requests.searches.term.TermQuery
@@ -10,9 +9,9 @@ import org.scalatest.matchers.should.Matchers
 class KnnBuilderFnTest extends AnyFunSuite with Matchers {
 
   test("Basic Knn request generates proper query.") {
-    val request = Knn("image-vector", 50, Seq(54,10,-2)) k 5
+    val request = Knn("image-vector", 50, Seq(54,10,-2))
     KnnBuilderFn(request).string shouldBe
-      """{"field":"image-vector","query_vector":[54.0,10.0,-2.0],"k":5,"num_candidates":50,"boost":1.0}"""
+      """{"field":"image-vector","query_vector":[54.0,10.0,-2.0],"k":1,"num_candidates":50,"boost":1.0}"""
   }
   test("Knn with all fields generates proper query.") {
     val request = Knn("image-vector", 50, Seq(54,10,-2)) k 5 filter TermQuery("file-type", "png") similarity 10 boost .4
