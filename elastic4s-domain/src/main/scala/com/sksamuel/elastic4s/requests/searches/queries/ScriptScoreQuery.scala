3b19578baf7360b53b5ffb51d9752263a263f727
Sam Briggs <sjbriggs14@email.com>
2023-10-25 18:16:23 +0100

    [#2941] Implement script score query
    Implements the script score query to fix #2941.
    For now the ScriptScoreQueryBodyFn2 class has the terrible name because
    I suspect that the current ScriptScoreQueryBodyFn is never exercised.
    The ScriptScore that it accepts isn't actually a query, so will never
    hit the pattern match?

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/ScriptScoreQuery.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/searches/queries/ScriptScoreQuery.scala
@@ -1,3 +1,10 @@
 package com.sksamuel.elastic4s.requests.searches.queries
 
-case class ScriptScoreQuery(script: String) extends Query
+import com.sksamuel.elastic4s.requests.script.Script
+
+case class ScriptScoreQuery(query: Option[Query] = None, script: Option[Script] = None, minScore: Option[Double] = None, boost: Option[Double] = None) extends Query {
+  def boost(boost: Double): ScriptScoreQuery   = copy(boost = Option(boost))
+  def minScore(min: Double): ScriptScoreQuery  = copy(minScore = Option(min))
+  def query(query: Query): ScriptScoreQuery    = copy(query = Some(query))
+  def script(script: Script): ScriptScoreQuery = copy(script = Some(script))
+}
