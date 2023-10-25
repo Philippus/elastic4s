d81ac319869f56a5ab4a912a20b67e4fd571dba6
Sam Briggs <sjbriggs14@email.com>
2023-10-25 18:28:03 +0100

    Remove ScriptScoreQueryBodyFn
    From inspection I suspect this case would never be hit, because
    ScriptScore doesn't implement the Query trait.
    This was the only call site for ScriptScoreQueryBodyFn (which has an odd
    name, given it accepted a ScriptScore and not a ScriptScoreQuery).
    Therefore I've removed it, and put the ScriptScoreQueryBodyFn2 in its
    place

--- elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/searches/queries/ScriptScoreQueryBodyFn.scala
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/searches/queries/ScriptScoreQueryBodyFn.scala
@@ -1,13 +1,20 @@
 package com.sksamuel.elastic4s.handlers.searches.queries
 
+import com.sksamuel.elastic4s.handlers.script.ScriptBuilderFn
 import com.sksamuel.elastic4s.json.{XContentBuilder, XContentFactory}
-import com.sksamuel.elastic4s.requests.searches.queries.funcscorer.ScriptScore
+import com.sksamuel.elastic4s.requests.searches.queries.ScriptScoreQuery
 
 object ScriptScoreQueryBodyFn {
 
-  def apply(q: ScriptScore): XContentBuilder = {
+  def apply(q: ScriptScoreQuery): XContentBuilder = {
     val builder = XContentFactory.jsonBuilder()
-    builder.rawField("script", q.script.script)
+    builder.startObject("script_score")
+    q.query.foreach(q => builder.rawField("query", QueryBuilderFn(q)))
+    q.script.foreach(s => builder.rawField("script", ScriptBuilderFn(s)))
+    q.boost.foreach(builder.field("boost", _))
+    q.minScore.foreach(builder.field("min_score", _))
+    builder.endObject()
+    builder.endObject()
     builder
   }
 
