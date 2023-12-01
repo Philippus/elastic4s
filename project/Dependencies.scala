e77527f5124a4c76daf0ad757d712a2eef7b72a2
Scala Steward <scala_steward@virtuslab.com>
2023-12-01 21:35:42 +0000

    Update pekko-stream to 1.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val MockitoVersion                 = "5.7.0"
   val MonixVersion                   = "3.4.1"
   val PekkoHttpVersion               = "1.0.0"
-  val PekkoVersion                   = "1.0.1"
+  val PekkoVersion                   = "1.0.2"
   val PlayJsonVersion                = "3.0.1"
   val ReactiveStreamsVersion         = "1.0.3"
   val ScalatestPlusMockitoArtifactId = "mockito-3-4"
