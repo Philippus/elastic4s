86a70da2ed0861d1b006f0d11663d75023fc48c2
Joel Vimenet <1035781+joelvim@users.noreply.github.com>
2024-01-26 16:33:35 +0100

    Add failed_shards in task error

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/task/TaskError.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/task/TaskError.scala
@@ -1,10 +1,12 @@
 package com.sksamuel.elastic4s.requests.task
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.sksamuel.elastic4s.FailedShard
 
 case class TaskError(`type`: String,
                      reason: String,
                      phase: String,
                      grouped: Boolean,
-                     @JsonProperty("caused_by") causedBy: Option[Cause]
+                     @JsonProperty("caused_by") causedBy: Option[Cause],
+                     @JsonProperty("failed_shards") failedShards: Seq[FailedShard] = Seq()
                     )
--- elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/tasks/GetTaskTest.scala
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/tasks/GetTaskTest.scala
@@ -1,7 +1,11 @@
 package com.sksamuel.elastic4s.tasks
 
 import com.sksamuel.elastic4s.requests.common.RefreshPolicy
-import com.sksamuel.elastic4s.requests.task.Retries
+import com.sksamuel.elastic4s.requests.searches.queries.QueryStringQuery
+import com.sksamuel.elastic4s.requests.searches.queries.compound.BoolQuery
+import com.sksamuel.elastic4s.requests.searches.term.TermQuery
+import com.sksamuel.elastic4s.requests.task.{GetTask, GetTaskResponse, Retries, Task}
+import com.sksamuel.elastic4s.requests.update.UpdateByQueryTask
 import com.sksamuel.elastic4s.testkit.DockerTests
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -47,6 +51,34 @@ class GetTaskTest extends AnyWordSpec with Matchers with DockerTests {
       task.status.throttledTime should be(0.millis)
       task.status.noops should be(0)
       task.status.retries should be(Retries(0, 0))
+    }
+
+    "return task information when error occurs" in {
+
+      val start = System.currentTimeMillis()
+
+      // Build an invalid query to make the update by query task fail
+      val query = (0 until 91265)
+        .flatMap(i => Seq(TermQuery("foo", i.toString), TermQuery("moo", (i+1).toString), TermQuery("goo" ,(i+2).toString)))
+
+      // kick off a task
+      val resp: GetTask = client.execute {
+        updateByQueryAsync("get_task_a", BoolQuery(should = query))
+      }.await.result.task
+
+      // use the task id from the above task
+      val result: GetTaskResponse = client.execute {
+        getTask(resp.nodeId, resp.taskId)
+      }.await.result
+
+      result.task.node shouldBe resp.nodeId
+      result.task.id shouldBe resp.taskId
+      result.task.`type` shouldBe "transport"
+      result.task.action shouldBe "indices:data/write/update/byquery"
+      result.task.startTimeInMillis >= start shouldBe true
+      result.error.get.failedShards.length should be > 0
+      val reason = result.error.get.failedShards.head.reason.get
+      reason.`type` shouldBe "query_shard_exception"
     }
   }
 }
