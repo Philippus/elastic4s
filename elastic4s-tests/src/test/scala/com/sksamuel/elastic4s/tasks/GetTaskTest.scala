657bb9865b9a7cbfa4fb888049d61b98f67363df
Joël Vimenet <1035781+joelvim@users.noreply.github.com>
2024-05-04 19:22:48 +0200

    Add `failed_shards` in task error (#2996)
    * Add failed_shards in task error
    
    * Upgrade ES servers and fix test
    
    * Try to sleep longer
    
    * Retry to get the task
    
    * Timeout at 1min
    
    * Sleep between tries
    
    * Add logging
    
    * Debug get task result
    
    * Increase timeout
    
    * Change condition
    
    * Increase the clause number to force the failure
    
    * Remove println

--- elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/tasks/GetTaskTest.scala
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/tasks/GetTaskTest.scala
@@ -1,7 +1,9 @@
 package com.sksamuel.elastic4s.tasks
 
 import com.sksamuel.elastic4s.requests.common.RefreshPolicy
-import com.sksamuel.elastic4s.requests.task.Retries
+import com.sksamuel.elastic4s.requests.searches.queries.compound.BoolQuery
+import com.sksamuel.elastic4s.requests.searches.term.TermQuery
+import com.sksamuel.elastic4s.requests.task.{GetTask, GetTaskResponse, Retries}
 import com.sksamuel.elastic4s.testkit.DockerTests
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -47,6 +49,46 @@ class GetTaskTest extends AnyWordSpec with Matchers with DockerTests {
       task.status.throttledTime should be(0.millis)
       task.status.noops should be(0)
       task.status.retries should be(Retries(0, 0))
+    }
+
+    "return task information when error occurs" in {
+
+      val start = System.currentTimeMillis()
+
+      // Build an invalid query to make the update by query task fail
+      val query = (0 until 200000)
+        .flatMap(i => Seq(TermQuery("foo", i.toString), TermQuery("moo", (i+1).toString), TermQuery("goo" ,(i+2).toString)))
+
+      // kick off a task
+      val resp: GetTask = client.execute {
+        updateByQueryAsync("get_task_a", BoolQuery(should = query))
+      }.await.result.task
+
+      val before = System.currentTimeMillis()
+
+      // Sorry for this
+      var result: GetTaskResponse = null
+      var found = false
+
+      // Retry to get the task until it is executed
+       while (!found) {
+         // use the task id from the above task
+         result = client.execute {
+          getTask(resp.nodeId, resp.taskId)
+        }.await.result
+         val elapsed = System.currentTimeMillis() - before
+         found = result.completed || (elapsed > 60000)
+         Thread.sleep(1000)
+      }
+
+      result.task.node shouldBe resp.nodeId
+      result.task.id shouldBe resp.taskId
+      result.task.`type` shouldBe "transport"
+      result.task.action shouldBe "indices:data/write/update/byquery"
+      result.task.startTimeInMillis >= start shouldBe true
+      result.error.get.failedShards.length should be > 0
+      val reason = result.error.get.failedShards.head.reason.get
+      reason.`type` shouldBe "query_shard_exception"
     }
   }
 }
