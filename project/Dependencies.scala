01d569516e7223d016a7ce6c5b4e45aa38885be8
Scala Steward <scala_steward@virtuslab.com>
2024-05-21 20:23:59 +0000

    Update client3:async-http-client-backend-future, ... to 3.9.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   val ScalatestVersion               = "3.2.18"
   val Slf4jVersion                   = "2.0.13"
   val SprayJsonVersion               = "1.3.6"
-  val SttpVersion                    = "3.9.6"
+  val SttpVersion                    = "3.9.7"
   val ZIOJson1Version                = "0.1.5"
   val ZIO1Version                    = "1.0.18"
   val ZIOVersion                     = "2.1.1"
