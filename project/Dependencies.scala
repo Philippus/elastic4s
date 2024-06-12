04ce40b9056aded5a319898700e890faade142f2
Scala Steward <scala_steward@virtuslab.com>
2024-06-12 16:56:42 +0000

    Update zio to 2.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   val SttpVersion                    = "3.9.7"
   val ZIOJson1Version                = "0.1.5"
   val ZIO1Version                    = "1.0.18"
-  val ZIOVersion                     = "2.1.2"
+  val ZIOVersion                     = "2.1.3"
   val ZIOJsonVersion                 = "0.7.0"
 
   lazy val commonDeps = Seq(
