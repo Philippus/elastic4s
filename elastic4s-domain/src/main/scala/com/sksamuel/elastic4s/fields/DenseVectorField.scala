24d6108e41f7e5d9fd600c52b64fb931be9e97c9
Alex Pavlychev <aspavlychev@yandex.ru>
2024-07-13 12:32:40 +0300

    DenseVectorField lacks index_options parameter (#3099)
    * close https://github.com/Philippus/elastic4s/issues/3098
    
    * Update elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/fields/DenseVectorFieldBuilderFn.scala
    
    Co-authored-by: Philippus Baalman <philippus@gmail.com>
    
    * [review] close https://github.com/Philippus/elastic4s/issues/3098
    
    ADT for a index_option field and tests
    
    ---------
    
    Co-authored-by: Philippus Baalman <philippus@gmail.com>

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/DenseVectorField.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/DenseVectorField.scala
@@ -13,6 +13,25 @@ case object Cosine extends Similarity { val name = "cosine" }
 case class DenseVectorField(name: String,
                             dims: Int,
                             index: Boolean = false,
-                            similarity: Similarity = L2Norm) extends ElasticField {
-  override def `type`: String = DenseVectorField.`type`
+                            similarity: Similarity = L2Norm,
+                            indexOptions: Option[DenseVectorIndexOptions] = None) extends ElasticField {
+  override def `type`: String  = DenseVectorField.`type`
+}
+
+sealed trait DenseVectorIndexOptions {
+  def `type`: String
+}
+case class HnswIndexOptions(m: Option[Int] = None, efConstruction: Option[Int] = None) extends DenseVectorIndexOptions {
+  val `type`: String = "hnsw"
+}
+case class Int8HnswIndexOptions(m: Option[Int] = None,
+                                efConstruction: Option[Int] = None,
+                                confidenceInterval: Option[Double] = None) extends DenseVectorIndexOptions {
+  val `type`: String = "int8_hnsw"
+}
+case class FlatIndexOptions() extends DenseVectorIndexOptions {
+  val `type`: String = "flat"
+}
+case class Int8FlatIndexOptions(confidenceInterval: Option[Double] = None) extends DenseVectorIndexOptions {
+  val `type`: String = "int8_flat"
 }
