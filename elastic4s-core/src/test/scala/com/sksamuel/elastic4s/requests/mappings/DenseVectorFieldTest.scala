4b57546f64cfba5fa58b003b3c6afb7fb29a623e
philippus <philippus@gmail.com>
2024-07-13 13:17:18 +0200

    Simplify tests

--- elastic4s-core/src/test/scala/com/sksamuel/elastic4s/requests/mappings/DenseVectorFieldTest.scala
+++ elastic4s-core/src/test/scala/com/sksamuel/elastic4s/requests/mappings/DenseVectorFieldTest.scala
@@ -1,13 +1,14 @@
 package com.sksamuel.elastic4s.requests.mappings
 
-import com.sksamuel.elastic4s.fields.DenseVectorField.{Hnsw, Int8Hnsw}
+import com.sksamuel.elastic4s.fields.DenseVectorField.{Flat, Hnsw, Int8Flat, Int8Hnsw}
 import com.sksamuel.elastic4s.ElasticApi
 import com.sksamuel.elastic4s.fields.{Cosine, DenseVectorField, DenseVectorIndexOptions, DotProduct, L2Norm, MaxInnerProduct}
-import com.sksamuel.elastic4s.handlers.fields.{DenseVectorFieldBuilderFn, ElasticFieldBuilderFn}
+import com.sksamuel.elastic4s.handlers.fields.{DenseVectorFieldBuilderFn}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class DenseVectorFieldTest extends AnyFlatSpec with Matchers with ElasticApi {
+  private val denseVectorIndexOptions = DenseVectorIndexOptions(Int8Hnsw, Some(10), Some(100), Some(1.0f))
 
   "A DenseVectorField" should "support dims property" in {
     val field = DenseVectorField(name = "myfield", dims = 3)
@@ -39,37 +40,21 @@ class DenseVectorFieldTest extends AnyFlatSpec with Matchers with ElasticApi {
       """{"type":"dense_vector","dims":3,"index":false}"""
   }
 
-  "A DenseVectorField" should "don't support a flat type of kNN algorithm for a index_options if a index property is false" in {
-    val field = DenseVectorField(
-      name = "myfield",
-      dims = 3,
-      similarity = L2Norm,
-      indexOptions = Some(DenseVectorIndexOptions(`type` = DenseVectorField.Flat))
-    )
-    val jsonStringValue = """{"type":"dense_vector","dims":3,"index":false,"similarity":"l2_norm"}"""
-    ElasticFieldBuilderFn(field).string shouldBe jsonStringValue
+  it should "support indexOptions property" in {
+    val field = DenseVectorField(name = "myfield", dims = 3, index = true, indexOptions = Some(denseVectorIndexOptions))
+    DenseVectorFieldBuilderFn.build(field).string shouldBe
+      """{"type":"dense_vector","dims":3,"index":true,"similarity":"l2_norm","index_options":{"type":"int8_hnsw","m":10,"ef_construction":100,"confidence_interval":1.0}}"""
   }
 
-  "A DenseVectorField" should "support a int8_flat type of kNN algorithm for a index_options if a index property is true" in {
-    val field = DenseVectorField(
-      name = "myfield",
-      dims = 3,
-      index = true,
-      similarity = L2Norm,
-      indexOptions = Some(DenseVectorIndexOptions(`type` = DenseVectorField.Int8Flat, confidenceInterval = Some(0.5d)))
-    )
-    val jsonStringValue = """{"type":"dense_vector","dims":3,"index":true,"similarity":"l2_norm","index_options":{"type":"int8_flat","confidence_interval":0.5}}"""
-    ElasticFieldBuilderFn(field).string shouldBe jsonStringValue
-  }
-
-  "A DenseVectorField" should "don't support a int8_flat type of kNN algorithm for a index_options if a index property  is false" in {
-    val field = DenseVectorField(
-      name = "myfield",
-      dims = 3,
-      similarity = L2Norm,
-      indexOptions = Some(DenseVectorIndexOptions(`type` = DenseVectorField.Int8Flat, confidenceInterval = Some(0.5d)))
-    )
-    val jsonStringValue = """{"type":"dense_vector","dims":3,"index":false,"similarity":"l2_norm"}"""
-    ElasticFieldBuilderFn(field).string shouldBe jsonStringValue
+  it should "support all index options types and only set m, efConstruction and confidenceInterval when applicable" in {
+    val field = DenseVectorField(name = "myfield", dims = 3, index = true, indexOptions = Some(denseVectorIndexOptions))
+    DenseVectorFieldBuilderFn.build(field).string shouldBe
+      """{"type":"dense_vector","dims":3,"index":true,"similarity":"l2_norm","index_options":{"type":"int8_hnsw","m":10,"ef_construction":100,"confidence_interval":1.0}}"""
+    DenseVectorFieldBuilderFn.build(field.copy(indexOptions = Some(denseVectorIndexOptions.copy(`type` = Hnsw)))).string shouldBe
+      """{"type":"dense_vector","dims":3,"index":true,"similarity":"l2_norm","index_options":{"type":"hnsw","m":10,"ef_construction":100}}"""
+    DenseVectorFieldBuilderFn.build(field.copy(indexOptions = Some(denseVectorIndexOptions.copy(`type` = Flat)))).string shouldBe
+      """{"type":"dense_vector","dims":3,"index":true,"similarity":"l2_norm","index_options":{"type":"flat"}}"""
+    DenseVectorFieldBuilderFn.build(field.copy(indexOptions = Some(denseVectorIndexOptions.copy(`type` = Int8Flat)))).string shouldBe
+      """{"type":"dense_vector","dims":3,"index":true,"similarity":"l2_norm","index_options":{"type":"int8_flat","confidence_interval":1.0}}"""
   }
 }
