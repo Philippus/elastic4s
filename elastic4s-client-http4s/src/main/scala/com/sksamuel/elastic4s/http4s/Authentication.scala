0e70d067f0904673b347f1f6d7a628dc82976eda
Igor Vovk <adwolf15@gmail.com>
2024-07-17 11:00:25 +0200

    Rename authorization => authentication

---
+++ elastic4s-client-http4s/src/main/scala/com/sksamuel/elastic4s/http4s/Authentication.scala
@@ -0,0 +1,8 @@
+package com.sksamuel.elastic4s.http4s
+
+sealed trait Authentication
+
+object Authentication {
+  case class UsernamePassword(username: String, password: String) extends Authentication
+  case object NoAuth extends Authentication
+}
