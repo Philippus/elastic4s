3aeab631c156de4db7649f4dc5c377b7882ac118
Igor Vovk <adwolf15@gmail.com>
2024-07-17 11:13:21 +0200

    Support ApiKey auth scheme

--- elastic4s-client-http4s/src/main/scala/com/sksamuel/elastic4s/http4s/Authentication.scala
+++ elastic4s-client-http4s/src/main/scala/com/sksamuel/elastic4s/http4s/Authentication.scala
@@ -4,5 +4,8 @@ sealed trait Authentication
 
 object Authentication {
   case class UsernamePassword(username: String, password: String) extends Authentication
+
+  case class ApiKey(apiKey: String) extends Authentication
+
   case object NoAuth extends Authentication
 }
