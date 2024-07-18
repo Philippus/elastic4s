495ad7f3e82723ab85039662d00c2ff797b9fb79
Igor Vovk <adwolf15@gmail.com>
2024-07-18 09:11:51 +0200

    Fix tests

--- elastic4s-effect-zio-1/src/test/scala/com/sksamuel/elastic4s/zio/ZIOTaskTest.scala
+++ elastic4s-effect-zio-1/src/test/scala/com/sksamuel/elastic4s/zio/ZIOTaskTest.scala
@@ -1,13 +1,27 @@
 package com.sksamuel.elastic4s.zio
 
+import com.sksamuel.elastic4s.{ElasticClient, ElasticNodeEndpoint}
+import com.sksamuel.elastic4s.sttp.SttpRequestHttpClient
 import com.sksamuel.elastic4s.testkit.DockerTests
 import com.sksamuel.elastic4s.zio.instances._
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import sttp.client3.SttpBackend
+import sttp.client3.httpclient.zio.HttpClientZioBackend
+import sttp.client3.impl.zio.RIOMonadAsyncError
+import sttp.monad.MonadError
 import zio.Task
 
 class ZIOTaskTest extends AnyFlatSpec with Matchers with DockerTests with BeforeAndAfterAll {
+  private implicit val sttpZioMonadError: MonadError[Task] = new RIOMonadAsyncError()
+  private implicit val sttpBackend: SttpBackend[Task, Any] =
+    HttpClientZioBackend().unsafeRun.fold(throw _, identity)
+
+  private lazy val sttpClient: SttpRequestHttpClient[Task] =
+    new SttpRequestHttpClient(ElasticNodeEndpoint("http", elasticHost, elasticPort.toInt, None))
+
+  val zioClient: ElasticClient[Task] = ElasticClient(sttpClient)
 
   implicit class RichZIO[A](task: Task[A]) {
     def unsafeRun: Either[Throwable, A] = {
@@ -16,19 +30,19 @@ class ZIOTaskTest extends AnyFlatSpec with Matchers with DockerTests with Before
   }
 
   override def beforeAll(): Unit = {
-    client.execute {
+    zioClient.execute {
       deleteIndex("testindex")
     }.unsafeRun
   }
 
   override def afterAll(): Unit = {
-    client.execute {
+    zioClient.execute {
       deleteIndex("testindex")
     }.unsafeRun
   }
 
   it should "index doc successfully" in {
-    val r = client.execute {
+    val r = zioClient.execute {
       indexInto("testindex").doc("""{ "text":"Buna ziua!" }""")
     }.unsafeRun
     r shouldBe Symbol("right")
