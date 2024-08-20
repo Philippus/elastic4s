cc8a77dfe494a759b6a9d3acd905b76bd83abede
philippus <philippus@gmail.com>
2024-08-20 12:24:21 -0500

    Add positiveScoreImpact parameter to RankFeaturesField

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/RankFeaturesField.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/RankFeaturesField.scala
@@ -3,6 +3,7 @@ package com.sksamuel.elastic4s.fields
 object RankFeaturesField {
   val `type`: String = "rank_features"
 }
-case class RankFeaturesField(name: String) extends ElasticField {
+case class RankFeaturesField(name: String,
+                             positiveScoreImpact: Option[Boolean] = None) extends ElasticField {
   override def `type`: String = RankFeaturesField.`type`
 }
--- elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/fields/RankFeaturesFieldBuilderFn.scala
+++ elastic4s-handlers/src/main/scala/com/sksamuel/elastic4s/handlers/fields/RankFeaturesFieldBuilderFn.scala
@@ -4,12 +4,17 @@ import com.sksamuel.elastic4s.fields.RankFeaturesField
 import com.sksamuel.elastic4s.json.{XContentBuilder, XContentFactory}
 
 object RankFeaturesFieldBuilderFn {
-  def toField(name: String, values: Map[String, Any]): RankFeaturesField = RankFeaturesField(name)
+  def toField(name: String, values: Map[String, Any]): RankFeaturesField =
+    RankFeaturesField(
+      name,
+      values.get("positive_score_impact").map(_.asInstanceOf[Boolean])
+    )
 
   def build(field: RankFeaturesField): XContentBuilder = {
 
     val builder = XContentFactory.jsonBuilder()
     builder.field("type", field.`type`)
+    field.positiveScoreImpact.foreach(builder.field("positive_score_impact", _))
     builder.endObject()
   }
 }
