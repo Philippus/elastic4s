efcc0fc6ec79a1a7fce3e14c5d2464693c23fe7a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-10-30 23:18:33 +0100

    Update jackson-core, jackson-databind to 2.18.1 (#3191)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val CirceVersion                   = "0.14.10"
   val CommonsIoVersion               = "2.17.0"
   val ElasticsearchVersion           = "8.15.3"
-  val JacksonVersion                 = "2.18.0"
+  val JacksonVersion                 = "2.18.1"
   val Json4sVersion                  = "4.0.7"
   val Log4jVersion                   = "2.15.0"
   val MockitoVersion                 = "5.14.2"
