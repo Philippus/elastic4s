6fd919d3ce6580a36216b7af35d11c37f2530a98
philippus <philippus@gmail.com>
2024-11-03 18:59:50 +0100

    Update akka to 2.6.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Dependencies {
   val AkkaHttpVersion                = "10.2.10"
-  val AkkaVersion                    = "2.6.20"
+  val AkkaVersion                    = "2.6.21"
   val CatsEffect2Version             = "2.5.5"
   val CatsEffectVersion              = "3.5.5"
   val CirceVersion                   = "0.14.10"
