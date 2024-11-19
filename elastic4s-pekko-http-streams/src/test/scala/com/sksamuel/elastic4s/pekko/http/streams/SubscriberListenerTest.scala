6872ba8f86cfa159eb8c6b0801b18f8845362ad3
jmarreroValsea <jorge.marrero@valsea.com>
2024-11-19 14:27:12 +0000

    feat: adding elastic4s-pekko-http-streams module

---
+++ elastic4s-pekko-http-streams/src/test/scala/com/sksamuel/elastic4s/pekko/http/streams/SubscriberListenerTest.scala
@@ -0,0 +1,40 @@
+package com.sksamuel.elastic4s.pekko.http.streams
+
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
+import org.apache.pekko.actor.ActorSystem
+import com.sksamuel.elastic4s.requests.bulk.BulkResponseItem
+import com.sksamuel.elastic4s.testkit.DockerTests
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.util.Try
+
+class SubscriberListenerTest extends AnyWordSpec with Matchers with DockerTests {
+
+  import ReactiveElastic._
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val builder: ShipRequestBuilder = new ShipRequestBuilder()
+
+  Try {
+    client.execute {
+      createIndex("subscriberlistenertest")
+    }.await
+  }
+
+  "Reactive streams subscriber" should {
+    "invoke listener for each confirmed doc" ignore {
+
+      val latch = new CountDownLatch(Ship.ships.length)
+
+      val config = SubscriberConfig(listener = new ResponseListener[Ship] {
+        def onAck(resp: BulkResponseItem, original: Ship): Unit = latch.countDown()
+      })
+      val subscriber = client.subscriber[Ship](config)
+      ShipPublisher.subscribe(subscriber)
+
+      latch.await(1, TimeUnit.MINUTES) shouldBe true
+    }
+  }
+}
