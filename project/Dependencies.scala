9a4bddc7b6087d4c59bb1285810db36104b3515a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-01-04 17:27:32 +0100

    Update mockito-core to 5.15.2 (#3273)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val JacksonVersion                 = "2.18.2"
   val Json4sVersion                  = "4.0.7"
   val Log4jVersion                   = "2.24.3"
-  val MockitoVersion                 = "5.14.2"
+  val MockitoVersion                 = "5.15.2"
   val MonixVersion                   = "3.4.1"
   val PekkoHttpVersion               = "1.1.0"
   val PekkoVersion                   = "1.1.2"
