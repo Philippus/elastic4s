a509bf614e677e114d02bcf9067d8a72b377f1c2
q-martindurchov <martindurchov@quantexa.com>
2025-02-13 19:41:22 +0000

    add tests for null edge case of the Maps class flatten function

---
+++ elastic4s-domain/src/test/scala/com/sksamuel/elastic4s/MapsTest.scala
@@ -0,0 +1,58 @@
+package com.sksamuel.elastic4s
+
+import com.sksamuel.elastic4s.ext.Maps
+import org.scalatest
+import org.scalatest.matchers.should.Matchers
+
+class MapsTest extends scalatest.flatspec.AnyFlatSpec with Matchers {
+  "Maps flatten function" should "support null values" in {
+    val jsonMap: Map[String, Any] = Map(
+      "settings" -> Map(
+        "index" -> Map(
+          "routing" -> Map(
+            "allocation" -> Map(
+              "include" -> Map(
+                "_tier_preference" -> "data_content"
+              ),
+              "initial_recovery" -> Map(
+                "_id" -> null
+              )
+            )
+          ),
+          "number_of_shards" -> "1",
+          "routing_partition_size" -> "1",
+          "blocks" -> Map(
+            "write" -> "true"
+          ),
+          "provided_name" -> "test-two",
+          "resize" -> Map(
+            "source" -> Map(
+              "name" -> "test-index",
+              "uuid" -> "1234"
+            )
+          ),
+          "creation_date" -> "1234",
+          "number_of_replicas" -> "1",
+          "uuid" -> "1234",
+          "version" -> Map(
+            "created" -> "1234"
+          )
+        )
+      )
+    )
+    Maps.flatten(jsonMap) shouldBe Map(
+      "settings.index.resize.source.name" -> "test-index",
+      "settings.index.resize.source.uuid" -> "1234",
+      "settings.index.provided_name" -> "test-two",
+      "settings.index.blocks.write" -> "true",
+      "settings.index.routing_partition_size" -> "1",
+      "settings.index.routing.allocation.include._tier_preference" -> "data_content",
+      "settings.index.routing.allocation.initial_recovery._id" -> null,
+      "settings.index.number_of_replicas" -> "1",
+      "settings.index.version.created" -> "1234",
+      "settings.index.number_of_shards" -> "1",
+      "settings.index.creation_date" -> "1234",
+      "settings.index.uuid" -> "1234"
+    )
+  }
+}
