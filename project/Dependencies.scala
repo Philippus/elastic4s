01c80b15b7db2025ee90791dedf516698a33f1e2
Scala Steward <scala_steward@virtuslab.com>
2025-03-17 21:07:24 +0000

    Update circe-core, circe-generic, ... to 0.14.12 in series/7.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val CatsEffect2Version             = "2.5.5"
   val CatsEffectVersion              = "3.5.7"
   val CatsVersion                    = "2.0.0"
-  val CirceVersion                   = "0.14.10"
+  val CirceVersion                   = "0.14.12"
   val CommonsIoVersion               = "2.18.0"
   val ElasticsearchVersion           = "7.17.28"
   val ExtsVersion                    = "1.61.1"
