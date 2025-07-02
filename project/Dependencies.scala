8de9c8e9bfef63495d203c79c8fe7abb0a7ba160
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-07-02 06:58:33 +0200

    Update play-json to 3.0.5 (#3527)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val MonixVersion                   = "3.4.1"
   val PekkoHttpVersion               = "1.2.0"
   val PekkoVersion                   = "1.1.4"
-  val PlayJsonVersion                = "3.0.4"
+  val PlayJsonVersion                = "3.0.5"
   val ReactiveStreamsVersion         = "1.0.4"
   val ScalatestPlusMockitoArtifactId = "mockito-5-12"
   val ScalatestPlusVersion           = "3.2.19.0"
