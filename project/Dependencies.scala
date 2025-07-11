957961aafc16eb405e030a82588e3329869fb061
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-07-11 18:55:25 +0200

    Update typesafe:config to 1.4.4 in series/9.x (#3542)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
 
   lazy val commonDeps = Seq(
     libraryDependencies ++= Seq(
-      "com.typesafe"       % "config"                       % "1.4.3",
+      "com.typesafe"       % "config"                       % "1.4.4",
       "org.slf4j"          % "slf4j-api"                    % Slf4jVersion,
       "org.scalatest"     %% "scalatest"                    % ScalatestVersion     % Test,
       "org.mockito"        % "mockito-core"                 % MockitoVersion       % Test,
