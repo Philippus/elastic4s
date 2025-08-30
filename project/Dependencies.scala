6846052426b9912d44d1f2aadcb8d72a803e383c
Scala Steward <scala_steward@virtuslab.com>
2025-08-30 16:56:11 +0000

    Update jackson-module-scala to 2.20.0 in series/7.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val CommonsIoVersion               = "2.20.0"
   val ElasticsearchVersion           = "7.17.29"
   val ExtsVersion                    = "1.61.1"
-  val JacksonVersion                 = "2.19.2"
+  val JacksonVersion                 = "2.20.0"
   val Json4sVersion                  = "4.0.7"
   val Log4jVersion                   = "2.25.1"
   val MockitoVersion                 = "5.19.0"
