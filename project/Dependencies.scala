99d040721af952b7697e9d26ca4662f70464e54d
Scala Steward <scala_steward@virtuslab.com>
2025-10-31 17:21:57 +0000

    Update zio-json to 0.7.45 in series/8.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   val ZIOJson1Version                = "0.1.5"
   val ZIO1Version                    = "1.0.18"
   val ZIOVersion                     = "2.1.22"
-  val ZIOJsonVersion                 = "0.7.44"
+  val ZIOJsonVersion                 = "0.7.45"
 
   lazy val commonDeps = Seq(
     libraryDependencies ++= Seq(
