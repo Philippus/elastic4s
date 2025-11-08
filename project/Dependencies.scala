309c65d61034567f7964e23f867668e2959c4a7a
Scala Steward <scala_steward@virtuslab.com>
2025-11-08 16:48:02 +0000

    Update commons-io to 2.21.0 in series/7.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val CatsEffectVersion              = "3.6.3"
   val CatsVersion                    = "2.0.0"
   val CirceVersion                   = "0.14.15"
-  val CommonsIoVersion               = "2.20.0"
+  val CommonsIoVersion               = "2.21.0"
   val ElasticsearchVersion           = "7.17.29"
   val ExtsVersion                    = "1.61.1"
   val JacksonVersion                 = "2.20.1"
