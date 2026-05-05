b81a0d301cdb6eb844157f956bdb349f622e1ec9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-05-05 23:10:59 +0200

    Update typesafe:config to 1.4.8 (#3945)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
 
   lazy val commonDeps = Seq(
     libraryDependencies ++= Seq(
-      "com.typesafe"   % "config"    % "1.4.7",
+      "com.typesafe"   % "config"    % "1.4.8",
       "org.slf4j"      % "slf4j-api" % Slf4jVersion,
       "org.scalatest" %% "scalatest" % ScalatestVersion % Test
     )
