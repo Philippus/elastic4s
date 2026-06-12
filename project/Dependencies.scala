f4826877f2d29612382bc9fe46fd8d8688cab897
Scala Steward <scala_steward@virtuslab.com>
2026-06-12 20:14:23 +0000

    Update scalaz-core to 7.3.9 in series/7.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   val ScalamockVersion               = "7.5.5"
   val ScalatestPlusMockitoArtifactId = "mockito-5-12"
   val ScalatestPlusVersion           = "3.2.19.0"
-  val ScalazVersion                  = "7.2.36"
+  val ScalazVersion                  = "7.3.9"
   val ScalatestVersion               = "3.2.20"
   val Slf4jVersion                   = "2.0.18"
   val SprayJsonVersion               = "1.3.6"
