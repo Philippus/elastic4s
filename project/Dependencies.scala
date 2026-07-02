61c9df0613a851d91a62e5497d799b3de6b392de
Scala Steward <scala_steward@virtuslab.com>
2026-07-02 19:27:37 +0000

    Update log4j-api, log4j-core, ... to 2.26.1 in series/8.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val Http4sVersion                  = "0.23.34"
   val JacksonVersion                 = "2.22.0"
   val Json4sVersion                  = "4.1.1"
-  val Log4jVersion                   = "2.26.0"
+  val Log4jVersion                   = "2.26.1"
   val MockitoVersion                 = "5.23.0"
   val MonixVersion                   = "3.4.1"
   val PekkoHttpVersion               = "1.3.0"
