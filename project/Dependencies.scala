eb7ec2d0f98c271770c76dfc441ab447a1dc472b
Scala Steward <scala_steward@virtuslab.com>
2026-07-08 18:21:25 +0000

    Update jackson-core to 2.22.1 in series/8.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val ElasticsearchVersion           = "8.19.18"
   val ExtsVersion                    = "1.61.1"
   val Http4sVersion                  = "0.23.34"
-  val JacksonVersion                 = "2.22.0"
+  val JacksonVersion                 = "2.22.1"
   val Json4sVersion                  = "4.1.1"
   val Log4jVersion                   = "2.26.1"
   val MockitoVersion                 = "5.23.0"
