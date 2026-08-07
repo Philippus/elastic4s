7242b769797db60fcc690d186ddaa46ba9bd04be
Philippus <philippus@gmail.com>
2026-08-07 19:13:27 +0200

    Populate id in Json4sHitReader from hit metadata

--- elastic4s-json-json4s/src/main/scala/com/sksamuel/elastic4s/json4s/ElasticJson4s.scala
+++ elastic4s-json-json4s/src/main/scala/com/sksamuel/elastic4s/json4s/ElasticJson4s.scala
@@ -2,8 +2,8 @@ package com.sksamuel.elastic4s.json4s
 
 import com.sksamuel.elastic4s.{AggReader, Hit, HitReader, Indexable, ParamSerializer}
 import org.json4s._
-
-import scala.reflect.Manifest
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods.{compact, render}
 import scala.util.Try
 
 object ElasticJson4s {
@@ -12,7 +12,9 @@ object ElasticJson4s {
     implicit def Json4sHitReader[T](implicit json4s: Serialization, formats: Formats, mf: Manifest[T]): HitReader[T] =
       (hit: Hit) =>
         Try {
-          json4s.read[T](hit.sourceAsString)
+          val source = json4s.read[JObject](hit.sourceAsString)
+          val withId = if ((source \ "_id") == JNothing) source ~ ("_id" -> hit.id) else source
+          json4s.read[T](compact(render(withId)))
         }
 
     implicit def Json4sAggReader[T](implicit json4s: Serialization, formats: Formats, mf: Manifest[T]): AggReader[T] =
--- elastic4s-json-json4s/src/test/scala/com/sksamuel/elastic4s/json4s/Json4sParamSerializerTest.scala
+++ elastic4s-json-json4s/src/test/scala/com/sksamuel/elastic4s/json4s/Json4sParamSerializerTest.scala
@@ -1,10 +1,13 @@
 package com.sksamuel.elastic4s.json4s
 
+import com.sksamuel.elastic4s.{Hit, HitReader}
 import com.sksamuel.elastic4s.requests.script.Script
 import org.json4s._
 import org.json4s.jackson.Serialization
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+
+case class Doc(_id: String, name: String)
 
 class Json4sParamSerializerTest extends AnyWordSpec with Matchers {
 
@@ -21,6 +24,30 @@ class Json4sParamSerializerTest extends AnyWordSpec with Matchers {
       Script("some script")
         .paramObject("some param", Foo(77))
         .paramsRaw shouldBe Map("some param" -> """{"bar":77}""")
+    }
+  }
+
+  "A derived HitReader instance" should {
+
+    "read _id from hit metadata when missing in source" in {
+      import ElasticJson4s.Implicits._
+      implicit val formats: Formats             = Serialization.formats(NoTypeHints)
+      implicit val serialization: Serialization = Serialization
+
+      val hit = new Hit {
+        override def id: String                       = "doc-1"
+        override def index: String                    = "places"
+        override def version: Long                    = 1L
+        override def seqNo: Long                      = 0L
+        override def primaryTerm: Long                = 1L
+        override def sort: Option[Seq[AnyRef]]        = None
+        override def sourceAsString: String           = """{"name":"Berlin"}"""
+        override def sourceAsMap: Map[String, AnyRef] = Map("name" -> "Berlin")
+        override def exists: Boolean                  = true
+        override def score: Float                     = 1.0F
+      }
+
+      implicitly[HitReader[Doc]].read(hit).get shouldBe Doc("doc-1", "Berlin")
     }
   }
 }
