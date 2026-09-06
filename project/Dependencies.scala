3f1d5a1868990d63a5a6490f4d7dd37c4f9d82e6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-06 08:50:53 +0200

    Update slf4j-api to 2.0.19 in series/7.x (#4103)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val ScalatestPlusVersion           = "3.2.19.0"
   val ScalazVersion                  = "7.2.36"
   val ScalatestVersion               = "3.2.20"
-  val Slf4jVersion                   = "2.0.18"
+  val Slf4jVersion                   = "2.0.19"
   val SprayJsonVersion               = "1.3.6"
   val SttpVersion                    = "1.7.2"
   val ZIOJsonVersion                 = "0.7.45"
