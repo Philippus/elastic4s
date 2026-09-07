bbb0c28aca0a027d971e67c867828c2005b4931e
Philippus <philippus@gmail.com>
2026-09-07 21:34:20 +0200

    Add new synonyms-related fields

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/synonyms/CreateOrUpdateSynonymsSetRequest.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/synonyms/CreateOrUpdateSynonymsSetRequest.scala
@@ -1,3 +1,8 @@
 package com.sksamuel.elastic4s.requests.synonyms
 
-case class CreateOrUpdateSynonymsSetRequest(synonymsSet: String, synonymRules: Seq[SynonymRule])
+case class CreateOrUpdateSynonymsSetRequest(
+    synonymsSet: String,
+    synonymRules: Seq[SynonymRule],
+    refresh: Option[Boolean] = None,
+    append: Option[Boolean] = None
+)
--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/synonyms/GetSynonymsSetRequest.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/requests/synonyms/GetSynonymsSetRequest.scala
@@ -1,6 +1,12 @@
 package com.sksamuel.elastic4s.requests.synonyms
 
-case class GetSynonymsSetRequest(synonymsSet: String, from: Option[Int] = None, size: Option[Int] = None) {
-  def from(from: Int): GetSynonymsSetRequest = copy(from = Some(from))
-  def size(size: Int): GetSynonymsSetRequest = copy(size = Some(size))
+case class GetSynonymsSetRequest(
+    synonymsSet: String,
+    from: Option[Int] = None,
+    size: Option[Int] = None,
+    searchAfter: Option[String] = None
+) {
+  def from(from: Int): GetSynonymsSetRequest                  = copy(from = Some(from))
+  def size(size: Int): GetSynonymsSetRequest                  = copy(size = Some(size))
+  def searchAfter(searchAfter: String): GetSynonymsSetRequest = copy(searchAfter = Some(searchAfter))
 }
